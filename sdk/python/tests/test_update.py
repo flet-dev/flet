be4c053d88e5557e5b030b17fdae1f2b6ad2b25b
Feodor Fitsner <feodor@appveyor.com>
2022-03-29 13:15:23 -0700

    Skip some Python tests

--- sdk/python/tests/test_update.py
+++ sdk/python/tests/test_update.py
@@ -1,6 +1,9 @@
+import pytest
+
 from flet import Textbox
 
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_update_single_control(page):
     txt = Textbox(id="txt1", label="First name:")
     page.add(txt)
