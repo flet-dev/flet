4c2b15f71e5f2fde19b0513090b5db1441029836
Feodor Fitsner <feodor@appveyor.com>
2022-03-30 21:00:50 -0700

    get free TCP port

--- sdk/python/examples/popen.py
+++ sdk/python/examples/popen.py
@@ -1,11 +1,20 @@
 import logging
 import os
+import socket
 import subprocess
-from asyncio.log import logger
 from re import sub
 from time import sleep
 
 logging.basicConfig(level=logging.INFO)
+
+
+def get_free_tcp_port():
+    sock = socket.socket()
+    sock.bind(("", 0))
+    return sock.getsockname()[1]
+
+
+print("Free TCP port", get_free_tcp_port())
 
 my_env = {**os.environ, "FLET_LOG_TO_FILE": "true"}
 
@@ -13,7 +22,7 @@ port = 8570
 flet_path = "flet.exe"
 args = [flet_path, "server", "--attached", "--port", str(port)]
 
-log_level = logger.getEffectiveLevel()
+log_level = logging.getLogger().getEffectiveLevel()
 if log_level == logging.CRITICAL:
     log_level = logging.FATAL
 
@@ -21,7 +30,13 @@ if log_level != logging.NOTSET:
     log_level_name = logging.getLevelName(log_level).lower()
     args.extend(["--log-level", log_level_name])
 
-subprocess.Popen(args, env=my_env, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.Popen(
+    args,
+    env=my_env,
+    # creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
 print("started")
 
 sleep(10)
