e8ffbf60d8c4c1475c50b0ec2fe2d05b06e5ca60
Feodor Fitsner <feodor@appveyor.com>
2022-03-30 20:48:27 -0700

    popen tests

--- sdk/python/examples/popen.py
+++ sdk/python/examples/popen.py
@@ -1,15 +1,29 @@
+import logging
 import os
 import subprocess
+from asyncio.log import logger
 from re import sub
 from time import sleep
+
+logging.basicConfig(level=logging.INFO)
+
+my_env = {**os.environ, "FLET_LOG_TO_FILE": "true"}
 
 port = 8570
 flet_path = "flet.exe"
 args = [flet_path, "server", "--attached", "--port", str(port)]
 
-subprocess.Popen(args, stdout=subprocess.DEVNULL)
+log_level = logger.getEffectiveLevel()
+if log_level == logging.CRITICAL:
+    log_level = logging.FATAL
+
+if log_level != logging.NOTSET:
+    log_level_name = logging.getLevelName(log_level).lower()
+    args.extend(["--log-level", log_level_name])
+
+subprocess.Popen(args, env=my_env, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 print("started")
 
-sleep(5)
+sleep(10)
 
 print("finished!")
