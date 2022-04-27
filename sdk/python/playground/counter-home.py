277dfcb884affe23dab6e9e1eeb109e85e3861c1
Feodor Fitsner <feodor@appveyor.com>
2022-04-26 19:55:54 -0700

    counter app updated

--- sdk/python/playground/counter-home.py
+++ sdk/python/playground/counter-home.py
@@ -1,14 +1,10 @@
-import logging
-
 import flet
 from flet import IconButton, Page, Row, TextField, icons
-from flet.progress_bar import ProgressBar
 
 
 def main(page: Page):
     page.title = "Flet counter example"
     page.vertical_alignment = "center"
-    page.splash = ProgressBar()
 
     txt_number = TextField(value="0", text_align="right", width=100)
 
@@ -16,7 +12,7 @@ def main(page: Page):
         txt_number.value = int(txt_number.value) - 1
         page.update()
 
-    def add_click(e):
+    def plus_click(e):
         txt_number.value = int(txt_number.value) + 1
         page.update()
 
@@ -25,11 +21,11 @@ def main(page: Page):
             [
                 IconButton(icons.REMOVE, on_click=minus_click),
                 txt_number,
-                IconButton(icons.ADD, on_click=add_click),
+                IconButton(icons.ADD, on_click=plus_click),
             ],
             alignment="center",
         )
     )
 
 
-flet.app(name="test1", port=8550, target=main, view=flet.WEB_BROWSER)
+flet.app(target=main)
