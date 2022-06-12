a47eca82112e22c5fa1a10224b83c5d91a34c4cb
Feodor Fitsner <feodor@appveyor.com>
2022-06-12 13:31:36 -0700

    Upgraded to Flutter 3.0.2

--- ci/install_flutter.ps1
+++ ci/install_flutter.ps1
@@ -1,7 +1,7 @@
 $distPath = "$env:TEMP\flutter_windows_stable.zip"
 
 Write-Host "Downloading Flutter SDK..."
-(New-Object Net.WebClient).DownloadFile("https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.0.1-stable.zip", $distPath)
+(New-Object Net.WebClient).DownloadFile("https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.0.2-stable.zip", $distPath)
 
 Write-Host "Unpacking Flutter SDK..."
 7z x $distPath -o"$env:SystemDrive\" | Out-Null
