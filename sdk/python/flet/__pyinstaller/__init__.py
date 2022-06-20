78be5fa32db192ef67f0a4ca5f4f559e84a9caf8
Feodor Fitsner <feodor@appveyor.com>
2022-06-20 12:54:03 -0700

    Test project.entry-points

---
+++ sdk/python/flet/__pyinstaller/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+
+def get_hook_dirs():
+    print(
+        "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+    )
+    raise Exception("dlkjdsjkfkjsdkjdhsfksjdf")
+    return [os.path.dirname(__file__)]
