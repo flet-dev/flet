ce4d44f800373dfb5fc5a7240cc65ece150edb87
Feodor Fitsner <feodor@appveyor.com>
2022-07-07 20:04:38 -0700

    Added `page.pwa` property to detect if an app is running as PWA (#58)
    * Loading animation as a separate icon
    
    * Added `page.pwa` to detect if app is running as PWA

---
+++ client/lib/utils/platform_utils_io.dart
@@ -0,0 +1,3 @@
+bool isProgressiveWebApp() {
+  return false;
+}
---
+++ client/lib/utils/platform_utils_js.dart
@@ -0,0 +1,7 @@
+import 'dart:html';
+
+bool isProgressiveWebApp() {
+  return window.matchMedia('(display-mode: standalone)').matches ||
+      window.matchMedia('(display-mode: fullscreen)').matches ||
+      window.matchMedia('(display-mode: minimal-ui)').matches;
+}
