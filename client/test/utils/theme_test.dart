82e05d22a14b698e3236b78bb8f9b4ab2c1f9628
Feodor Fitsner <feodor@appveyor.com>
2022-08-02 10:53:28 -0700

    Fix Flutter tests

--- client/test/utils/theme_test.dart
+++ client/test/utils/theme_test.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:flet_view/utils/theme.dart';
 import 'package:flutter/cupertino.dart';
@@ -7,8 +6,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test("Light theme is parsed correctly from JSON", () {
-    const t1 =
-        '''{
+    const t1 = '''{
         "color_scheme_seed": "red",
         "brightness": "light1",
         "use_material3": true
@@ -19,12 +17,11 @@ void main() {
 
     expect(theme.brightness, Brightness.light);
     expect(theme.useMaterial3, true);
-    expect(theme.primaryColor, const Color(0xffbc1714));
+    expect(theme.primaryColor, const Color(0xffbb1614));
   });
 
   test("Dark theme is parsed correctly from JSON", () {
-    const t1 =
-        '''{
+    const t1 = '''{
         "color_scheme_seed": "cyan",
         "brightness": "dark"
       }''';
