c84cb5777d55f7dbfebb09bc3be2b2a2332cfc32
Mikael Honkala <mikael.honkala@gmail.com>
2022-08-06 11:45:07 +0300

    Add slugify to utils

--- sdk/python/flet/utils.py
+++ sdk/python/flet/utils.py
@@ -1,6 +1,7 @@
 import os
 import platform
 import sys
+import unicodedata
 import webbrowser
 
 
@@ -82,3 +83,23 @@ def is_localhost_url(url):
 def get_current_script_dir():
     pathname = os.path.dirname(sys.argv[0])
     return os.path.abspath(pathname)
+
+
+def slugify(original: str) -> str:
+    """
+    Make a string url friendly. Useful for creating routes for navigation.
+
+    >>> slugify("What's    up?")
+    'whats-up'
+
+    >>> slugify("  Mitä kuuluu?  ")
+    'mitä-kuuluu'
+    """
+    slugified = original.strip()
+    slugified = " ".join(slugified.split())  # Remove extra spaces between words
+    slugified = slugified.lower()
+    # Remove unicode punctuation
+    slugified = "".join(character for character in slugified if not unicodedata.category(character).startswith("P"))
+    slugified = slugified.replace(" ", "-")
+
+    return slugified
