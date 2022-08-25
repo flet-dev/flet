706611df31ea470f9c612dec44d9f032e8fc49bd
Feodor Fitsner <feodor@appveyor.com>
2022-08-25 13:58:12 -0700

    Show window from dart [skip ci]

--- package/lib/src/utils.dart
+++ package/lib/src/utils.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -6,7 +7,25 @@ import 'utils/desktop.dart';
 Future setupDesktop() async {
   if (isDesktop()) {
     WidgetsFlutterBinding.ensureInitialized();
-    // Must add this line.
     await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = WindowOptions(
+      size: Size(800, 600),
+      center: true,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.hidden,
+    );
+
+    await windowManager.waitUntilReadyToShow(null, () async {
+      //await windowManager.show();
+      //await windowManager.focus();
+      Future.delayed(Duration(seconds: 5)).then((value) async {
+        await windowManager.setTitleBarStyle(TitleBarStyle.hidden,
+            windowButtonVisibility: false);
+        await windowManager.show();
+        await windowManager.focus();
+      });
+    });
   }
 }
