3af83d1fce2975fa22a8ed6cd0cfc4dbdf911bc1
Feodor Fitsner <feodor@appveyor.com>
2022-09-27 12:36:59 -0700

    renamed to secret_key

--- sdk/python/flet/security.py
+++ sdk/python/flet/security.py
@@ -7,28 +7,28 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
-def __generate_fernet_key(password: str) -> bytes:
-    key = hashlib.sha256(password.encode()).digest()
+def __generate_fernet_key(secret_key: str) -> bytes:
+    key = hashlib.sha256(secret_key.encode()).digest()
     return base64.urlsafe_b64encode(key)
 
 
-def __generate_fernet_key_kdf(password: str, salt: bytes) -> bytes:
+def __generate_fernet_key_kdf(secret_key: str, salt: bytes) -> bytes:
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000)
-    return base64.urlsafe_b64encode(kdf.derive(password.encode("utf-8")))
+    return base64.urlsafe_b64encode(kdf.derive(secret_key.encode("utf-8")))
 
 
-def encrypt(plain_text: str, password: str) -> str:
+def encrypt(plain_text: str, secret_key: str) -> str:
     salt = os.urandom(16)
-    key = __generate_fernet_key_kdf(password, salt)
+    key = __generate_fernet_key_kdf(secret_key, salt)
     f = Fernet(key)
     return base64.urlsafe_b64encode(
         salt + f.encrypt(plain_text.encode("utf-8"))
     ).decode()
 
 
-def decrypt(encrypted_data: str, password: str) -> str:
+def decrypt(encrypted_data: str, secret_key: str) -> str:
     encrypted_data_bytes = base64.urlsafe_b64decode(encrypted_data)
     salt = encrypted_data_bytes[:16]
-    key = __generate_fernet_key_kdf(password, salt)
+    key = __generate_fernet_key_kdf(secret_key, salt)
     f = Fernet(key)
     return f.decrypt(encrypted_data_bytes[16:]).decode("utf-8")
