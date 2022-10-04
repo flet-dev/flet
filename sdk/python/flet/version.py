d796180ba99481d79758959c8ac8e4fb0972e940
Andrew Phillips <skeledrew@gmail.com>
2022-10-04 12:33:13 -0500

    fix: ensure correct version is fetched in dev mode
    Because the version file isn't replaced when the repo is cloned, the version
    
    info may go out of date, which can cause issues for local dev.

--- sdk/python/flet/version.py
+++ sdk/python/flet/version.py
@@ -1,2 +1,21 @@
 # this file will be replaced by CI
-version = "0.1.57"
+
+import subprocess as sp
+
+
+version = "0.1.60"  # default to old version
+in_repo = sp.run(
+    ["git", "status"],
+    capture_output=True,
+    text=True
+).stdout.startswith("On branch ")
+
+if in_repo:
+    # NOTE: this may break if there is a tag name starting with
+    #         "v" that isn't a version number
+    version = sp.run(
+        ["git tag |grep '^v' |sort -V |tail -n 1 |sed 's/v//g'"],
+        capture_output=True,
+        shell=True,
+        text=True
+    ).stdout.strip()
