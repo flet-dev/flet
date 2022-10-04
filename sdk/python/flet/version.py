bca02389219f346d6f06c27e6abce6f4b78f94a7
Andrew Phillips <skeledrew@gmail.com>
2022-10-04 14:58:39 -0500

    enh: check if the version is set before update

--- sdk/python/flet/version.py
+++ sdk/python/flet/version.py
@@ -3,19 +3,27 @@
 import subprocess as sp
 
 
-version = "0.1.60"  # default to old version
-in_repo = sp.run(
-    ["git", "status"],
-    capture_output=True,
-    text=True
-).stdout.startswith("On branch ")
-
-if in_repo:
-    # NOTE: this may break if there is a tag name starting with
-    #         "v" that isn't a version number
-    version = sp.run(
-        ["git tag |grep '^v' |sort -V |tail -n 1 |sed 's/v//g'"],
+def update_version():
+    in_repo = sp.run(
+        ["git", "status"],
         capture_output=True,
-        shell=True,
         text=True
-    ).stdout.strip()
+    ).stdout.startswith("On branch ")
+
+    if in_repo:
+        # NOTE: this may break if there is a tag name starting with
+        #         "v" that isn't a version number
+        version = sp.run(
+            ["git tag |grep '^v' |sort -V |tail -n 1 |sed 's/v//g'"],
+            capture_output=True,
+            shell=True,
+            text=True
+        ).stdout.strip()
+
+    else:
+        version = "0.1.60"  # default to old version
+    return version
+
+
+if not globals().get("version", None):
+    version = update_version()
