6f58716dd1c346ccfc6ba3a99eb287ccead223a3
Andrew Phillips <skeledrew@gmail.com>
2022-10-04 15:10:59 -0500

    enh: modify parse method to support Windows

--- sdk/python/flet/version.py
+++ sdk/python/flet/version.py
@@ -1,24 +1,28 @@
 # this file will be replaced by CI
 
 import subprocess as sp
+from pkg_resources import parse_version
+
+from flet.utils import which
 
 
 def update_version():
-    in_repo = sp.run(
+    in_repo = which("git") and sp.run(
         ["git", "status"],
         capture_output=True,
-        text=True
+        text=True,
     ).stdout.startswith("On branch ")
 
     if in_repo:
         # NOTE: this may break if there is a tag name starting with
         #         "v" that isn't a version number
-        version = sp.run(
-            ["git tag |grep '^v' |sort -V |tail -n 1 |sed 's/v//g'"],
+        tags = sp.run(
+            ["git", "tag"],
             capture_output=True,
-            shell=True,
-            text=True
-        ).stdout.strip()
+            text=True,
+        ).stdout.splitlines()
+        versions = filter(lambda t: t.startswith("v"), tags)
+        version = sorted(versions, key=parse_version)[-1][1:]
 
     else:
         version = "0.1.60"  # default to old version
