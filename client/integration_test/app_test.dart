180499cf30049bbcae2caafba1b367f49f849447
Feodor Fitsner <feodor@appveyor.com>
2022-10-07 20:44:12 -0700

    Added a simple integration test - just for fun

---
+++ client/integration_test/app_test.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:flet_client/main.dart' as app;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  tearDown(() {
+    debugPrint("TEAR DOWN");
+  });
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      var dir = Directory.current.path;
+      debugPrint("Current dir: $dir");
+      app.main();
+      await tester.pumpAndSettle(const Duration(milliseconds: 100),
+          EnginePhase.sendSemanticsUpdate, const Duration(seconds: 20));
+
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+
+      // Finds the floating action button to tap on.
+      final Finder fab = find.byTooltip('Increment');
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
+    });
+  });
+}
