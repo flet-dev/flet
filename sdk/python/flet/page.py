10a2b61d51336430bdb44c86d7d177638694473e
Inesa Fitsner <inesa@appveyor.com>
2022-11-28 13:41:21 -0800

    added comments for page, container, tooltip

--- sdk/python/flet/page.py
+++ sdk/python/flet/page.py
@@ -49,6 +49,30 @@ except ImportError:
 
 
 class Page(Control):
+    """
+    Page is a container for `View` (https://flet.dev/docs/controls/view) controls.
+
+    A page instance and the root view are automatically created when a new user session started.
+
+    Example:
+
+    ```
+    import flet as ft
+
+
+    def main(page: ft.Page):
+        page.title = "New page"
+        page.add(ft.Text("Hello"))
+
+
+    ft.app(target=main)
+    ```
+
+    -----
+
+    Online docs: https://flet.dev/docs/controls/page
+    """
+
     def __init__(self, conn: Connection, session_id):
         Control.__init__(self)
 
