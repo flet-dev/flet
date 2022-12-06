a448d5209a0b0204874207b409c0c6adf2612040
InesaFitsner <inesa@appveyor.com>
2022-12-06 10:10:11 -0800

    Update icon_button.py

--- sdk/python/flet/icon_button.py
+++ sdk/python/flet/icon_button.py
@@ -16,6 +16,43 @@ from flet.types import (
 
 
 class IconButton(ConstrainedControl):
+    """
+    An icon button is a round button with an icon in the middle that reacts to touches by filling with color (ink).
+
+    Icon buttons are commonly used in the toolbars, but they can be used in many other places as well.
+    
+    Example:
+    ```
+    import flet as ft
+
+    def main(page: ft.Page):
+        page.title = "Icon buttons"
+        page.add(
+            ft.Row(
+                [
+                    ft.IconButton(
+                        icon=ft.icons.PAUSE_CIRCLE_FILLED_ROUNDED,
+                        icon_color="blue400",
+                        icon_size=20,
+                        tooltip="Pause record",
+                    ),
+                    ft.IconButton(
+                        icon=ft.icons.DELETE_FOREVER_ROUNDED,
+                        icon_color="pink600",
+                        icon_size=40,
+                        tooltip="Delete record",
+                    ),
+                ]
+            ),
+        )
+
+    ft.app(target=main)
+    ```
+
+    -----
+
+    Online docs: https://flet.dev/docs/controls/iconbutton
+    """
     def __init__(
         self,
         icon: Optional[str] = None,
