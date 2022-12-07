e86f58e1b06e30ef26e5760e1959504c61bd1d58
InesaFitsner <inesa@appveyor.com>
2022-12-07 11:00:02 -0800

    Update haptic_feedback.py

--- sdk/python/flet/haptic_feedback.py
+++ sdk/python/flet/haptic_feedback.py
@@ -7,6 +7,33 @@ from flet.ref import Ref
 
 
 class HapticFeedback(CallableControl):
+    """
+    Allows access to the haptic feedback interface on the device.
+
+    It is non-visual and should be added to `page.overlay` list.
+    
+    Example:
+    ```
+    import flet as ft
+
+    def main(page: ft.Page):
+        hf = ft.HapticFeedback()
+        page.overlay.append(hf)
+
+        page.add(
+            ft.ElevatedButton("Heavy impact", on_click=lambda _: hf.heavy_impact()),
+            ft.ElevatedButton("Medium impact", on_click=lambda _: hf.medium_impact()),
+            ft.ElevatedButton("Light impact", on_click=lambda _: hf.light_impact()),
+            ft.ElevatedButton("Vibrate", on_click=lambda _: hf.vibrate()),
+        )
+
+    ft.app(target=main)
+    ```
+
+    -----
+
+    Online docs: https://flet.dev/docs/controls/hapticfeedback
+    """
     def __init__(
         self,
         ref: Optional[Ref] = None,
