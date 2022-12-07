c00a607c5a8ae1755d2308fde37cfdb520222d6d
InesaFitsner <inesa@appveyor.com>
2022-12-07 09:47:45 -0800

    Update snack_bar.py

--- sdk/python/flet/snack_bar.py
+++ sdk/python/flet/snack_bar.py
@@ -7,6 +7,42 @@ from flet.ref import Ref
 
 
 class SnackBar(Control):
+    """
+    A lightweight message with an optional action which briefly displays at the bottom of the screen.
+    
+    Example:
+    ```
+    import flet as ft
+
+    class Data:
+        def __init__(self) -> None:
+            self.counter = 0
+
+    d = Data()
+
+    def main(page):
+
+        page.snack_bar = ft.SnackBar(
+            content=ft.Text("Hello, world!"),
+            action="Alright!",
+        )
+        page.snack_bar.open = True
+
+        def on_click(e):
+            page.snack_bar = ft.SnackBar(ft.Text(f"Hello {d.counter}"))
+            page.snack_bar.open = True
+            d.counter += 1
+            page.update()
+
+        page.add(ft.ElevatedButton("Open SnackBar", on_click=on_click))
+
+    ft.app(target=main)
+    ```
+
+    -----
+
+    Online docs: https://flet.dev/docs/controls/snackbar
+    """
     def __init__(
         self,
         content: Control,
