8d7f6ca8b4b5a9576a1a2a616e7aee9b3c072a55
Feodor Fitsner <feodor@appveyor.com>
2022-12-15 19:51:08 -0800

    Version package args

--- sdk/python/flet/cli/commands/package.py
+++ sdk/python/flet/cli/commands/package.py
@@ -31,11 +31,58 @@ class Command(BaseCommand):
             dest="name",
             help="name for the generated executable",
         )
+        parser.add_argument(
+            "--add-data",
+            dest="add_data",
+            help="additional non-binary files or folders to be added to the executable",
+        )
+        parser.add_argument(
+            "--product-name",
+            dest="product_name",
+            help="executable product name",
+        )
+        parser.add_argument(
+            "--file-description",
+            dest="file_description",
+            help="executable file description",
+        )
+        parser.add_argument(
+            "--product-version",
+            dest="product_version",
+            help="executable product version (any string)",
+        )
+        parser.add_argument(
+            "--file-version",
+            dest="file_version",
+            help="executable file version (n.n.n.n)",
+        )
+        parser.add_argument(
+            "--company-name",
+            dest="company_name",
+            help="executable companyname",
+        )
+        parser.add_argument(
+            "--copyright",
+            dest="copyright",
+            help="executable copyright",
+        )
 
     def handle(self, options: argparse.Namespace) -> None:
 
         try:
+            import pefile
             import PyInstaller.__main__
+            from PyInstaller.compat import win32api
+            from PyInstaller.utils.win32.versioninfo import (
+                FixedFileInfo,
+                StringFileInfo,
+                StringStruct,
+                StringTable,
+                VarFileInfo,
+                VarStruct,
+                VSVersionInfo,
+                decode,
+            )
 
             pyi_args = [options.script, "--noconsole", "--noconfirm"]
             if options.icon:
@@ -43,6 +90,8 @@ class Command(BaseCommand):
                 hook_config.icon_file = options.icon
             if options.name:
                 pyi_args.extend(["--name", options.name])
+            if options.add_data:
+                pyi_args.extend(["--add-data", options.add_data])
             PyInstaller.__main__.run(pyi_args)
         except ImportError:
             print("Please install PyInstaller module to use flet package command.")
