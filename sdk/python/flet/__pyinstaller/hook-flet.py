ac45cd9bcbc74011ffecf021171ccfd4d3c87286
Feodor Fitsner <feodor@appveyor.com>
2023-01-03 09:19:17 -0800

    `flet pack` CLI to create app bundles with custom icons and metadata (#770)
    * Trying to wrap PyInstaller
    
    * Version package args
    
    * Copy bin dir
    
    * Patch icon and version info
    
    * certifi==2022.12.7
    
    * Try removing certifi pin
    
    * Update main exec version info
    
    * Move Windows utils into a separate module
    
    * Update icon on macOS
    
    * Patching plist and signing app bundle
    
    * Remove icon name field
    
    * Rename Flet.app to a custom bundle name
    
    * Update CFBundleIdentifier
    
    * --onefile is always forced
    
    * Update pack.py
    
    * Update win_utils.py
    
    * Fix --name arg description

--- sdk/python/flet/__pyinstaller/hook-flet.py
+++ sdk/python/flet/__pyinstaller/hook-flet.py
@@ -1,10 +1,15 @@
 import os
 
-# package entire "bin" folder
-bin_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "bin"))
+import flet.__pyinstaller.config as hook_config
+from flet.__pyinstaller.utils import get_flet_bin_path
 
-# package "bin/fletd" only
-if os.getenv("PACKAGE_FLETD_ONLY"):
-    bin_path = os.path.join(bin_path, "fletd*")
+bin_path = hook_config.temp_bin_dir
+if not bin_path:
+    bin_path = get_flet_bin_path()
 
-datas = [(bin_path, "flet/bin")]
+if bin_path:
+    # package "bin/fletd" only
+    if os.getenv("PACKAGE_FLETD_ONLY"):
+        bin_path = os.path.join(bin_path, "fletd*")
+
+    datas = [(bin_path, "flet/bin")]
