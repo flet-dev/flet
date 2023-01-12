c4b9815e4485a77cc56c92e43ba61148edf0c669
Feodor Fitsner <feodor@appveyor.com>
2023-01-12 13:15:29 -0800

    Fix Studio build

--- studio/client/lib/main.dart
+++ studio/client/lib/main.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 
 void main() async {
   await setupDesktop();
-  runApp(const FletApp(pageUrl: "http://192.168.1.243:8550/"));
+  runApp(const FletApp(
+    pageUrl: "http://192.168.1.243:8550/",
+    assetsDir: "",
+  ));
 }
