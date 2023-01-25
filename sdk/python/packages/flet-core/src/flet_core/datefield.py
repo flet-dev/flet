7f61d8bbb7396afc1359beb4ae01427a158aea78
Adam Beret Manczuk <beret@hipisi.org.pl>
2023-01-25 16:30:39 +0000

    rewrite control as a picker

--- sdk/python/packages/flet-core/src/flet_core/datefield.py
+++ sdk/python/packages/flet-core/src/flet_core/datefield.py
@@ -78,7 +78,10 @@ class DatePickerEntryMode(Enum):
     INPUT_ONLY = "inputOnly"
 
 
-class DateField(FormFieldControl):
+DatePickerState = Literal["pickDate", "initState"]
+
+
+class DateField(Control):
     """
     A button lets the user select date on datepicker dialog.
 
@@ -107,66 +110,15 @@ class DateField(FormFieldControl):
     def __init__(
             self,
             ref: Optional[Ref] = None,
-            width: OptionalNumber = None,
-            height: OptionalNumber = None,
             expand: Union[None, bool, int] = None,
             col: Optional[ResponsiveNumber] = None,
             opacity: OptionalNumber = None,
-            rotate: RotateValue = None,
-            scale: ScaleValue = None,
-            offset: OffsetValue = None,
-            aspect_ratio: OptionalNumber = None,
-            animate_opacity: AnimationValue = None,
-            animate_size: AnimationValue = None,
-            animate_position: AnimationValue = None,
-            animate_rotation: AnimationValue = None,
-            animate_scale: AnimationValue = None,
-            animate_offset: AnimationValue = None,
-            on_animation_end=None,
             tooltip: Optional[str] = None,
             visible: Optional[bool] = None,
             disabled: Optional[bool] = None,
             data: Any = None,
-            #
-            # FormField specific
-            #
-            text_size: OptionalNumber = None,
+
             text_style: Optional[TextStyle] = None,
-            label: Optional[str] = None,
-            label_style: Optional[TextStyle] = None,
-            icon: Optional[str] = None,
-            border: Optional[InputBorder] = None,
-            color: Optional[str] = None,
-            bgcolor: Optional[str] = None,
-            border_radius: BorderRadiusValue = None,
-            border_width: OptionalNumber = None,
-            border_color: Optional[str] = None,
-            focused_color: Optional[str] = None,
-            focused_bgcolor: Optional[str] = None,
-            focused_border_width: OptionalNumber = None,
-            focused_border_color: Optional[str] = None,
-            content_padding: PaddingValue = None,
-            dense: Optional[bool] = None,
-            filled: Optional[bool] = None,
-            hint_text: Optional[str] = None,
-            hint_style: Optional[TextStyle] = None,
-            helper_text: Optional[str] = None,
-            helper_style: Optional[TextStyle] = None,
-            counter_text: Optional[str] = None,
-            counter_style: Optional[TextStyle] = None,
-            error_text: Optional[str] = None,
-            error_style: Optional[TextStyle] = None,
-            prefix: Optional[Control] = None,
-            prefix_icon: Optional[str] = None,
-            prefix_text: Optional[str] = None,
-            prefix_style: Optional[TextStyle] = None,
-            suffix: Optional[Control] = None,
-            suffix_icon: Optional[str] = None,
-            suffix_text: Optional[str] = None,
-            suffix_style: Optional[TextStyle] = None,
-            #
-            # DateField Specific
-            #
             value: Optional[datetime] = None,
             first_date: Optional[datetime] = None,
             last_date: Optional[datetime] = None,
@@ -175,78 +127,29 @@ class DateField(FormFieldControl):
             date_picker_entry_mode: Optional[DatePickerEntryMode] = DatePickerEntryMode.CALENDAR,
             read_only: Optional[bool] = None,
             autofocus: Optional[bool] = None,
-            button_style: Optional[ButtonStyle] = None,
+            # button_style: Optional[ButtonStyle] = None,
             locale: Optional[str] = None,
             help_text: Optional[str] = None,
             cancel_text: Optional[str] = None,
             confirm_text: Optional[str] = None,
+            # hint_text: Optional[str] = None,
             on_change=None,
             on_submit=None,
-            on_focus=None,
-            on_blur=None,
+            # on_focus=None,
+            # on_blur=None,
     ):
-        FormFieldControl.__init__(
+        Control.__init__(
             self,
             ref=ref,
-            width=width,
-            height=height,
             expand=expand,
             col=col,
             opacity=opacity,
-            rotate=rotate,
-            scale=scale,
-            offset=offset,
-            aspect_ratio=aspect_ratio,
-            animate_opacity=animate_opacity,
-            animate_size=animate_size,
-            animate_position=animate_position,
-            animate_rotation=animate_rotation,
-            animate_scale=animate_scale,
-            animate_offset=animate_offset,
-            on_animation_end=on_animation_end,
             tooltip=tooltip,
             visible=visible,
             disabled=disabled,
             data=data,
-            #
-            # FormField
-            #
-            text_size=text_size,
-            text_style=text_style,
-            label=label,
-            label_style=label_style,
-            icon=icon,
-            border=border,
-            color=color,
-            bgcolor=bgcolor,
-            border_radius=border_radius,
-            border_width=border_width,
-            border_color=border_color,
-            focused_color=focused_color,
-            focused_bgcolor=focused_bgcolor,
-            focused_border_width=focused_border_width,
-            focused_border_color=focused_border_color,
-            content_padding=content_padding,
-            dense=dense,
-            filled=filled,
-            hint_text=hint_text,
-            hint_style=hint_style,
-            helper_text=helper_text,
-            helper_style=helper_style,
-            counter_text=counter_text,
-            counter_style=counter_style,
-            error_text=error_text,
-            error_style=error_style,
-            prefix=prefix,
-            prefix_icon=prefix_icon,
-            prefix_text=prefix_text,
-            prefix_style=prefix_style,
-            suffix=suffix,
-            suffix_icon=suffix_icon,
-            suffix_text=suffix_text,
-            suffix_style=suffix_style,
         )
-        self.button_style = button_style
+        # self.button_style = button_style
         self.value = value
         self.first_date = first_date
         self.last_date = last_date
@@ -262,25 +165,42 @@ class DateField(FormFieldControl):
         self.autofocus = autofocus
         self.on_change = on_change
         self.on_submit = on_submit
-        self.on_focus = on_focus
-        self.on_blur = on_blur
+        # self.on_focus = on_focus
+        # self.on_blur = on_blur
 
     def _get_control_name(self):
         return "datefield"
 
     def _before_build_command(self):
         super()._before_build_command()
-        if self.bgcolor is not None and self.filled is None:
-            self.filled = True  # Flutter requires filled = True to display a bgcolor
-        self._set_attr_json("buttonStyle", self.__button_style)
+        # if self.bgcolor is not None and self.filled is None:
+        #     self.filled = True  # Flutter requires filled = True to display a bgcolor
+        # self._set_attr_json("buttonStyle", self.__button_style)
 
-    def focus(self):
-        self._set_attr_json("focus", FocusData())
+    # def focus(self):
+    #     self._set_attr_json("focus", FocusData())
+    #     self.update()
+    #
+    # async def focus_async(self):
+    #     self._set_attr_json("focus", FocusData())
+    #     await self.update_async()
+
+    def pick_date(self):
+        self.state = "pickDate"
         self.update()
 
-    async def focus_async(self):
-        self._set_attr_json("focus", FocusData())
+    async def pick_date_async(self):
+        self.state = "pickDate"
         await self.update_async()
+
+    # state
+    @property
+    def state(self) -> Optional[DatePickerState]:
+        return self._get_attr("state")
+
+    @state.setter
+    def state(self, value: Optional[DatePickerState]):
+        self._set_attr("state", value)
 
     # value
     @property
@@ -411,32 +331,32 @@ class DateField(FormFieldControl):
     def __set_date_picker_entry_mode(self, value: DatePickerEntryMode):
         self._set_attr("datePickerMode", value)
 
-    # read_only
-    @property
-    def read_only(self) -> Optional[bool]:
-        return self._get_attr("readOnly", data_type="bool", def_value=False)
+    # # read_only
+    # @property
+    # def read_only(self) -> Optional[bool]:
+    #     return self._get_attr("readOnly", data_type="bool", def_value=False)
+    #
+    # @read_only.setter
+    # def read_only(self, value: Optional[bool]):
+    #     self._set_attr("readOnly", value)
 
-    @read_only.setter
-    def read_only(self, value: Optional[bool]):
-        self._set_attr("readOnly", value)
+    # # autofocus
+    # @property
+    # def autofocus(self) -> Optional[bool]:
+    #     return self._get_attr("autofocus", data_type="bool", def_value=False)
+    #
+    # @autofocus.setter
+    # def autofocus(self, value: Optional[bool]):
+    #     self._set_attr("autofocus", value)
 
-    # autofocus
-    @property
-    def autofocus(self) -> Optional[bool]:
-        return self._get_attr("autofocus", data_type="bool", def_value=False)
-
-    @autofocus.setter
-    def autofocus(self, value: Optional[bool]):
-        self._set_attr("autofocus", value)
-
-    # code_style
-    @property
-    def button_style(self):
-        return self.__button_style
-
-    @button_style.setter
-    def button_style(self, value: Optional[ButtonStyle]):
-        self.__button_style = value
+    # # code_style
+    # @property
+    # def button_style(self):
+    #     return self.__button_style
+    #
+    # @button_style.setter
+    # def button_style(self, value: Optional[ButtonStyle]):
+    #     self.__button_style = value
 
     # on_change
     @property
@@ -460,20 +380,20 @@ class DateField(FormFieldControl):
     def on_submit(self, handler):
         self._add_event_handler("submit", handler)
 
-    # on_focus
-    @property
-    def on_focus(self):
-        return self._get_event_handler("focus")
-
-    @on_focus.setter
-    def on_focus(self, handler):
-        self._add_event_handler("focus", handler)
-
-    # on_blur
-    @property
-    def on_blur(self):
-        return self._get_event_handler("blur")
-
-    @on_blur.setter
-    def on_blur(self, handler):
-        self._add_event_handler("blur", handler)
+    # # on_focus
+    # @property
+    # def on_focus(self):
+    #     return self._get_event_handler("focus")
+    #
+    # @on_focus.setter
+    # def on_focus(self, handler):
+    #     self._add_event_handler("focus", handler)
+    #
+    # # on_blur
+    # @property
+    # def on_blur(self):
+    #     return self._get_event_handler("blur")
+    #
+    # @on_blur.setter
+    # def on_blur(self, handler):
+    #     self._add_event_handler("blur", handler)
