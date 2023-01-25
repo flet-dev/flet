ff3b9cfca73024077046b9477c8332879346eea0
Adam Beret Manczuk <beret@hipisi.org.pl>
2023-01-25 16:30:39 +0000

    rewrite control as a picker

--- package/lib/src/controls/datefield.dart
+++ package/lib/src/controls/datefield.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:intl/intl.dart';
 
 import '../actions.dart';
 import '../flet_app_services.dart';
 import '../models/app_state.dart';
 import '../models/control.dart';
 import '../protocol/update_control_props_payload.dart';
-import 'create_control.dart';
 import 'form_field.dart';
 
 class DateFieldControl extends StatefulWidget {
@@ -30,10 +28,16 @@ class DateFieldControl extends StatefulWidget {
 
 class _DateFieldControlState extends State<DateFieldControl> {
   DateTime? _value;
+  String? _state;
+
+  DateTime? get value {
+    return widget.control.attrDateTime("value");
+  }
 
   @override
   void initState() {
     super.initState();
+    _value = value;
   }
 
   @override
@@ -44,9 +48,7 @@ class _DateFieldControlState extends State<DateFieldControl> {
   @override
   Widget build(BuildContext context) {
     debugPrint("DateField build: ${widget.control.id}");
-
-    bool autofocus = widget.control.attrBool("autofocus", false)!;
-    bool disabled = widget.control.isDisabled || widget.parentDisabled;
+    String state = widget.control.attrString("state") ?? "initState";
     DateTime? firstDate = widget.control.attrDateTime("firstDate");
     DateTime? lastDate = widget.control.attrDateTime("firstDate");
     bool onChange = widget.control.attrBool("onChange", false)!;
@@ -62,9 +64,6 @@ class _DateFieldControlState extends State<DateFieldControl> {
         widget.control.attrString("datePickerEntryMode", "")!);
     String? hintText = widget.control.attrString("hintText");
 
-    // TODO: later
-    // ButtonStyle? buttonStyle = parseButtonStyle(Theme.of(context), widget.control, "buttonStyle");
-
     return StoreConnector<AppState, Function>(
         distinct: true,
         converter: (store) => store.dispatch,
@@ -79,14 +78,15 @@ class _DateFieldControlState extends State<DateFieldControl> {
           }
 
           void onChanged(DateTime? dateValue) {
-            DateTime? value = dateValue;
-            debugPrint(value.toString());
+            debugPrint("New date: $dateValue");
+            var newState = "initState";
             setState(() {
-              _value = value;
+              _value = dateValue;
+              _state = newState;
             });
-            String stringValue = value?.toIso8601String() ?? "null";
+            String stringValue = dateValue?.toIso8601String() ?? "null";
             List<Map<String, String>> props = [
-              {"i": widget.control.id, "value": stringValue}
+              {"i": widget.control.id, "value": stringValue, "state": newState}
             ];
             dispatch(UpdateControlPropsAction(
                 UpdateControlPropsPayload(props: props)));
@@ -103,65 +103,41 @@ class _DateFieldControlState extends State<DateFieldControl> {
                 eventData: "");
           }
 
-          DateTime? value = widget.control.attrDateTime("value");
-          if (_value != value) {
-            _value = value;
-          }
-
-          Future<void> selectDateDialog() async {
-            final DateTime? pickedDate = await showDatePicker(
-              context: context,
-              initialDate: _value ?? DateTime.now(),
+          Widget selectDateDialog() {
+            Widget dialog = DatePickerDialog(
+              initialDate: value ?? DateTime.now(),
               firstDate: firstDate ?? DateTime(1900),
               lastDate: lastDate ?? DateTime(2050),
-              locale: locale,
               helpText: helpText,
               cancelText: cancelText,
               confirmText: confirmText,
               keyboardType: keyboardType,
-              initialDatePickerMode: datePickerMode,
-              initialEntryMode: datePickerEntryMode,
-              fieldHintText: hintText,
             );
-            if (!disabled && pickedDate != null && pickedDate != value) {
-              onChanged(pickedDate);
+
+            dialog = Localizations.override(
+              context: context,
+              locale: locale,
+              child: dialog,
+            );
+
+            return dialog;
+          }
+
+          if (_state != state) {
+            switch (state) {
+              case "pickDate":
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  showDialog<DateTime>(
+                      context: context,
+                      builder: (context) => selectDateDialog()).then((result) {
+                    debugPrint("pickDate() completed");
+                    onChanged(result);
+                  });
+                });
+                break;
             }
           }
-
-          String buttonText;
-
-          if (value == null) {
-            buttonText = hintText ?? "";
-          } else {
-            buttonText = DateFormat.yMMMd(locale.toString()).format(value);
-          }
-
-          Widget button = ElevatedButton.icon(
-            onPressed: selectDateDialog,
-            autofocus: autofocus,
-            icon: const Icon(Icons.calendar_month),
-            label: Text(buttonText),
-          );
-
-          if (widget.control.attrInt("expand", 0)! > 0) {
-            return constrainedControl(
-                context, button, widget.parent, widget.control);
-          } else {
-            return LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                if (constraints.maxWidth == double.infinity &&
-                    widget.control.attrDouble("width") == null) {
-                  button = ConstrainedBox(
-                    constraints: const BoxConstraints.tightFor(width: 300),
-                    child: button,
-                  );
-                }
-
-                return constrainedControl(
-                    context, button, widget.parent, widget.control);
-              },
-            );
-          }
+          return const SizedBox.shrink();
         });
   }
 }
