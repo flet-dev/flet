dae9f62e0d105256cec572a8e9e6e6fb7234a353
Adam Beret Manczuk <beret@hipisi.org.pl>
2023-01-26 01:16:48 +0000

    add more tests

--- sdk/python/packages/flet-core/tests/test_date_picker.py
+++ sdk/python/packages/flet-core/tests/test_date_picker.py
@@ -19,10 +19,10 @@ def test_instance_no_attrs_set():
 
 
 # pick_date
-def test_pick_date():
+def test_value():
     r = ft.DatePicker()
     assert r._get_attr("state") == "initState"
-    r.update = lambda *_: None  # monkeypatch
+
     r.pick_date()
     r.state = "pickDate"
     assert r._get_attr("state") == "pickDate"
