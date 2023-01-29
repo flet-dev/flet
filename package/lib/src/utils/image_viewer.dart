f3d4a10f04d34e6ef4861433d06aca410c7622d8
Alan Bedian <skquark@yahoo.com>
2023-01-28 20:49:22 -0800

    Corrected way of getting imageProvider from string. Fixed syntax error with parameters.
    #963

--- package/lib/src/utils/image_viewer.dart
+++ package/lib/src/utils/image_viewer.dart
@@ -1,6 +1,10 @@
 import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import '../utils/colors.dart';
+import '../utils/images.dart';
+import '../models/app_state.dart';
+import '../models/page_args_model.dart';
 
 /// Shows the given [imageProvider] in a full-screen [Dialog].
 /// The optional [swipeDismissible] boolean defaults to false and allows swipe-down-to-dismiss.
@@ -11,32 +15,34 @@ import '../utils/colors.dart';
 /// Setting [immersive] to false will prevent the top and bottom bars from being hidden.
 /// The [initialIndex] is the image to start with in a MultiPage viewer.
 Future openImageViewer(String image, bool swipeDismissible, bool doubleTapZoomable, String? backgroundColor, String? closeButtonColor, String? closeButtonTooltip, bool? immersive, int? initialIndex) async {
-  List<String> imageList = [];
-  var imageProvider;
-  if (image.contains('|')) {
-    for (String i in image.split('|')) {
-      imageList.add(Image.network(i).image);
-    }
-    if (imageList.length > 1)
-      imageProvider = MultiImageProvider(imageList, initialIndex = initialIndex);
-    else
-      imageProvider = SingleImageProvider(Image.network(image).image);
-  } else
-    imageProvider = SingleImageProvider(Image.network(image).image);
-  return Builder(
-    builder: (context) {
+  return StoreConnector<AppState, PageArgsModel>(
+    distinct: true,
+    converter: (store) => PageArgsModel.fromStore(store),
+    builder: (context, pageArgs) {
+      List<String> imageList = [];
+      var imageProvider;
+      if (image.contains('|')) {
+        for (String imageSrc in image.split('|')) {
+          var assetSrc = getAssetSrc(imageSrc, pageArgs.pageUri!, pageArgs.assetsDir);
+          imageList.add(assetSrc.isFile ? getFileImageProvider(assetSrc.path) : NetworkImage(assetSrc.path));
+        }
+        imageProvider = MultiImageProvider(imageList, initialIndex: initialIndex);
+      } else {
+        var assetSrc = getAssetSrc(image, pageArgs.pageUri!, pageArgs.assetsDir);
+        imageProvider = SingleImageProvider(assetSrc.isFile ? getFileImageProvider(assetSrc.path) : NetworkImage(assetSrc.path));
+      }
       var background_color = checkString(backgroundColor) ? HexColor.fromString(Theme.of(context), backgroundColor!) : Colors.black;
       var close_button_color = checkString(closeButtonColor) ? HexColor.fromString(Theme.of(context), closeButtonColor!) : Colors.white;
       showImageViewerPager(
         context,
-        imageProvider,
-        swipeDismissible = swipeDismissible,
-        doubleTapZoomable = doubleTapZoomable,
-        backgroundColor = background_color,
-        closeButtonColor = close_button_color,
-        closeButtonTooltip = closeButtonTooltip,
-        immersive = immersive,
-        initialIndex = initialIndex,
+        imageProvider!,
+        swipeDismissible: swipeDismissible,
+        doubleTapZoomable: doubleTapZoomable,
+        backgroundColor: background_color,
+        closeButtonColor: close_button_color,
+        closeButtonTooltip: closeButtonTooltip,
+        immersive: immersive,
+        initialIndex: initialIndex,
       );
     },
   );
