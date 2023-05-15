22fe2122ea384b2822a3b07267a8b934a3ea069c
Feodor Fitsner <feodor@appveyor.com>
2023-05-14 18:43:02 -0700

    Flet studio icons updated

--- studio/client/lib/main.dart
+++ studio/client/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() async {
   await setupDesktop();
   runApp(const FletApp(
-    pageUrl: "http://192.168.1.243:8550/",
+    pageUrl: "http://192.168.1.79:8550",
     assetsDir: "",
   ));
 }
