c3a53bdc7e44d15eed7a965226b5fce861508279
vlod-github <103655830+Vlod-github@users.noreply.github.com>
2023-05-17 21:45:56 +0500

    fix BlendMode tests 2

--- sdk/python/packages/flet-core/tests/test_shader_mask.py
+++ sdk/python/packages/flet-core/tests/test_shader_mask.py
@@ -21,7 +21,7 @@ def test_blend_mode_enum():
     r = ft.ShaderMask(blend_mode=ft.BlendMode.LIGHTEN)
     assert isinstance(r.blend_mode, ft.BlendMode)
     assert isinstance(r._get_attr('blendMode'), str)
-    assert r.color_blend_mode == ft.BlendMode.LIGHTEN
+    assert r.blend_mode == ft.BlendMode.LIGHTEN
     assert r._get_attr("blendMode") == ft.BlendMode.LIGHTEN.value
     cmd = r._build_add_commands()
     assert cmd[0].attrs['blendmode'] == 'lighten'
@@ -31,7 +31,7 @@ def test_blend_mode_str():
     r = ft.ShaderMask(blend_mode='darken')
     assert isinstance(r.blend_mode, ft.BlendMode)
     assert isinstance(r._get_attr('blendMode'), str)
-    assert r.color_blend_mode == ft.BlendMode.DARKEN
+    assert r.blend_mode == ft.BlendMode.DARKEN
     assert r._get_attr("blendMode") == ft.BlendMode.DARKEN.value
     cmd = r._build_add_commands()
     assert cmd[0].attrs['blendmode'] == 'darken'
