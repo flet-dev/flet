e22fdb432a6f18c91fc1046ae79ef3e5c915faec
Feodor Fitsner <feodor@appveyor.com>
2023-05-23 20:38:55 -0700

    Run Python without wrapping widget

--- studio/client/lib/main.dart
+++ studio/client/lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
+
 import 'package:flet/flet.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:python_engine/python_engine.dart';
 
@@ -11,47 +12,17 @@ void main() async {
   var tmpDir = await getTemporaryDirectory();
   debugPrint("TEMP DIR IN DART: ${tmpDir.path}");
 
+  var appPath =
+      await extractZipArchive("assets/python-app/app.zip", "python-app");
+  debugPrint("PYTHON APP PATH: $appPath");
+  Directory.current = appPath;
+
+  var pythonEnginePlugin = PythonEngine();
+  pythonEnginePlugin.runPython(appPath, "main");
+
   await setupDesktop();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _pythonEnginePlugin = PythonEngine();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    var appPath =
-        await extractZipArchive("assets/python-app/app.zip", "python-app");
-    debugPrint("PYTHON APP PATH: $appPath");
-
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      await _pythonEnginePlugin.runPython(appPath, "main") ??
-          'Unknown run python result';
-    } on PlatformException catch (e) {
-      debugPrint('Failed to run Python: ${e.message}');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const FletApp(
-      pageUrl: "http://Feodors-MacBook-Pro.local:8550/",
-      assetsDir: "",
-    );
-  }
+  runApp(const FletApp(
+    pageUrl: "http://Feodors-MacBook-Pro.local:8550/",
+    assetsDir: "",
+  ));
 }
