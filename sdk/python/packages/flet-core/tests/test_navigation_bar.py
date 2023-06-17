e44977dc6255eecfb646ef952d989d8ad6e2bcb0
vlod-github <103655830+Vlod-github@users.noreply.github.com>
2023-06-17 12:43:06 +0500

    fix NavigationBarLabelBehavior test

--- sdk/python/packages/flet-core/tests/test_navigation_bar.py
+++ sdk/python/packages/flet-core/tests/test_navigation_bar.py
@@ -19,14 +19,20 @@ def test_instance_no_attrs_set():
 
 def test_extension_set_enum():
     r = ft.NavigationBar()
-    assert r.label_behavior is None
+    assert isinstance(r.label_behavior, ft.NavigationBarLabelBehavior)
+    assert r.extension_set == ft.NavigationBarLabelBehavior.ALWAYS_SHOW
     assert r._get_attr("labelType") is None
 
-    r = ft.NavigationBar(label_behavior=ft.NavigationBarLabelBehavior.ALWAYS_SHOW)
+    r = ft.NavigationBar(label_behavior=ft.NavigationBarLabelBehavior.ONLY_SHOW_SELECTED)
     assert isinstance(r.label_behavior, ft.NavigationBarLabelBehavior)
-    assert r.label_behavior == ft.NavigationBarLabelBehavior.ALWAYS_SHOW
-    assert r._get_attr("labelType") == "alwaysShow"
+    assert isinstance(r._get_attr("labelType"), str)
+    assert r.label_behavior == ft.NavigationBarLabelBehavior.ONLY_SHOW_SELECTED
+    assert r._get_attr("labelType") == ft.NavigationBarLabelBehavior.ONLY_SHOW_SELECTED.value
+    assert r._get_attr("labelType") == "onlyShowSelected"
 
     r = ft.NavigationBar(label_behavior="alwaysHide")
-    assert isinstance(r.label_behavior, str)
+    assert isinstance(r.label_behavior, ft.NavigationBarLabelBehavior)
+    assert isinstance(r._get_attr("labelType"), str)
+    assert r.label_behavior == ft.NavigationBarLabelBehavior.ALWAYS_HIDE
+    assert r._get_attr("labelType") == ft.NavigationBarLabelBehavior.ALWAYS_HIDE.value
     assert r._get_attr("labelType") == "alwaysHide"
