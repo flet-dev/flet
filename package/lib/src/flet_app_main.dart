0d96c740807b2793ad60018bf69651390778872b
Feodor Fitsner <feodor@appveyor.com>
2023-06-19 18:58:16 -0700

    Overlayed loading page

--- package/lib/src/flet_app_main.dart
+++ package/lib/src/flet_app_main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'controls/create_control.dart';
 import 'flet_app_services.dart';
 import 'models/app_state.dart';
-import 'models/page_view_model.dart';
 
 class FletAppMain extends StatelessWidget {
   final String title;
@@ -18,35 +17,7 @@ class FletAppMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreProvider<AppState>(
       store: FletAppServices.of(context).store,
-      child: StoreConnector<AppState, PageViewModel>(
-        distinct: true,
-        converter: (store) => PageViewModel.fromStore(store),
-        builder: (context, viewModel) {
-          if (viewModel.error != "" && !viewModel.isLoading) {
-            return MaterialApp(
-                title: title,
-                home: Scaffold(
-                  body: Container(
-                      padding: const EdgeInsets.all(30),
-                      alignment: Alignment.center,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Icon(Icons.error,
-                              color: Colors.redAccent, size: 30),
-                          const SizedBox(height: 8),
-                          Flexible(
-                              child: Text(viewModel.error,
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(color: Colors.red)))
-                        ],
-                      )),
-                ));
-          } else {
-            return createControl(null, "page", false);
-          }
-        },
-      ),
+      child: createControl(null, "page", false),
     );
   }
 }
--- package/lib/src/widgets/loading_page.dart
+++ package/lib/src/widgets/loading_page.dart
@@ -1,35 +1,70 @@
 import 'package:flutter/material.dart';
 
-import 'page_media.dart';
-
 class LoadingPage extends StatelessWidget {
-  final String title;
+  final bool isLoading;
   final String message;
 
-  const LoadingPage({Key? key, required this.title, required this.message})
+  const LoadingPage({Key? key, required this.isLoading, required this.message})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("LoadingPage build");
-    List<Widget> children = [const CircularProgressIndicator()];
-    if (message != "") {
-      children.addAll([
-        const SizedBox(height: 10),
-        Text(
-          message,
-          style: Theme.of(context).textTheme.bodySmall,
+    Widget? child;
+
+    var theme = Theme.of(context);
+
+    if (isLoading) {
+      List<Widget> children = [
+        const SizedBox(
+          width: 30,
+          height: 30,
+          child: CircularProgressIndicator(
+            strokeWidth: 3,
+          ),
         )
-      ]);
-    }
-    return Scaffold(
-        body: Stack(children: [
-      Center(
-          child: Column(
+      ];
+      if (message != "") {
+        children.addAll([
+          const SizedBox(height: 8),
+          Text(
+            message,
+            style: Theme.of(context).textTheme.bodySmall,
+          )
+        ]);
+      }
+      child = Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: children,
-      )),
-      const PageMedia()
-    ]));
+      );
+    } else if (message != "") {
+      child = Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: theme.colorScheme.errorContainer,
+                borderRadius: BorderRadius.circular(8)),
+            child: Row(children: [
+              Icon(Icons.error_outline,
+                  color: theme.colorScheme.onErrorContainer, size: 30),
+              const SizedBox(width: 8),
+              Text(
+                message,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(color: theme.colorScheme.onErrorContainer),
+              )
+            ]),
+          )
+        ],
+      );
+    }
+
+    return Container(
+        alignment: Alignment.center,
+        color: theme.colorScheme.surface.withOpacity(0.7),
+        child: child);
   }
 }
