8f9a4d02a07fec1ab80c9b0c81ee7e83a9dc2959
CPullr <123842525+CPullr@users.noreply.github.com>
2023-07-11 23:00:54 +0300

    Added Support for aes-256 gcm encryption

--- sdk/python/packages/flet/src/flet/security.py
+++ sdk/python/packages/flet/src/flet/security.py
@@ -6,6 +6,7 @@ try:
     from cryptography.fernet import Fernet
     from cryptography.hazmat.primitives import hashes
     from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 except ImportError:
     raise Exception('Install "cryptography" Python package to use Flet security utils.')
 
@@ -35,3 +36,21 @@ def decrypt(encrypted_data: str, secret_key: str) -> str:
     key = __generate_fernet_key_kdf(secret_key, salt)
     f = Fernet(key)
     return f.decrypt(encrypted_data_bytes[16:]).decode("utf-8")
+
+def encrypt_aes_gcm_256(plain_text:str,secret_key:str) -> str:
+    nonce = os.urandom(32)
+    salt = os.urandom(16)
+    key = __generate_fernet_key_kdf(secret_key,salt)
+    key = hashlib.sha256(key).hexdigest()
+    ag = AESGCM(key[:32].encode())
+    return base64.urlsafe_b64encode(salt + nonce + ag.encrypt(nonce,plain_text.encode("utf-8"),None)).decode("utf-8")
+
+def decrypt_aes_gcm_256(encrypted_data:str,secret_key:str) -> str:
+    ciphertext_data_in_bytes = base64.urlsafe_b64decode(encrypted_data)
+    salt = ciphertext_data_in_bytes[:16]
+    key = __generate_fernet_key_kdf(secret_key,salt)
+    key = hashlib.sha256(key).hexdigest()
+    ag = AESGCM(key[:32].encode())
+    return ag.decrypt(ciphertext_data_in_bytes[16:48],ciphertext_data_in_bytes[48:],None).decode("utf-8")
+
+
