d9eec3bc5e68ede7c7368d318d33bbe381065237
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-09-23 14:44:42 +0300

    update `MobileWebViewer` with document

--- sdk/python/packages/flet-core/src/flet_core/mobilewebviewer.py
+++ sdk/python/packages/flet-core/src/flet_core/mobilewebviewer.py
@@ -72,7 +72,50 @@ class TextThemeStyle(Enum):
 
 class MobileWebViewer(ConstrainedControl):
     """
-    To be documented
+Easily load webpages while allowing user interaction.
+
+The `MobileWebViewer` control is designed exclusively for iOS and Android platforms. To use a webview on desktop or in a browser, consider utilizing the `DesktopWebViewer` control.
+
+## Examples
+A simple webview implementation using this class could be like:
+```python
+import flet, time
+
+def main (page:flet.Page):
+    wv = flet.MobileWebViewer("https://flet.dev", width=400, height=650)
+    page.add(wv)
+
+flet.app(target=main, view=flet.AppView.WEB_BROWSER, port=8550)
+```
+
+
+## Properties
+### `url`
+Start the webview by loading the `url` value.
+
+### `width` & `height`
+The width and height of the webview.
+
+### `javascript_enabled`
+Enable or disable the javascript execution of the page. Note that disabling the javascript execution of the page may result unexpected webpage behaviour.
+
+### `prevent_link`
+Specify a link to prevent it from downloading.
+
+### `bgcolor`
+Set the background color of the webview.
+
+## Events
+### `on_page_started`
+Fires soon as the first loading process of the webpage is started.
+
+### `on_page_ended`
+Fires when all the webpage loading processes are ended.
+
+### `on_web_resource_error`
+Fires when there is error with loading a webpage resource.
+
+View docs in github: [MobileWebViewer in github](https://flet.dev/docs/controls/mobilewebviewer)
     """
 
     def __init__(
