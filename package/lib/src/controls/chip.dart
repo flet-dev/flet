08607f8fdd7d2ef1d13b462eba2c7f4f6417a8e3
InesaFitsner <inesa@appveyor.com>
2023-10-06 13:19:54 -0700

    added Chip with label property

---
+++ package/lib/src/controls/chip.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../models/control.dart';
+import '../utils/colors.dart';
+import '../utils/edge_insets.dart';
+import 'create_control.dart';
+
+class ChipControl extends StatelessWidget {
+  final Control? parent;
+  final Control control;
+  //final List<Control> children;
+  final bool parentDisabled;
+
+  const ChipControl(
+      {Key? key,
+      this.parent,
+      required this.control,
+      //required this.children,
+      required this.parentDisabled})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("Chip build: ${control.id}");
+
+    // var contentCtrls =
+    //     children.where((c) => c.name == "content" && c.isVisible);
+    bool disabled = control.isDisabled || parentDisabled;
+
+    return constrainedControl(
+        context,
+        Chip(
+          label: Text(control.attrString("label", "")!),
+        ),
+        //elevation: control.attrDouble("elevation"),
+        //margin: parseEdgeInsets(control, "margin"),
+        //color: HexColor.fromString(
+        //     Theme.of(context), control.attrString("color", "")!),
+        // shadowColor: HexColor.fromString(
+        //     Theme.of(context), control.attrString("shadowColor", "")!),
+        // surfaceTintColor: HexColor.fromString(
+        //     Theme.of(context), control.attrString("surfaceTintColor", "")!),
+        // child: contentCtrls.isNotEmpty
+        //     ? createControl(control, contentCtrls.first.id, disabled)
+        //     : null),
+        parent,
+        control);
+  }
+}
---
+++ sdk/python/packages/flet-core/src/flet_core/chip.py
@@ -0,0 +1,145 @@
+from typing import Any, Optional, Union
+
+from flet_core.constrained_control import ConstrainedControl
+from flet_core.control import Control, OptionalNumber
+from flet_core.ref import Ref
+from flet_core.types import (
+    AnimationValue,
+    MarginValue,
+    OffsetValue,
+    ResponsiveNumber,
+    RotateValue,
+    ScaleValue,
+)
+
+
+class Chip(ConstrainedControl):
+    """
+    Chips are compact elements that represent an attribute, text, entity, or action.
+
+    Example:
+    ```
+    import flet as ft
+
+    # Example is under construction
+    ```
+
+    -----
+
+    Online docs: https://flet.dev/docs/controls/chip
+    """
+
+    def __init__(
+        self,
+        #content: Optional[Control] = None,
+        ref: Optional[Ref] = None,
+        width: OptionalNumber = None,
+        height: OptionalNumber = None,
+        left: OptionalNumber = None,
+        top: OptionalNumber = None,
+        right: OptionalNumber = None,
+        bottom: OptionalNumber = None,
+        expand: Union[None, bool, int] = None,
+        col: Optional[ResponsiveNumber] = None,
+        opacity: OptionalNumber = None,
+        rotate: RotateValue = None,
+        scale: ScaleValue = None,
+        offset: OffsetValue = None,
+        aspect_ratio: OptionalNumber = None,
+        animate_opacity: AnimationValue = None,
+        animate_size: AnimationValue = None,
+        animate_position: AnimationValue = None,
+        animate_rotation: AnimationValue = None,
+        animate_scale: AnimationValue = None,
+        animate_offset: AnimationValue = None,
+        on_animation_end=None,
+        tooltip: Optional[str] = None,
+        visible: Optional[bool] = None,
+        disabled: Optional[bool] = None,
+        data: Any = None,
+        key: Optional[str] = None,
+        #
+        # Specific
+        #
+
+        # margin: MarginValue = None,
+        # elevation: OptionalNumber = None,
+        # color: Optional[str] = None,
+        # shadow_color: Optional[str] = None,
+        # surface_tint_color: Optional[str] = None,
+        label: Optional[str] = None,
+    ):
+        ConstrainedControl.__init__(
+            self,
+            ref=ref,
+            key=key,
+            width=width,
+            height=height,
+            left=left,
+            top=top,
+            right=right,
+            bottom=bottom,
+            expand=expand,
+            col=col,
+            opacity=opacity,
+            rotate=rotate,
+            scale=scale,
+            offset=offset,
+            aspect_ratio=aspect_ratio,
+            animate_opacity=animate_opacity,
+            animate_size=animate_size,
+            animate_position=animate_position,
+            animate_rotation=animate_rotation,
+            animate_scale=animate_scale,
+            animate_offset=animate_offset,
+            on_animation_end=on_animation_end,
+            tooltip=tooltip,
+            visible=visible,
+            disabled=disabled,
+            data=data,
+        )
+
+        #self.content = content
+        self.label = label
+
+    def _get_control_name(self):
+        return "chip"
+
+    # def _before_build_command(self):
+    #     super()._before_build_command()
+    #     self._set_attr_json("margin", self.__margin)
+
+    # def _get_children(self):
+    #     children = []
+    #     if self.__content is not None:
+    #         self.__content._set_attr_internal("n", "content")
+    #         children.append(self.__content)
+    #     return children
+
+    # # margin
+    # @property
+    # def margin(self) -> MarginValue:
+    #     return self.__margin
+
+    # @margin.setter
+    # def margin(self, value: MarginValue):
+    #     self.__margin = value
+
+
+    # label
+    @property
+    def label(self):
+        return self._get_attr("label")
+
+    @label.setter
+    def label(self, value):
+        self._set_attr("label", value)
+
+    # # content
+    # @property
+    # def content(self) -> Optional[Control]:
+    #     return self.__content
+
+    # @content.setter
+    # def content(self, value: Optional[Control]):
+    #     self.__content = value
