743601b93875bcc77f32928f05d3f801812a94b9
InesaFitsner <inesa@appveyor.com>
2023-11-08 12:07:36 -0800

    Fix issue #2047

--- sdk/python/packages/flet-core/src/flet_core/chip.py
+++ sdk/python/packages/flet-core/src/flet_core/chip.py
@@ -23,19 +23,19 @@ class Chip(ConstrainedControl):
     Example:
     ```
     import flet as ft
-    
-    
+
+
     def main(page: ft.Page):
         def save_to_favorites_clicked(e):
             e.control.label.value = "Saved to favorites"
             e.control.leading = ft.Icon(ft.icons.FAVORITE_OUTLINED)
             e.control.disabled = True
             page.update()
-    
+
         def open_google_maps(e):
             page.launch_url("https://maps.google.com")
             page.update()
-    
+
         save_to_favourites = ft.Chip(
             label=ft.Text("Save to favourites"),
             leading=ft.Icon(ft.icons.FAVORITE_BORDER_OUTLINED),
@@ -44,16 +44,16 @@ class Chip(ConstrainedControl):
             autofocus=True,
             on_click=save_to_favorites_clicked,
         )
-    
+
         open_in_maps = ft.Chip(
             label=ft.Text("9 min walk"),
             leading=ft.Icon(ft.icons.MAP_SHARP),
             bgcolor=ft.colors.GREEN_200,
             on_click=open_google_maps,
         )
-    
+
         page.add(ft.Row([save_to_favourites, open_in_maps]))
-    
+
     ft.app(target=main)
     ```
 
@@ -208,7 +208,7 @@ class Chip(ConstrainedControl):
     # selected
     @property
     def selected(self) -> Optional[bool]:
-        return self._get_attr("selected")
+        return self._get_attr("selected", data_type="bool", def_value=False)
 
     @selected.setter
     def selected(self, value: Optional[bool]):
