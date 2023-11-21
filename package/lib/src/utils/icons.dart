3b829ae330a09913961656e1e674a0df98979bf5
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2023-11-21 20:09:25 +0100

    add `thumb_icon` to `Switch` (#2116)

--- package/lib/src/utils/icons.dart
+++ package/lib/src/utils/icons.dart
@@ -1,7 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+import '../models/control.dart';
 import 'material_icons.dart';
+import 'material_state.dart';
 
 IconData? getMaterialIcon(String iconName) {
   return materialIcons[iconName.toLowerCase()];
+}
+
+MaterialStateProperty<Icon?>? parseMaterialStateIcon(
+    ThemeData theme, Control control, String propName) {
+  var v = control.attrString(propName, null);
+  if (v == null) {
+    return null;
+  }
+
+  final j1 = json.decode(v);
+
+  return getMaterialStateProperty<Icon?>(
+      j1, (jv) => Icon(getMaterialIcon(jv as String)), null);
 }
