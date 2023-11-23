65b4a1ab357749518f5b85dd1f3999fb602a508b
InesaFitsner <inesa@appveyor.com>
2023-11-23 10:40:20 -0800

    refactor

--- sdk/python/packages/flet-core/src/flet_core/time_picker.py
+++ sdk/python/packages/flet-core/src/flet_core/time_picker.py
@@ -4,8 +4,7 @@ from typing import Any, Optional, Union
 
 from flet_core.control import Control, OptionalNumber
 from flet_core.ref import Ref
-from flet_core.text_style import TextStyle
-from flet_core.textfield import KeyboardType, KeyboardTypeString
+
 from flet_core.types import ResponsiveNumber
 
 
@@ -22,7 +21,7 @@ class TimePicker(Control):
 
     It is added to [`page.overlay`](page#overlay) and called using its `pick_time()` method.
 
-    Depending on the `time_picker_mode`, it will show either a Dial or an Input (TextField) for picking a time.
+    Depending on the `time_picker_mode`, it will show either a Dial or an Input (hour and minute text fields) for picking a time.
 
             Example:
             ```
@@ -45,20 +44,13 @@ class TimePicker(Control):
         data: Any = None,
         open: bool = False,
         value: Optional[time] = None,
-        text_style: Optional[TextStyle] = None,
-        keyboard_type: Optional[KeyboardType] = None,
         time_picker_entry_mode: Optional[TimePickerEntryMode] = None,
         hour_label_text: Optional[str] = None,
         minute_label_text: Optional[str] = None,
         help_text: Optional[str] = None,
         cancel_text: Optional[str] = None,
         confirm_text: Optional[str] = None,
-        error_format_text: Optional[str] = None,
         error_invalid_text: Optional[str] = None,
-        field_hint_text: Optional[str] = None,
-        field_label_text: Optional[str] = None,
-        switch_to_calendar_icon: Optional[str] = None,
-        switch_to_input_icon: Optional[str] = None,
         on_change=None,
         on_dismiss=None,
     ):
@@ -74,29 +66,19 @@ class TimePicker(Control):
             data=data,
         )
         self.value = value
-        self.keyboard_type = keyboard_type
         self.help_text = help_text
         self.cancel_text = cancel_text
         self.confirm_text = confirm_text
-        self.error_format_text = error_format_text
         self.error_invalid_text = error_invalid_text
         self.hour_label_text = hour_label_text
         self.minute_label_text = minute_label_text
         self.time_picker_entry_mode = time_picker_entry_mode
-        self.text_style = text_style
-        self.field_hint_text = field_hint_text
-        self.field_label_text = field_label_text
-        self.switch_to_calendar_icon = switch_to_calendar_icon
-        self.switch_to_input_icon = switch_to_input_icon
         self.on_change = on_change
         self.on_dismiss = on_dismiss
         self.open = open
 
     def _get_control_name(self):
         return "timepicker"
-
-    def _before_build_command(self):
-        super()._before_build_command()
 
     def pick_time(self):
         self.open = True
@@ -179,15 +161,6 @@ class TimePicker(Control):
     def confirm_text(self, value: Optional[str]):
         self._set_attr("confirmText", value)
 
-    # error_format_text
-    @property
-    def error_format_text(self) -> Optional[str]:
-        return self._get_attr("errorFormatText", def_value=None)
-
-    @error_format_text.setter
-    def error_format_text(self, value: Optional[str]):
-        self._set_attr("errorFormatText", value)
-
     # error_invalid_text
     @property
     def error_invalid_text(self) -> Optional[str]:
@@ -208,24 +181,6 @@ class TimePicker(Control):
         self._set_attr(
             "timePickerEntryMode", value.value if value is not None else None
         )
-
-    # switch_to_calendar_icon
-    @property
-    def switch_to_calendar_icon(self):
-        return self._get_attr("switchToCalendarEntryModeIcon")
-
-    @switch_to_calendar_icon.setter
-    def switch_to_calendar_icon(self, value):
-        self._set_attr("switchToCalendarEntryModeIcon", value)
-
-    # switch_to_input_icon
-    @property
-    def switch_to_input_icon(self):
-        return self._get_attr("switchToInputEntryModeIcon")
-
-    @switch_to_input_icon.setter
-    def switch_to_input_icon(self, value):
-        self._set_attr("switchToInputEntryModeIcon", value)
 
     # on_change
     @property
