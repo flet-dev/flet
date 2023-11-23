b36f2efeb4d447f7595060205c2f9e4cec8b1207
InesaFitsner <inesa@appveyor.com>
2023-11-23 11:23:15 -0800

    removed commented line

--- package/lib/src/models/control.dart
+++ package/lib/src/models/control.dart
@@ -1,6 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter/src/material/time.dart';
 
 class Control extends Equatable {
   static const reservedProps = ['i', 'p', 't', 'c', 'n'];
