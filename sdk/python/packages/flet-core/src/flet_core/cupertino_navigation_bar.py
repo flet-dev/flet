8efe1831ffee790ceb80cdfd8721b1d01f374f81
ndonkoHenri <robotcoder4@protonmail.com>
2023-12-17 13:19:28 +0100

    add docstring

--- sdk/python/packages/flet-core/src/flet_core/cupertino_navigation_bar.py
+++ sdk/python/packages/flet-core/src/flet_core/cupertino_navigation_bar.py
@@ -20,6 +20,38 @@ except ImportError:
 
 class CupertinoNavigationBar(ConstrainedControl):
     """
+    An iOS-styled bottom navigation tab bar.
+
+    Navigation bars offer a persistent and convenient way to switch between primary destinations in an app.
+
+    Example:
+
+    ```
+    import flet as ft
+
+    def main(page: ft.Page):
+        page.title = "CupertinoNavigationBar Example"
+        page.navigation_bar = ft.CupertinoNavigationBar(
+            bgcolor=ft.colors.AMBER_100,
+            inactive_color=ft.colors.GREY,
+            active_color=ft.colors.BLACK,
+            height=100,
+            on_change=lambda e: print("Selected tab:", e.control.selected_index),
+            destinations=[
+                ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Explore"),
+                ft.NavigationDestination(icon=ft.icons.COMMUTE, label="Commute"),
+                ft.NavigationDestination(
+                    icon=ft.icons.BOOKMARK_BORDER,
+                    selected_icon=ft.icons.BOOKMARK,
+                    label="Explore",
+                ),
+            ]
+        )
+        page.add(ft.SafeArea(ft.Text("Body!")))
+
+
+    ft.app(target=main)
+    ```
 
     -----
 
