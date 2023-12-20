e8519832aa0cd141121f62cccee71401b5616986
ndonkoHenri <robotcoder4@protonmail.com>
2023-12-20 17:04:46 +0100

    add .dart

---
+++ package/lib/src/controls/submenu_button.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+import '../flet_app_services.dart';
+import '../models/control.dart';
+import '../utils/buttons.dart';
+import '../utils/menu.dart';
+import 'create_control.dart';
+
+class SubMenuButtonControl extends StatefulWidget {
+  final Control? parent;
+  final Control control;
+  final List<Control> children;
+  final bool parentDisabled;
+
+  const SubMenuButtonControl(
+      {super.key,
+      this.parent,
+      required this.control,
+      required this.children,
+      required this.parentDisabled});
+
+  @override
+  State<SubMenuButtonControl> createState() => _SubMenuButtonControlState();
+}
+
+class _SubMenuButtonControlState extends State<SubMenuButtonControl> {
+  late final FocusNode _focusNode;
+  String? _lastFocusValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_onFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    FletAppServices.of(context).server.sendPageEvent(
+        eventTarget: widget.control.id,
+        eventName: _focusNode.hasFocus ? "focus" : "blur",
+        eventData: "");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("SubMenuButton build: ${widget.control.id}");
+    bool disabled = widget.control.isDisabled || widget.parentDisabled;
+
+    var content =
+        widget.children.where((c) => c.name == "content" && c.isVisible);
+    var ctrls =
+        widget.children.where((c) => c.name == "controls" && c.isVisible);
+    var leading =
+        widget.children.where((c) => c.name == "leading" && c.isVisible);
+    var trailing =
+        widget.children.where((c) => c.name == "trailing" && c.isVisible);
+
+    var clipBehavior = Clip.values.firstWhere(
+        (e) =>
+            e.name.toLowerCase() ==
+            widget.control.attrString("clipBehavior", "")!.toLowerCase(),
+        orElse: () => Clip.hardEdge);
+
+    var theme = Theme.of(context);
+    var style = parseButtonStyle(Theme.of(context), widget.control, "style",
+        defaultForegroundColor: theme.colorScheme.primary,
+        defaultBackgroundColor: Colors.transparent,
+        defaultOverlayColor: Colors.transparent,
+        defaultShadowColor: Colors.transparent,
+        defaultSurfaceTintColor: Colors.transparent,
+        defaultElevation: 0,
+        defaultPadding: const EdgeInsets.all(8),
+        defaultBorderSide: BorderSide.none,
+        defaultShape: theme.useMaterial3
+            ? const StadiumBorder()
+            : RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)));
+
+    var menuStyle =
+        parseMenuStyle(Theme.of(context), widget.control, "menuStyle");
+
+    bool onOpen = widget.control.attrBool("onOpen", false)!;
+    bool onClose = widget.control.attrBool("onClose", false)!;
+    bool onHover = widget.control.attrBool("onHover", false)!;
+
+    var server = FletAppServices.of(context).server;
+
+    var subMenu = SubmenuButton(
+      focusNode: _focusNode,
+      clipBehavior: clipBehavior,
+      style: style,
+      menuStyle: menuStyle,
+      onClose: onClose && !disabled
+          ? () {
+              server.sendPageEvent(
+                  eventTarget: widget.control.id,
+                  eventName: "close",
+                  eventData: "");
+            }
+          : null,
+      onHover: onHover && !disabled
+          ? (bool value) {
+              server.sendPageEvent(
+                  eventTarget: widget.control.id,
+                  eventName: "hover",
+                  eventData: "$value");
+            }
+          : null,
+      onOpen: onOpen && !disabled
+          ? () {
+              server.sendPageEvent(
+                  eventTarget: widget.control.id,
+                  eventName: "open",
+                  eventData: "");
+            }
+          : null,
+      leadingIcon: leading.isNotEmpty
+          ? leading
+              .map((c) => createControl(widget.control, c.id, disabled))
+              .toList()
+              .first
+          : null,
+      trailingIcon: trailing.isNotEmpty
+          ? trailing
+              .map((c) => createControl(widget.control, c.id, disabled))
+              .toList()
+              .first
+          : null,
+      menuChildren: ctrls.isNotEmpty
+          ? ctrls.map((c) {
+              return createControl(widget.control, c.id, disabled);
+            }).toList()
+          : [],
+      child: content.isNotEmpty
+          ? content
+              .map((c) => createControl(widget.control, c.id, disabled))
+              .toList()
+              .first
+          : null,
+    );
+
+    var focusValue = widget.control.attrString("focus");
+    if (focusValue != null && focusValue != _lastFocusValue) {
+      _lastFocusValue = focusValue;
+      _focusNode.requestFocus();
+    }
+
+    return constrainedControl(context, subMenu, widget.parent, widget.control);
+  }
+}
