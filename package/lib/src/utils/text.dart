835fcb40bb4cf757ce4456062e01e84633cec80e
50bytes.dev <50bytes.dev@gmail.com>
2023-12-30 02:12:20 +0300

    fix

--- package/lib/src/utils/text.dart
+++ package/lib/src/utils/text.dart
@@ -173,11 +173,6 @@ TextStyle textStyleFromJson(ThemeData theme, Map<String, dynamic> json) {
     decorations.add(TextDecoration.lineThrough);
   }
 
-  var height = json["height"];
-  if (height != null && !(height is double)) {
-    height = double.tryParse(height.toString());
-  }
-
   return TextStyle(
       fontSize: json["size"] != null ? parseDouble(json["size"]) : null,
       fontWeight: fontWeight != null ? getFontWeight(fontWeight) : null,
@@ -186,7 +181,7 @@ TextStyle textStyleFromJson(ThemeData theme, Map<String, dynamic> json) {
           : null,
       fontFamily: json["font_family"],
       fontVariations: variations,
-      height: height,
+      height: json["height"] != null ? parseDouble(json["height"]) : null,
       decoration:
           decorations.isNotEmpty ? TextDecoration.combine(decorations) : null,
       decorationStyle: json["decoration_style"] != null
