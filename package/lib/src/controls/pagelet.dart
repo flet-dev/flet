5fda1e73056c27dfb93642bef8c520032bd263b0
InesaFitsner <inesa@appveyor.com>
2024-01-19 13:07:12 -0800

    navigation_bar and bottom_appbar properties

--- package/lib/src/controls/pagelet.dart
+++ package/lib/src/controls/pagelet.dart
@@ -1,14 +1,11 @@
 import 'package:collection/collection.dart';
-import 'package:flet/src/flet_app_services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-import '../actions.dart';
 import '../models/app_state.dart';
 import '../models/control.dart';
 import '../models/controls_view_model.dart';
-import '../protocol/update_control_props_payload.dart';
 import '../utils/colors.dart';
 import 'app_bar.dart';
 import 'create_control.dart';
@@ -35,49 +32,49 @@ class PageletControl extends StatefulWidget {
 }
 
 class _PageletControlState extends State<PageletControl> {
-  bool _selected = false;
+  // bool _selected = false;
 
-  late final FocusNode _focusNode;
+  // late final FocusNode _focusNode;
 
-  @override
-  void initState() {
-    super.initState();
-    _focusNode = FocusNode();
-    _focusNode.addListener(_onFocusChange);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _focusNode = FocusNode();
+  //   _focusNode.addListener(_onFocusChange);
+  // }
 
-  @override
-  void dispose() {
-    _focusNode.removeListener(_onFocusChange);
-    _focusNode.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   _focusNode.removeListener(_onFocusChange);
+  //   _focusNode.dispose();
+  //   super.dispose();
+  // }
 
-  void _onSelect(bool selected) {
-    var strSelected = selected.toString();
-    debugPrint(strSelected);
-    setState(() {
-      _selected = selected;
-    });
-    List<Map<String, String>> props = [
-      {"i": widget.control.id, "selected": strSelected}
-    ];
-    widget.dispatch(
-        UpdateControlPropsAction(UpdateControlPropsPayload(props: props)));
-    final server = FletAppServices.of(context).server;
-    server.updateControlProps(props: props);
-    server.sendPageEvent(
-        eventTarget: widget.control.id,
-        eventName: "select",
-        eventData: strSelected);
-  }
+  // void _onSelect(bool selected) {
+  //   var strSelected = selected.toString();
+  //   debugPrint(strSelected);
+  //   setState(() {
+  //     _selected = selected;
+  //   });
+  //   List<Map<String, String>> props = [
+  //     {"i": widget.control.id, "selected": strSelected}
+  //   ];
+  //   widget.dispatch(
+  //       UpdateControlPropsAction(UpdateControlPropsPayload(props: props)));
+  //   final server = FletAppServices.of(context).server;
+  //   server.updateControlProps(props: props);
+  //   server.sendPageEvent(
+  //       eventTarget: widget.control.id,
+  //       eventName: "select",
+  //       eventData: strSelected);
+  // }
 
-  void _onFocusChange() {
-    FletAppServices.of(context).server.sendPageEvent(
-        eventTarget: widget.control.id,
-        eventName: _focusNode.hasFocus ? "focus" : "blur",
-        eventData: "");
-  }
+  // void _onFocusChange() {
+  //   FletAppServices.of(context).server.sendPageEvent(
+  //       eventTarget: widget.control.id,
+  //       eventName: _focusNode.hasFocus ? "focus" : "blur",
+  //       eventData: "");
+  //}
 
   @override
   Widget build(BuildContext context) {
@@ -87,8 +84,10 @@ class _PageletControlState extends State<PageletControl> {
         widget.children.where((c) => c.name == "appbar" && c.isVisible);
     var contentCtrls =
         widget.children.where((c) => c.name == "content" && c.isVisible);
-    var deleteIconCtrls =
-        widget.children.where((c) => c.name == "deleteIcon" && c.isVisible);
+    var navigationBarCtrls =
+        widget.children.where((c) => c.name == "navigationbar" && c.isVisible);
+    var bottomAppBarCtrls =
+        widget.children.where((c) => c.name == "bottomappbar" && c.isVisible);
 
     if (contentCtrls.isEmpty) {
       return const ErrorControl("Pagelet must have content specified.");
@@ -98,56 +97,6 @@ class _PageletControlState extends State<PageletControl> {
 
     var bgcolor = HexColor.fromString(
         Theme.of(context), widget.control.attrString("bgcolor", "")!);
-    var deleteIconColor = HexColor.fromString(
-        Theme.of(context), widget.control.attrString("deleteIconColor", "")!);
-    var disabledColor = HexColor.fromString(
-        Theme.of(context), widget.control.attrString("disabledColor", "")!);
-
-    final server = FletAppServices.of(context).server;
-    bool onClick = widget.control.attrBool("onclick", false)!;
-    bool onDelete = widget.control.attrBool("onDelete", false)!;
-    bool onSelect = widget.control.attrBool("onSelect", false)!;
-
-    if (onSelect && onClick) {
-      return const ErrorControl(
-          "Chip cannot have both on_select and on_click events specified.");
-    }
-
-    bool autofocus = widget.control.attrBool("autofocus", false)!;
-    bool selected = widget.control.attrBool("selected", false)!;
-    if (_selected != selected) {
-      _selected = selected;
-    }
-    bool showCheckmark = widget.control.attrBool("showCheckmark", true)!;
-    String deleteButtonTooltipMessage =
-        widget.control.attrString("deleteButtonTooltipMessage", "")!;
-
-    var elevation = widget.control.attrDouble("elevation");
-
-    // var appBarView = childrenViews.controlViews
-    //     .firstWhereOrNull((v) => v.control.id == (appBar?.id ?? ""));
-    // var cupertinoAppBarView = childrenViews.controlViews
-    //     .firstWhereOrNull((v) => v.control.id == (cupertinoAppBar?.id ?? ""));
-
-    Function()? onClickHandler = onClick && !disabled
-        ? () {
-            debugPrint("Chip ${widget.control.id} clicked!");
-            server.sendPageEvent(
-                eventTarget: widget.control.id,
-                eventName: "click",
-                eventData: "");
-          }
-        : null;
-
-    Function()? onDeleteHandler = onDelete && !disabled
-        ? () {
-            debugPrint("Chip ${widget.control.id} deleted!");
-            server.sendPageEvent(
-                eventTarget: widget.control.id,
-                eventName: "delete",
-                eventData: "");
-          }
-        : null;
 
     List<String> childIds = [
       appBarCtrls.firstOrNull?.id,
@@ -168,6 +117,16 @@ class _PageletControlState extends State<PageletControl> {
           return false;
         },
         builder: (context, childrenViews) {
+          var navBar = navigationBarCtrls.isNotEmpty
+              ? createControl(
+                  widget.control, navigationBarCtrls.first.id, disabled)
+              : null;
+          var bottomAppBar = bottomAppBarCtrls.isNotEmpty
+              ? createControl(
+                  widget.control, bottomAppBarCtrls.first.id, disabled)
+              : null;
+          var bnb = navBar ?? bottomAppBar;
+
           var appBarView = childrenViews.controlViews.firstWhereOrNull(
               (v) => v.control.id == (appBarCtrls.firstOrNull?.id ?? ""));
 
@@ -194,46 +153,14 @@ class _PageletControlState extends State<PageletControl> {
           return constrainedControl(
               context,
               Scaffold(
-                // autofocus: autofocus,
-                // focusNode: _focusNode,
                 appBar: bar,
-                // avatar: leadingCtrls.isNotEmpty
-                //     ? createControl(widget.control, leadingCtrls.first.id, disabled)
-                //     : null,
                 backgroundColor: bgcolor,
-                body: createControl(
-                    widget.control, contentCtrls.first.id, disabled),
-                // checkmarkColor: HexColor.fromString(
-                //     Theme.of(context), widget.control.attrString("checkColor", "")!),
-                // selected: _selected,
-                // showCheckmark: showCheckmark,
-                // deleteButtonTooltipMessage: deleteButtonTooltipMessage,
-                // onPressed: onClickHandler,
-                // onDeleted: onDeleteHandler,
-                // onSelected: onSelect && !disabled
-                //     ? (bool selected) {
-                //         _onSelect(selected);
-                //       }
-                //     : null,
-                // deleteIcon: deleteIconCtrls.isNotEmpty
-                //     ? createControl(
-                //         widget.control, deleteIconCtrls.first.id, disabled)
-                //     : null,
-                // deleteIconColor: deleteIconColor,
-                // disabledColor: disabledColor,
-                // elevation: elevation,
-                // isEnabled: !disabled,
-                // padding: parseEdgeInsets(widget.control, "padding"),
-                // labelPadding: parseEdgeInsets(widget.control, "labelPadding"),
-                // labelStyle:
-                //     parseTextStyle(Theme.of(context), widget.control, "labelStyle"),
-                // selectedColor: HexColor.fromString(Theme.of(context),
-                //     widget.control.attrString("selectedColor", "")!),
-                // selectedShadowColor: HexColor.fromString(Theme.of(context),
-                //     widget.control.attrString("selectedShadowColor", "")!),
-                // shadowColor: HexColor.fromString(
-                //     Theme.of(context), widget.control.attrString("shadowColor", "")!),
-                // shape: parseOutlinedBorder(widget.control, "shape"),
+                //backgroundColor: Colors.red,
+                body: contentCtrls.isNotEmpty
+                    ? createControl(
+                        widget.control, contentCtrls.first.id, disabled)
+                    : null,
+                bottomNavigationBar: bnb,
               ),
               widget.parent,
               widget.control);
--- sdk/python/packages/flet-core/src/flet_core/pagelet.py
+++ sdk/python/packages/flet-core/src/flet_core/pagelet.py
@@ -1,14 +1,14 @@
 from typing import Any, Optional, Union
 
-from flet_core.buttons import OutlinedBorder
+from flet_core.bottom_app_bar import BottomAppBar
 from flet_core.constrained_control import ConstrainedControl
 from flet_core.control import Control, OptionalNumber
+from flet_core.cupertino_navigation_bar import CupertinoNavigationBar
+from flet_core.navigation_bar import NavigationBar
 from flet_core.ref import Ref
-from flet_core.text_style import TextStyle
 from flet_core.types import (
     AnimationValue,
     OffsetValue,
-    PaddingValue,
     ResponsiveNumber,
     RotateValue,
     ScaleValue,
@@ -17,53 +17,34 @@ from flet_core.types import (
 
 class Pagelet(ConstrainedControl):
     """
-    Chips are compact elements that represent an attribute, text, entity, or action.
+        Pagelet implements the basic Material Design visual layout structure.
 
-    Example:
-    ```
+        Use it for projects that require "page within a page" layouts with its own AppBar, BottomBar, Drawer, such as demos and galleries.
+
+        Example:
+        ```
     import flet as ft
 
 
     def main(page: ft.Page):
-        def save_to_favorites_clicked(e):
-            e.control.label.value = "Saved to favorites"
-            e.control.leading = ft.Icon(ft.icons.FAVORITE_OUTLINED)
-            e.control.disabled = True
-            page.update()
-
-        def open_google_maps(e):
-            page.launch_url("https://maps.google.com")
-            page.update()
-
-        save_to_favourites = ft.Chip(
-            label=ft.Text("Save to favourites"),
-            leading=ft.Icon(ft.icons.FAVORITE_BORDER_OUTLINED),
-            bgcolor=ft.colors.GREEN_200,
-            disabled_color=ft.colors.GREEN_100,
-            autofocus=True,
-            on_click=save_to_favorites_clicked,
+        page.add(
+            ft.Pagelet(
+                appbar=ft.CupertinoAppBar(middle=ft.Text("AppBar title")),
+                content=ft.Text("This is pagelet"),
+            )
         )
 
-        open_in_maps = ft.Chip(
-            label=ft.Text("9 min walk"),
-            leading=ft.Icon(ft.icons.MAP_SHARP),
-            bgcolor=ft.colors.GREEN_200,
-            on_click=open_google_maps,
-        )
-
-        page.add(ft.Row([save_to_favourites, open_in_maps]))
 
     ft.app(target=main)
-    ```
+        ```
 
-    -----
+        -----
 
-    Online docs: https://flet.dev/docs/controls/chip
+        Online docs: https://flet.dev/docs/controls/pagelet
     """
 
     def __init__(
         self,
-        # label: Control,
         ref: Optional[Ref] = None,
         width: OptionalNumber = None,
         height: OptionalNumber = None,
@@ -93,30 +74,11 @@ class Pagelet(ConstrainedControl):
         #
         # Specific
         #
-        autofocus: Optional[bool] = None,
         content: Optional[Control] = None,
         appbar: Optional[Control] = None,
+        navigation_bar: Union[NavigationBar, CupertinoNavigationBar, None] = None,
+        bottom_app_bar: Optional[BottomAppBar] = None,
         bgcolor: Optional[str] = None,
-        selected: Optional[bool] = False,
-        check_color: Optional[str] = None,
-        delete_icon_tooltip: Optional[str] = None,
-        delete_icon: Optional[Control] = None,
-        delete_icon_color: Optional[str] = None,
-        disabled_color: Optional[str] = None,
-        elevation: OptionalNumber = None,
-        label_padding: PaddingValue = None,
-        label_style: Optional[TextStyle] = None,
-        padding: PaddingValue = None,
-        selected_color: Optional[str] = None,
-        selected_shadow_color: Optional[str] = None,
-        shadow_color: Optional[str] = None,
-        shape: Optional[OutlinedBorder] = None,
-        show_checkmark: Optional[bool] = None,
-        on_click=None,
-        on_delete=None,
-        on_select=None,
-        on_focus=None,
-        on_blur=None,
     ):
         ConstrainedControl.__init__(
             self,
@@ -148,41 +110,14 @@ class Pagelet(ConstrainedControl):
             data=data,
         )
 
-        self.autofocus = autofocus
-        # self.label = label
         self.content = content
         self.appbar = appbar
         self.bgcolor = bgcolor
-        self.check_color = check_color
-        self.selected = selected
-        self.delete_icon_tooltip = delete_icon_tooltip
-        self.delete_icon = delete_icon
-        self.delete_icon_color = delete_icon_color
-        self.disabled_color = disabled_color
-        self.elevation = elevation
-        self.label_padding = label_padding
-        self.label_style = label_style
-        self.padding = padding
-        self.selected_color = selected_color
-        self.selected_shadow_color = selected_shadow_color
-        self.shadow_color = shadow_color
-        self.shape = shape
-        self.show_checkmark = show_checkmark
-        self.on_click = on_click
-        self.on_delete = on_delete
-        self.on_select = on_select
-        self.on_focus = on_focus
-        self.on_blur = on_blur
+        self.navigation_bar = navigation_bar
+        self.bottom_appbar = bottom_app_bar
 
     def _get_control_name(self):
         return "pagelet"
-
-    def _before_build_command(self):
-        super()._before_build_command()
-        self._set_attr_json("labelPadding", self.__label_padding)
-        self._set_attr_json("labelStyle", self.__label_style)
-        self._set_attr_json("padding", self.__padding)
-        self._set_attr_json("shape", self.__shape)
 
     def _get_children(self):
         children = []
@@ -192,46 +127,13 @@ class Pagelet(ConstrainedControl):
         if self.__content:
             self.__content._set_attr_internal("n", "content")
             children.append(self.__content)
-        if self.__delete_icon:
-            self.__delete_icon._set_attr_internal("n", "deleteIcon")
-            children.append(self.__delete_icon)
+        if self.__navigation_bar:
+            self.__navigation_bar._set_attr_internal("n", "navigationbar")
+            children.append(self.__navigation_bar)
+        if self.__bottom_appbar:
+            self.__bottom_appbar._set_attr_internal("n", "bottomappbar")
+            children.append(self.__bottom_appbar)
         return children
-
-    # padding
-    @property
-    def padding(self) -> PaddingValue:
-        return self.__padding
-
-    @padding.setter
-    def padding(self, value: PaddingValue):
-        self.__padding = value
-
-    # selected
-    @property
-    def selected(self) -> Optional[bool]:
-        return self._get_attr("selected", data_type="bool", def_value=False)
-
-    @selected.setter
-    def selected(self, value: Optional[bool]):
-        self._set_attr("selected", value)
-
-    # show_checkmark
-    @property
-    def show_checkmark(self) -> Optional[bool]:
-        return self._get_attr("showCheckmark")
-
-    @show_checkmark.setter
-    def show_checkmark(self, value: Optional[bool]):
-        self._set_attr("showCheckmark", value)
-
-    # delete_icon_tooltip
-    @property
-    def delete_icon_tooltip(self):
-        return self._get_attr("deleteButtonTooltipMessage")
-
-    @delete_icon_tooltip.setter
-    def delete_icon_tooltip(self, value):
-        self._set_attr("deleteButtonTooltipMessage", value)
 
     # appbar
     @property
@@ -242,24 +144,6 @@ class Pagelet(ConstrainedControl):
     def appbar(self, value: Control):
         self.__appbar = value
 
-    # label_padding
-    @property
-    def label_padding(self) -> PaddingValue:
-        return self.__label_padding
-
-    @label_padding.setter
-    def label_padding(self, value: PaddingValue):
-        self.__label_padding = value
-
-    # label_style
-    @property
-    def label_style(self):
-        return self.__label_style
-
-    @label_style.setter
-    def label_style(self, value: Optional[TextStyle]):
-        self.__label_style = value
-
     # content
     @property
     def content(self) -> Optional[Control]:
@@ -268,42 +152,6 @@ class Pagelet(ConstrainedControl):
     @content.setter
     def content(self, value: Optional[Control]):
         self.__content = value
-
-    # delete_icon
-    @property
-    def delete_icon(self) -> Optional[Control]:
-        return self.__delete_icon
-
-    @delete_icon.setter
-    def delete_icon(self, value: Optional[Control]):
-        self.__delete_icon = value
-
-    # delete_icon_color
-    @property
-    def delete_icon_color(self):
-        return self._get_attr("deleteIconColor")
-
-    @delete_icon_color.setter
-    def delete_icon_color(self, value):
-        self._set_attr("deleteIconColor", value)
-
-    # disabled_color
-    @property
-    def disabled_color(self):
-        return self._get_attr("disabledColor")
-
-    @disabled_color.setter
-    def disabled_color(self, value):
-        self._set_attr("disabledColor", value)
-
-    # autofocus
-    @property
-    def autofocus(self) -> Optional[bool]:
-        return self._get_attr("autofocus", data_type="bool", def_value=False)
-
-    @autofocus.setter
-    def autofocus(self, value: Optional[bool]):
-        self._set_attr("autofocus", value)
 
     # bgcolor
     @property
@@ -314,104 +162,23 @@ class Pagelet(ConstrainedControl):
     def bgcolor(self, value):
         self._set_attr("bgcolor", value)
 
-    # check_color
+    # bottom_appbar
     @property
-    def check_color(self):
-        return self._get_attr("checkColor")
+    def bottom_appbar(self) -> Optional[BottomAppBar]:
+        return self.__bottom_appbar
 
-    @check_color.setter
-    def check_color(self, value):
-        self._set_attr("checkColor", value)
+    @bottom_appbar.setter
+    def bottom_appbar(self, value: Optional[BottomAppBar]):
+        self.__bottom_appbar = value
 
-    # selected_color
+    # navigation_bar
     @property
-    def selected_color(self):
-        return self._get_attr("selectedColor")
+    def navigation_bar(self) -> Union[NavigationBar, CupertinoNavigationBar, None]:
+        return self.__navigation_bar
 
-    @selected_color.setter
-    def selected_color(self, value):
-        self._set_attr("selectedColor", value)
-
-    # selected_shadow_color
-    @property
-    def selected_shadow_color(self):
-        return self._get_attr("selectedShadowColor")
-
-    @selected_shadow_color.setter
-    def selected_shadow_color(self, value):
-        self._set_attr("selectedShadowColor", value)
-
-    # shadow_color
-    @property
-    def shadow_color(self):
-        return self._get_attr("shadowColor")
-
-    @shadow_color.setter
-    def shadow_color(self, value):
-        self._set_attr("shadowColor", value)
-
-    # elevation
-    @property
-    def elevation(self) -> OptionalNumber:
-        return self._get_attr("elevation")
-
-    @elevation.setter
-    def elevation(self, value: OptionalNumber):
-        self._set_attr("elevation", value)
-
-    # shape
-    @property
-    def shape(self) -> Optional[OutlinedBorder]:
-        return self.__shape
-
-    @shape.setter
-    def shape(self, value: Optional[OutlinedBorder]):
-        self.__shape = value
-
-    # on_click
-    @property
-    def on_click(self):
-        return self._get_event_handler("click")
-
-    @on_click.setter
-    def on_click(self, handler):
-        self._add_event_handler("click", handler)
-        self._set_attr("onclick", True if handler is not None else None)
-
-    # on_delete
-    @property
-    def on_delete(self):
-        return self._get_event_handler("delete")
-
-    @on_delete.setter
-    def on_delete(self, handler):
-        self._add_event_handler("delete", handler)
-        self._set_attr("onDelete", True if handler is not None else None)
-
-    # on_select
-    @property
-    def on_select(self):
-        return self._get_event_handler("select")
-
-    @on_select.setter
-    def on_select(self, handler):
-        self._add_event_handler("select", handler)
-        self._set_attr("onSelect", True if handler is not None else None)
-
-    # on_focus
-    @property
-    def on_focus(self):
-        return self._get_event_handler("focus")
-
-    @on_focus.setter
-    def on_focus(self, handler):
-        self._add_event_handler("focus", handler)
-
-    # on_blur
-    @property
-    def on_blur(self):
-        return self._get_event_handler("blur")
-
-    @on_blur.setter
-    def on_blur(self, handler):
-        self._add_event_handler("blur", handler)
+    @navigation_bar.setter
+    def navigation_bar(
+        self,
+        value: Union[NavigationBar, CupertinoNavigationBar, None],
+    ):
+        self.__navigation_bar = value
