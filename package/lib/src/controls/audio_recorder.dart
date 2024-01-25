97a5e2ce540f99950e46ddac60febc9b3118d38a
ndonkoHenri <robotcoder4@protonmail.com>
2024-01-25 10:45:25 +0100

    remove storeconnector

--- package/lib/src/controls/audio_recorder.dart
+++ package/lib/src/controls/audio_recorder.dart
@@ -2,14 +2,11 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:record/record.dart';
 
 import '../flet_app_services.dart';
 import '../flet_server.dart';
-import '../models/app_state.dart';
 import '../models/control.dart';
-import '../models/page_args_model.dart';
 import '../utils/audio_recorder.dart';
 
 class AudioRecorderControl extends StatefulWidget {
@@ -62,7 +59,6 @@ class _AudioRecorderControlState extends State<AudioRecorderControl> {
   Future<String?> stopRecording() async {
     debugPrint("AudioRecorder.stopRecording($hashCode)");
     final path = await recorder!.stop();
-    debugPrint("AudioRecorder.stopRecording: $path");
     return path;
   }
 
@@ -77,81 +73,73 @@ class _AudioRecorderControlState extends State<AudioRecorderControl> {
     bool autoGain = widget.control.attrBool("autoGain", false)!;
     bool cancelEcho = widget.control.attrBool("cancelEcho", false)!;
     bool suppressNoise = widget.control.attrBool("suppressNoise", false)!;
-    AudioEncoder audioEncoding =
-        parseAudioEncoder(widget.control.attrString("audioEncoding", "wav"))!;
+    AudioEncoder audioEncoder =
+        parseAudioEncoder(widget.control.attrString("audioEncoder", "wav"))!;
 
     var server = FletAppServices.of(context).server;
 
-    return StoreConnector<AppState, PageArgsModel>(
-        distinct: true,
-        converter: (store) => PageArgsModel.fromStore(store),
-        builder: (context, pageArgs) {
-          () async {
-            _server = server;
-            _server?.controlInvokeMethods[widget.control.id] =
-                (methodName, args) async {
-              switch (methodName) {
-                case "start_recording":
-                  debugPrint("AudioRecorder.start-PATH: ${args["outputPath"]}");
-                  if (await recorder!.hasPermission()) {
-                    await recorder!.start(
-                        RecordConfig(
-                          encoder: audioEncoding,
-                          bitRate: bitRate,
-                          sampleRate: sampleRate,
-                          numChannels: numChannels,
-                          autoGain: autoGain,
-                          echoCancel: cancelEcho,
-                          noiseSuppress: suppressNoise,
-                        ),
-                        path: args["outputPath"] ??
-                            ""); // FIX: a better default value just in case
-                  }
-                  break;
-                case "stop_recording":
-                  debugPrint("AudioRecorder.stopRecording($hashCode)");
-                  return await recorder!.stop();
-                case "resume_recording":
-                  debugPrint("AudioRecorder.resumeRecording($hashCode)");
-                  await recorder!.resume();
-                case "pause_recording":
-                  debugPrint("AudioRecorder.pauseRecording($hashCode)");
-                  await recorder!.pause();
-                case "is_supported_encoder":
-                  debugPrint("AudioRecorder.isEncoderSupported($hashCode)");
-                  if (parseAudioEncoder(args["encoder"]) != null) {
-                    bool isSupported = await recorder!.isEncoderSupported(
-                        parseAudioEncoder(args["encoder"]) ?? AudioEncoder.wav);
-                    return isSupported.toString();
-                  }
-                  return null;
-                case "is_paused":
-                  debugPrint("AudioRecorder.isPaused($hashCode)");
-                  bool isPaused = await recorder!.isPaused();
-                  return isPaused.toString();
-                case "is_recording":
-                  debugPrint("AudioRecorder.isRecording($hashCode)");
-                  bool isRecording = await recorder!.isRecording();
-                  return isRecording.toString();
-                case "has_permission":
-                  debugPrint("AudioRecorder.hasPermission($hashCode)");
-                  bool hasPermission = await recorder!.hasPermission();
-                  return hasPermission.toString();
-                case "get_input_devices":
-                  debugPrint("AudioRecorder.getInputDevices($hashCode)");
-                  List<InputDevice> devices =
-                      await recorder!.listInputDevices();
-                  String devicesJson =  json.encode(devices.asMap().map((key, value) {
-                    return MapEntry(key, (value.id, value.label));
-                  }).toString());
-                  debugPrint("AudioRecorder.getInputDevices: $devicesJson");
-                  return devicesJson;
-              }
-              return null;
-            };
-          }();
+    () async {
+      _server = server;
+      _server?.controlInvokeMethods[widget.control.id] =
+          (methodName, args) async {
+        switch (methodName) {
+          case "start_recording":
+            if (await recorder!.hasPermission()) {
+              await recorder!.start(
+                  RecordConfig(
+                    encoder: audioEncoder,
+                    bitRate: bitRate,
+                    sampleRate: sampleRate,
+                    numChannels: numChannels,
+                    autoGain: autoGain,
+                    echoCancel: cancelEcho,
+                    noiseSuppress: suppressNoise,
+                  ),
+                  path: args["outputPath"] ??
+                      ""); // FIX: a better default value just in case
+            }
+            break;
+          case "stop_recording":
+            debugPrint("AudioRecorder.stopRecording($hashCode)");
+            return await recorder!.stop();
+          case "resume_recording":
+            debugPrint("AudioRecorder.resumeRecording($hashCode)");
+            await recorder!.resume();
+          case "pause_recording":
+            debugPrint("AudioRecorder.pauseRecording($hashCode)");
+            await recorder!.pause();
+          case "is_supported_encoder":
+            debugPrint("AudioRecorder.isEncoderSupported($hashCode)");
+            if (parseAudioEncoder(args["encoder"]) != null) {
+              bool isSupported = await recorder!.isEncoderSupported(
+                  parseAudioEncoder(args["encoder"]) ?? AudioEncoder.wav);
+              return isSupported.toString();
+            }
+            return null;
+          case "is_paused":
+            debugPrint("AudioRecorder.isPaused($hashCode)");
+            bool isPaused = await recorder!.isPaused();
+            return isPaused.toString();
+          case "is_recording":
+            debugPrint("AudioRecorder.isRecording($hashCode)");
+            bool isRecording = await recorder!.isRecording();
+            return isRecording.toString();
+          case "has_permission":
+            debugPrint("AudioRecorder.hasPermission($hashCode)");
+            bool hasPermission = await recorder!.hasPermission();
+            return hasPermission.toString();
+          case "get_input_devices":
+            debugPrint("AudioRecorder.getInputDevices($hashCode)");
+            List<InputDevice> devices = await recorder!.listInputDevices();
+            String devicesJson = json.encode(devices.asMap().map((key, value) {
+              return MapEntry(key, (value.id, value.label));
+            }).toString());
+            return devicesJson;
+        }
+        return null;
+      };
+    }();
 
-          return const SizedBox.shrink();
-        });
+    return const SizedBox.shrink();
   }
 }
