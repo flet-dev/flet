9e2834380ef2bb6c91de9295807cf5debdc3a081
Feodor Fitsner <feodor@appveyor.com>
2024-02-11 18:22:27 -0800

    Pagelet.design

--- packages/flet/lib/src/controls/pagelet.dart
+++ packages/flet/lib/src/controls/pagelet.dart
@@ -12,8 +12,10 @@ import 'app_bar.dart';
 import 'create_control.dart';
 import 'cupertino_app_bar.dart';
 import 'error.dart';
+import 'flet_store_mixin.dart';
 import 'floating_action_button.dart';
 import 'navigation_drawer.dart';
+import 'page.dart';
 
 class PageletControl extends StatefulWidget {
   final Control? parent;
@@ -36,7 +38,7 @@ class PageletControl extends StatefulWidget {
   State<PageletControl> createState() => _PageletControlState();
 }
 
-class _PageletControlState extends State<PageletControl> {
+class _PageletControlState extends State<PageletControl> with FletStoreMixin {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
@@ -64,184 +66,212 @@ class _PageletControlState extends State<PageletControl> {
       return const ErrorControl("Pagelet must have content specified.");
     }
 
-    bool disabled = widget.control.isDisabled || widget.parentDisabled;
-    bool? adaptive =
-        widget.control.attrBool("adaptive") ?? widget.parentAdaptive;
+    return withPagePlatform((context, platform) {
+      PageDesign widgetsDesign = PageDesign.material;
 
-    var bgcolor = HexColor.fromString(
-        Theme.of(context), widget.control.attrString("bgcolor", "")!);
+      var pageDesign = PageDesign.values.firstWhereOrNull((a) =>
+          a.name.toLowerCase() ==
+          widget.control.attrString("design", "")!.toLowerCase());
 
-    List<String> childIds = [
-      appBarCtrls.firstOrNull?.id,
-      drawerCtrls.firstOrNull?.id,
-      endDrawerCtrls.firstOrNull?.id
-    ].whereNotNull().toList();
+      if ((pageDesign == PageDesign.adaptive &&
+              (platform == TargetPlatform.iOS ||
+                  platform == TargetPlatform.macOS)) ||
+          pageDesign == PageDesign.cupertino) {
+        widgetsDesign = PageDesign.cupertino;
+      } else {
+        widgetsDesign = PageDesign.material;
+      }
 
-    return StoreConnector<AppState, ControlsViewModel>(
-        distinct: true,
-        converter: (store) => ControlsViewModel.fromStore(store, childIds),
-        ignoreChange: (state) {
-          //debugPrint("ignoreChange: $id");
-          for (var id in childIds) {
-            if (state.controls[id] == null) {
-              return true;
-            }
-          }
-          return false;
-        },
-        builder: (context, childrenViews) {
-          var navBar = navigationBarCtrls.isNotEmpty
-              ? createControl(
-                  widget.control, navigationBarCtrls.first.id, disabled,
-                  parentAdaptive: adaptive)
-              : null;
-          var bottomAppBar = bottomAppBarCtrls.isNotEmpty
-              ? createControl(
-                  widget.control, bottomAppBarCtrls.first.id, disabled,
-                  parentAdaptive: adaptive)
-              : null;
-          var bnb = navBar ?? bottomAppBar;
+      bool disabled = widget.control.isDisabled || widget.parentDisabled;
+      bool? adaptive = pageDesign != null
+          ? pageDesign == PageDesign.adaptive
+          : widget.parentAdaptive;
 
-          var appBarView = childrenViews.controlViews.firstWhereOrNull(
-              (v) => v.control.id == (appBarCtrls.firstOrNull?.id ?? ""));
+      var bgcolor = HexColor.fromString(
+          Theme.of(context), widget.control.attrString("bgcolor", "")!);
 
-          var drawerView = childrenViews.controlViews.firstWhereOrNull(
-              (v) => v.control.id == (drawerCtrls.firstOrNull?.id ?? ""));
-          var endDrawerView = childrenViews.controlViews.firstWhereOrNull(
-              (v) => v.control.id == (endDrawerCtrls.firstOrNull?.id ?? ""));
+      List<String> childIds = [
+        appBarCtrls.firstOrNull?.id,
+        drawerCtrls.firstOrNull?.id,
+        endDrawerCtrls.firstOrNull?.id
+      ].whereNotNull().toList();
 
-          final bool? drawerOpened = widget.control.state["drawerOpened"];
-          final bool? endDrawerOpened = widget.control.state["endDrawerOpened"];
-
-          final fabLocation = parseFloatingActionButtonLocation(
-              widget.control,
-              "floatingActionButtonLocation",
-              FloatingActionButtonLocation.endFloat);
-
-          void dismissDrawer(String id) {
-            widget.backend.updateControlState(id, {"open": "false"});
-            widget.backend.triggerControlEvent(id, "dismiss", "");
-          }
-
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (drawerView != null) {
-              if (scaffoldKey.currentState?.isDrawerOpen == false &&
-                  drawerOpened == true) {
-                widget.control.state["drawerOpened"] = false;
-                dismissDrawer(drawerView.control.id);
+      return StoreConnector<AppState, ControlsViewModel>(
+          distinct: true,
+          converter: (store) => ControlsViewModel.fromStore(store, childIds),
+          ignoreChange: (state) {
+            //debugPrint("ignoreChange: $id");
+            for (var id in childIds) {
+              if (state.controls[id] == null) {
+                return true;
               }
-              if (drawerView.control.attrBool("open", false)! &&
-                  drawerOpened != true) {
-                if (scaffoldKey.currentState?.isEndDrawerOpen == true) {
-                  scaffoldKey.currentState?.closeEndDrawer();
+            }
+            return false;
+          },
+          builder: (context, childrenViews) {
+            var navBar = navigationBarCtrls.isNotEmpty
+                ? createControl(
+                    widget.control, navigationBarCtrls.first.id, disabled,
+                    parentAdaptive: adaptive)
+                : null;
+            var bottomAppBar = bottomAppBarCtrls.isNotEmpty
+                ? createControl(
+                    widget.control, bottomAppBarCtrls.first.id, disabled,
+                    parentAdaptive: adaptive)
+                : null;
+            var bnb = navBar ?? bottomAppBar;
+
+            var appBarView = childrenViews.controlViews.firstWhereOrNull(
+                (v) => v.control.id == (appBarCtrls.firstOrNull?.id ?? ""));
+
+            var drawerView = childrenViews.controlViews.firstWhereOrNull(
+                (v) => v.control.id == (drawerCtrls.firstOrNull?.id ?? ""));
+            var endDrawerView = childrenViews.controlViews.firstWhereOrNull(
+                (v) => v.control.id == (endDrawerCtrls.firstOrNull?.id ?? ""));
+
+            final bool? drawerOpened = widget.control.state["drawerOpened"];
+            final bool? endDrawerOpened =
+                widget.control.state["endDrawerOpened"];
+
+            final fabLocation = parseFloatingActionButtonLocation(
+                widget.control,
+                "floatingActionButtonLocation",
+                FloatingActionButtonLocation.endFloat);
+
+            void dismissDrawer(String id) {
+              widget.backend.updateControlState(id, {"open": "false"});
+              widget.backend.triggerControlEvent(id, "dismiss", "");
+            }
+
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              if (drawerView != null) {
+                if (scaffoldKey.currentState?.isDrawerOpen == false &&
+                    drawerOpened == true) {
+                  widget.control.state["drawerOpened"] = false;
+                  dismissDrawer(drawerView.control.id);
                 }
-                Future.delayed(const Duration(milliseconds: 1)).then((value) {
-                  scaffoldKey.currentState?.openDrawer();
-                  widget.control.state["drawerOpened"] = true;
-                });
-              } else if (!drawerView.control.attrBool("open", false)! &&
-                  drawerOpened == true) {
-                scaffoldKey.currentState?.closeDrawer();
-                widget.control.state["drawerOpened"] = false;
-              }
-            }
-            if (endDrawerView != null) {
-              if (scaffoldKey.currentState?.isEndDrawerOpen == false &&
-                  endDrawerOpened == true) {
-                widget.control.state["endDrawerOpened"] = false;
-                dismissDrawer(endDrawerView.control.id);
-              }
-              if (endDrawerView.control.attrBool("open", false)! &&
-                  endDrawerOpened != true) {
-                if (scaffoldKey.currentState?.isDrawerOpen == true) {
+                if (drawerView.control.attrBool("open", false)! &&
+                    drawerOpened != true) {
+                  if (scaffoldKey.currentState?.isEndDrawerOpen == true) {
+                    scaffoldKey.currentState?.closeEndDrawer();
+                  }
+                  Future.delayed(const Duration(milliseconds: 1)).then((value) {
+                    scaffoldKey.currentState?.openDrawer();
+                    widget.control.state["drawerOpened"] = true;
+                  });
+                } else if (!drawerView.control.attrBool("open", false)! &&
+                    drawerOpened == true) {
                   scaffoldKey.currentState?.closeDrawer();
+                  widget.control.state["drawerOpened"] = false;
                 }
-                Future.delayed(const Duration(milliseconds: 1)).then((value) {
-                  scaffoldKey.currentState?.openEndDrawer();
-                  widget.control.state["endDrawerOpened"] = true;
-                });
-              } else if (!endDrawerView.control.attrBool("open", false)! &&
-                  endDrawerOpened == true) {
-                scaffoldKey.currentState?.closeEndDrawer();
-                widget.control.state["endDrawerOpened"] = false;
               }
-            }
-          });
+              if (endDrawerView != null) {
+                if (scaffoldKey.currentState?.isEndDrawerOpen == false &&
+                    endDrawerOpened == true) {
+                  widget.control.state["endDrawerOpened"] = false;
+                  dismissDrawer(endDrawerView.control.id);
+                }
+                if (endDrawerView.control.attrBool("open", false)! &&
+                    endDrawerOpened != true) {
+                  if (scaffoldKey.currentState?.isDrawerOpen == true) {
+                    scaffoldKey.currentState?.closeDrawer();
+                  }
+                  Future.delayed(const Duration(milliseconds: 1)).then((value) {
+                    scaffoldKey.currentState?.openEndDrawer();
+                    widget.control.state["endDrawerOpened"] = true;
+                  });
+                } else if (!endDrawerView.control.attrBool("open", false)! &&
+                    endDrawerOpened == true) {
+                  scaffoldKey.currentState?.closeEndDrawer();
+                  widget.control.state["endDrawerOpened"] = false;
+                }
+              }
+            });
 
-          var bar = appBarView != null
-              ? appBarView.control.type == "appbar"
-                  ? AppBarControl(
-                      parent: widget.control,
-                      control: appBarView.control,
-                      children: appBarView.children,
-                      parentDisabled: widget.control.isDisabled,
-                      parentAdaptive: adaptive,
-                      height: appBarView.control
-                          .attrDouble("toolbarHeight", kToolbarHeight)!)
-                  : appBarView.control.type == "cupertinoappbar"
-                      ? CupertinoAppBarControl(
-                          parent: widget.control,
-                          control: appBarView.control,
-                          children: appBarView.children,
-                          parentDisabled: widget.control.isDisabled,
-                          parentAdaptive: adaptive,
-                        ) as ObstructingPreferredSizeWidget
-                      : null
-              : null;
-          return constrainedControl(
-              context,
-              Scaffold(
+            var bar = appBarView != null
+                ? appBarView.control.type == "appbar"
+                    ? AppBarControl(
+                        parent: widget.control,
+                        control: appBarView.control,
+                        children: appBarView.children,
+                        parentDisabled: widget.control.isDisabled,
+                        parentAdaptive: adaptive,
+                        height: appBarView.control
+                            .attrDouble("toolbarHeight", kToolbarHeight)!)
+                    : appBarView.control.type == "cupertinoappbar"
+                        ? CupertinoAppBarControl(
+                            parent: widget.control,
+                            control: appBarView.control,
+                            children: appBarView.children,
+                            parentDisabled: widget.control.isDisabled,
+                            parentAdaptive: adaptive,
+                          ) as ObstructingPreferredSizeWidget
+                        : null
+                : null;
+
+            Widget scaffold = Scaffold(
+                key: widgetsDesign != PageDesign.cupertino ? scaffoldKey : null,
+                backgroundColor: HexColor.fromString(Theme.of(context),
+                        widget.control.attrString("bgcolor", "")!) ??
+                    CupertinoTheme.of(context).scaffoldBackgroundColor,
+                appBar: widgetsDesign != PageDesign.cupertino ? bar : null,
+                drawer: drawerView != null
+                    ? NavigationDrawerControl(
+                        control: drawerView.control,
+                        children: drawerView.children,
+                        parentDisabled: widget.control.isDisabled,
+                        parentAdaptive: adaptive,
+                        backend: widget.backend)
+                    : null,
+                onDrawerChanged: (opened) {
+                  if (drawerView != null && !opened) {
+                    widget.control.state["drawerOpened"] = false;
+                    dismissDrawer(drawerView.control.id);
+                  }
+                },
+                endDrawer: endDrawerView != null
+                    ? NavigationDrawerControl(
+                        control: endDrawerView.control,
+                        children: endDrawerView.children,
+                        parentDisabled: widget.control.isDisabled,
+                        parentAdaptive: adaptive,
+                        backend: widget.backend)
+                    : null,
+                onEndDrawerChanged: (opened) {
+                  if (endDrawerView != null && !opened) {
+                    widget.control.state["endDrawerOpened"] = false;
+                    dismissDrawer(endDrawerView.control.id);
+                  }
+                },
+                body: contentCtrls.isNotEmpty
+                    ? createControl(
+                        widget.control, contentCtrls.first.id, disabled,
+                        parentAdaptive: adaptive)
+                    : null,
+                bottomNavigationBar: bnb,
+                bottomSheet: bottomSheetCtrls.isNotEmpty
+                    ? createControl(
+                        widget.control, bottomSheetCtrls.first.id, disabled,
+                        parentAdaptive: adaptive)
+                    : null,
+                floatingActionButton: fabCtrls.isNotEmpty
+                    ? createControl(widget.control, fabCtrls.first.id, disabled,
+                        parentAdaptive: adaptive)
+                    : null,
+                floatingActionButtonLocation: fabLocation);
+
+            if (widgetsDesign == PageDesign.cupertino) {
+              scaffold = CupertinoPageScaffold(
                   key: scaffoldKey,
-                  appBar: bar,
-                  backgroundColor: bgcolor,
-                  //backgroundColor: Colors.red,
-                  drawer: drawerView != null
-                      ? NavigationDrawerControl(
-                          control: drawerView.control,
-                          children: drawerView.children,
-                          parentDisabled: widget.control.isDisabled,
-                          parentAdaptive: adaptive,
-                          backend: widget.backend)
-                      : null,
-                  onDrawerChanged: (opened) {
-                    if (drawerView != null && !opened) {
-                      widget.control.state["drawerOpened"] = false;
-                      dismissDrawer(drawerView.control.id);
-                    }
-                  },
-                  endDrawer: endDrawerView != null
-                      ? NavigationDrawerControl(
-                          control: endDrawerView.control,
-                          children: endDrawerView.children,
-                          parentDisabled: widget.control.isDisabled,
-                          parentAdaptive: adaptive,
-                          backend: widget.backend)
-                      : null,
-                  onEndDrawerChanged: (opened) {
-                    if (endDrawerView != null && !opened) {
-                      widget.control.state["endDrawerOpened"] = false;
-                      dismissDrawer(endDrawerView.control.id);
-                    }
-                  },
-                  body: contentCtrls.isNotEmpty
-                      ? createControl(
-                          widget.control, contentCtrls.first.id, disabled,
-                          parentAdaptive: adaptive)
-                      : null,
-                  bottomNavigationBar: bnb,
-                  bottomSheet: bottomSheetCtrls.isNotEmpty
-                      ? createControl(
-                          widget.control, bottomSheetCtrls.first.id, disabled,
-                          parentAdaptive: adaptive)
-                      : null,
-                  floatingActionButton: fabCtrls.isNotEmpty
-                      ? createControl(
-                          widget.control, fabCtrls.first.id, disabled,
-                          parentAdaptive: adaptive)
-                      : null,
-                  floatingActionButtonLocation: fabLocation),
-              widget.parent,
-              widget.control);
-        });
+                  backgroundColor: HexColor.fromString(Theme.of(context),
+                      widget.control.attrString("bgcolor", "")!),
+                  navigationBar: bar as ObstructingPreferredSizeWidget,
+                  child: scaffold);
+            }
+
+            return constrainedControl(
+                context, scaffold, widget.parent, widget.control);
+          });
+    });
   }
 }
