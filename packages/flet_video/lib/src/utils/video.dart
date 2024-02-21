861b11ccbabb2d5bd950ab6eee79aab1df47aaab
ndonkoHenri <robotcoder4@protonmail.com>
2024-02-21 14:56:27 +0100

    parseSubtitleTrack, parseSubtitleConfiguration

--- packages/flet_video/lib/src/utils/video.dart
+++ packages/flet_video/lib/src/utils/video.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flet/flet.dart';
+import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
+import 'package:media_kit_video/media_kit_video.dart';
 
 List<Media> parseVideoMedia(Control control, String propName) {
   var v = control.attrString(propName, null);
@@ -46,4 +49,60 @@ Media? videoMediaFromJSON(dynamic json) {
     );
   }
   return null;
+}
+
+Map<String, dynamic>? parseSubtitleConfiguration(
+    ThemeData theme, Control control, String propName) {
+  var v = control.attrString(propName, null);
+  if (v == null) {
+    return null;
+  }
+
+  final j1 = json.decode(v);
+  return subtitleConfigurationFromJSON(theme, j1);
+}
+
+Map<String, dynamic> subtitleConfigurationFromJSON(
+    ThemeData theme, dynamic json) {
+  SubtitleViewConfiguration configuration = SubtitleViewConfiguration(
+    style: json["text_style"] != null
+        ? textStyleFromJson(theme, json["text_style"])
+        : const TextStyle(
+            height: 1.4,
+            fontSize: 32.0,
+            letterSpacing: 0.0,
+            wordSpacing: 0.0,
+            color: Color(0xffffffff),
+            fontWeight: FontWeight.normal,
+            backgroundColor: Color(0xaa000000)),
+    visible: json["visible"] != null ? parseBool(json["visible"]) : true,
+    textScaleFactor: json["text_scale_factor"] != null
+        ? parseDouble(json["text_scale_factor"])
+        : null,
+    textAlign: json["text_align"] != null
+        ? parseTextAlign(json["text_align"], TextAlign.center)!
+        : TextAlign.center,
+    padding: json["padding"] != null
+        ? edgeInsetsFromJson(json["padding"])
+        : const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 24.0),
+  );
+
+  return <String, dynamic>{
+    "src": json["src"],
+    "title": json["title"],
+    "language": json["language"],
+    "subtitleViewConfiguration": configuration
+  };
+}
+
+SubtitleTrack parseSubtitleTrack(
+    AssetSrc assetSrc, String? title, String? language) {
+  if (assetSrc.isFile) {
+    String filePath = assetSrc.path;
+    File file = File(filePath);
+    String content = file.readAsStringSync();
+    return SubtitleTrack.data(content, title: title, language: language);
+  } else {
+    return SubtitleTrack.uri(assetSrc.path, title: title, language: language);
+  }
 }
