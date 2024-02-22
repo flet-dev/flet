6fd4294ccfa4d6dc2297e4014571875b02cf04d3
InesaFitsner <inesa@appveyor.com>
2024-02-22 09:51:57 -0800

    CupertinoFilledButton

--- sdk/python/packages/flet-core/src/flet_core/cupertino_filled_button.py
+++ sdk/python/packages/flet-core/src/flet_core/cupertino_filled_button.py
@@ -1,15 +1,15 @@
 from typing import Any, Optional, Union
 
-from flet_core.buttons import ButtonStyle
+from flet_core.alignment import Alignment
 from flet_core.control import Control, OptionalNumber
 from flet_core.cupertino_button import CupertinoButton
 from flet_core.ref import Ref
-from flet_core.types import ResponsiveNumber
+from flet_core.types import BorderRadiusValue, PaddingValue, ResponsiveNumber
 
 
 class CupertinoFilledButton(CupertinoButton):
     """
-    Filled buttons have the most visual impact after the FloatingActionButton (https://flet.dev/docs/controls/floatingactionbutton), and should be used for important, final actions that complete a flow, like Save, Join now, or Confirm.
+    An iOS-style button filled with default background color.
 
     Example:
     ```
@@ -17,24 +17,26 @@ class CupertinoFilledButton(CupertinoButton):
 
 
     def main(page: ft.Page):
-        page.title = "Basic filled buttons"
         page.add(
-            ft.FilledButton(text="Filled button"),
-            ft.FilledButton("Disabled button", disabled=True),
-            ft.FilledButton("Button with icon", icon="add"),
+            ft.CupertinoFilledButton(text="OK"),
         )
 
     ft.app(target=main)
     ```
-
     -----
 
-    Online docs: https://flet.dev/docs/controls/filledbutton
+    Online docs: https://flet.dev/docs/controls/cupertinofilledbutton
     """
 
     def __init__(
         self,
         text: Optional[str] = None,
+        disabled_color: Optional[str] = None,
+        opacity_on_click: OptionalNumber = None,
+        min_size: OptionalNumber = None,
+        padding: PaddingValue = None,
+        alignment: Optional[Alignment] = None,
+        border_radius: BorderRadiusValue = None,
         ref: Optional[Ref] = None,
         key: Optional[str] = None,
         width: OptionalNumber = None,
@@ -50,17 +52,12 @@ class CupertinoFilledButton(CupertinoButton):
         #
         # Specific
         #
-        # adaptive: Optional[bool] = None,
         icon: Optional[str] = None,
         icon_color: Optional[str] = None,
-        # style: Optional[ButtonStyle] = None,
         content: Optional[Control] = None,
-        # autofocus: Optional[bool] = None,
         url: Optional[str] = None,
         url_target: Optional[str] = None,
         on_click=None,
-        # on_long_press=None,
-        # on_hover=None,
     ):
         CupertinoButton.__init__(
             self,
@@ -81,17 +78,17 @@ class CupertinoFilledButton(CupertinoButton):
             #
             color="onPrimary",
             bgcolor="primary",
-            # elevation=0,
-            # style=style,
+            disabled_color=disabled_color,
             text=text,
             icon=icon,
             icon_color=icon_color,
             content=content,
-            # autofocus=autofocus,
             url=url,
             url_target=url_target,
             on_click=on_click,
-            # on_long_press=on_long_press,
-            # on_hover=on_hover,
-            # adaptive=adaptive,
+            border_radius=border_radius,
+            min_size=min_size,
+            opacity_on_click=opacity_on_click,
+            padding=padding,
+            alignment=alignment,
         )
