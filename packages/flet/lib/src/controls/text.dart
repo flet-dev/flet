6143f01aa046de71649065b3db77142a93d6423c
ndonkoHenri <robotcoder4@protonmail.com>
2024-03-01 00:44:02 +0100

    update textAlign prop name

--- packages/flet/lib/src/controls/text.dart
+++ packages/flet/lib/src/controls/text.dart
@@ -95,8 +95,8 @@ class TextControl extends StatelessWidget with FletStoreMixin {
             Theme.of(context), control.attrString("bgcolor", "")!),
       );
 
-      TextAlign textAlign =
-          parseTextAlign(control.attrString("align", "")!, TextAlign.start)!;
+      TextAlign textAlign = parseTextAlign(
+          control.attrString("textAlign", "")!, TextAlign.start)!;
 
       TextOverflow overflow = TextOverflow.values.firstWhere(
           (v) =>
