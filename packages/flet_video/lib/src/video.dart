240897e47b9d35bbee33f52fc9b3d297aef4d7e4
bl1nc <bl1nch.pro@gmail.com>
2024-04-21 17:05:58 +0600

    Make VideoController using 'gpu' vo

--- packages/flet_video/lib/src/video.dart
+++ packages/flet_video/lib/src/video.dart
@@ -38,7 +38,8 @@ class _VideoControlState extends State<VideoControl> with FletStoreMixin {
   late final Player player = Player(
     configuration: playerConfig,
   );
-  late final controller = VideoController(player);
+  late final videoControllerConfiguration = VideoControllerConfiguration(vo: 'gpu', hwdec: 'mediacodec');
+  late final controller = VideoController(player, configuration: videoControllerConfiguration);
 
   @override
   void initState() {
