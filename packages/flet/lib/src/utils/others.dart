cf090954d7ceb2bf8d2ea77dd352135bd508ae85
ndonkoHenri <robotcoder4@protonmail.com>
2024-05-05 23:07:51 +0200

    create parseOrientation util

--- packages/flet/lib/src/utils/others.dart
+++ packages/flet/lib/src/utils/others.dart
@@ -1,6 +1,5 @@
-import 'dart:ui';
-
 import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
 
 Clip? parseClip(String? clip, [Clip? defaultValue]) {
   if (clip == null) {
@@ -9,4 +8,14 @@ Clip? parseClip(String? clip, [Clip? defaultValue]) {
   return Clip.values.firstWhereOrNull(
           (e) => e.name.toLowerCase() == clip.toLowerCase()) ??
       defaultValue;
+}
+
+Orientation? parseOrientation(String? orientation,
+    [Orientation? defaultOrientation]) {
+  if (orientation == null) {
+    return defaultOrientation;
+  }
+  return Orientation.values.firstWhereOrNull(
+          (a) => a.name.toLowerCase() == orientation.toLowerCase()) ??
+      defaultOrientation;
 }
