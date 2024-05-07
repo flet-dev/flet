69430206a85a4296441de3c071e1c30084f85fdb
vivek.bruh <62519221+vv2006-mc@users.noreply.github.com>
2024-05-07 13:06:04 +0530

    Changed logic of on and off, Changed the Flashlight.switch function to Flashlight.toggle

--- sdk/python/packages/flet-core/src/flet_core/flashlight.py
+++ sdk/python/packages/flet-core/src/flet_core/flashlight.py
@@ -45,15 +45,19 @@ class Flashlight(Control):
 
     def on(self, wait_timeout: Optional[int] = 3) -> int:
         sr = self.invoke_method("on", wait_for_result=True, wait_timeout=wait_timeout)
-        self.turned_on = True
-        return int(sr)
+
+        if int(sr) == 1:
+            self.turned_on = True
+        return self.turned_on
 
     def off(self, wait_timeout: Optional[int] = 3) -> int:
         sr = self.invoke_method("off", wait_for_result=True, wait_timeout=wait_timeout)
-        self.turned_on = False
-        return int(sr)
 
-    def switch(self, wait_timeout: Optional[int] = 3) -> int:
+        if int(sr) == 1:
+            self.turned_on = False
+        return self.turned_on
+
+    def toggle(self, wait_timeout: Optional[int] = 3) -> int:
         if self.turned_on:
             return self.off(wait_timeout)
         return self.on(wait_timeout)
