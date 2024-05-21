1484c257538162dd4ee30b58bb0b81dc1698fef5
Chetan Sharma <csharma@papersoft-tech.com>
2024-05-21 14:36:47 +0530

    fixed webview flickering issue

--- packages/flet_webview/lib/src/webview.dart
+++ packages/flet_webview/lib/src/webview.dart
@@ -3,10 +3,11 @@ import 'dart:io';
 
 import 'package:flet/flet.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_windows/webview_windows.dart' as web_windows;
 
-class WebViewControl extends StatelessWidget {
+class WebViewControl extends StatefulWidget {
   final Control? parent;
   final Control control;
   final bool parentDisabled;
@@ -20,20 +21,63 @@ class WebViewControl extends StatelessWidget {
       required this.backend});
 
   @override
-  Widget build(BuildContext context) {
-    debugPrint("WebViewControl build: ${control.id}");
+  State<WebViewControl> createState() => _WebViewControlState();
+}
 
-    Widget? result;
+class _WebViewControlState extends State<WebViewControl> {
+  web_windows.WebviewController winController = web_windows.WebviewController();
+  bool windowsWebInitialized = false;
+  Widget? result;
+  Color? bgcolor;
+  String url = "";
 
-    String url = control.attrString("url", "")!;
+  @override
+  void initState() {
+    super.initState();
+
+    url = widget.control.attrString("url", "")!;
     if (url == "") {
-      return const ErrorControl("WebView.url cannot be empty.");
+      result = const ErrorControl("WebView.url cannot be empty.");
     }
 
-    bool javascriptEnabled = control.attrBool("javascriptEnabled", false)!;
-    var bgcolor = HexColor.fromString(
-        Theme.of(context), control.attrString("bgcolor", "")!);
-    String preventLink = control.attrString("preventLink", "")!;
+    if (Platform.isWindows) {
+      winController.initialize().then((value) {
+        winController.loadingState.listen((event) {
+          if (event == web_windows.LoadingState.loading) {
+            widget.backend
+                .triggerControlEvent(widget.control.id, "page_started", url);
+          } else if (event == web_windows.LoadingState.navigationCompleted) {
+            widget.backend
+                .triggerControlEvent(widget.control.id, "page_ended", url);
+          }
+        });
+        winController.onLoadError.listen((error) {
+          widget.backend.triggerControlEvent(
+              widget.control.id, "web_resource_error", error.toString());
+        });
+        winController.loadUrl(url);
+        setState(() {
+          windowsWebInitialized = true;
+        });
+      });
+    }
+    debugPrint("WebViewControl Initialized: ${widget.control.id}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("WebViewControl build: ${widget.control.id}");
+
+    if (result != null) {
+      return result!;
+    }
+
+    bgcolor = HexColor.fromString(
+        Theme.of(context), widget.control.attrString("bgcolor", "")!);
+
+    bool javascriptEnabled =
+        widget.control.attrBool("javascriptEnabled", false)!;
+    String preventLink = widget.control.attrString("preventLink", "")!;
 
     if (Platform.isIOS || Platform.isAndroid) {
       var controller = WebViewController()
@@ -44,14 +88,16 @@ class WebViewControl extends StatelessWidget {
           NavigationDelegate(
             onProgress: (int progress) {},
             onPageStarted: (String url) {
-              backend.triggerControlEvent(control.id, "page_started", url);
+              widget.backend
+                  .triggerControlEvent(widget.control.id, "page_started", url);
             },
             onPageFinished: (String url) {
-              backend.triggerControlEvent(control.id, "page_ended", url);
+              widget.backend
+                  .triggerControlEvent(widget.control.id, "page_ended", url);
             },
             onWebResourceError: (WebResourceError error) {
-              backend.triggerControlEvent(
-                  control.id, "web_resource_error", error.toString());
+              widget.backend.triggerControlEvent(
+                  widget.control.id, "web_resource_error", error.toString());
             },
             onNavigationRequest: (NavigationRequest request) {
               if (preventLink != "" && request.url.startsWith(preventLink)) {
@@ -62,47 +108,45 @@ class WebViewControl extends StatelessWidget {
           ),
         );
       if (bgcolor != null) {
-        controller.setBackgroundColor(bgcolor);
+        controller.setBackgroundColor(bgcolor!);
       }
       controller.loadRequest(Uri.parse(url));
-      result = WebViewWidget(controller: controller);
 
-      return constrainedControl(context, result, parent, control);
+      return constrainedControl(context, WebViewWidget(controller: controller),
+          widget.parent, widget.control);
     } else if (Platform.isWindows) {
-      web_windows.WebviewController controller;
-      controller = web_windows.WebviewController();
-      Future<void> initializeController() async {
-        await controller.initialize();
-        controller
-          ..loadingState.listen((event) {
-            if (event == web_windows.LoadingState.loading) {
-              backend.triggerControlEvent(control.id, "page_started", url);
-            } else if (event == web_windows.LoadingState.navigationCompleted) {
-              backend.triggerControlEvent(control.id, "page_ended", url);
-            }
-          })
-          ..onLoadError.listen((error) {
-            backend.triggerControlEvent(
-                control.id, "web_resource_error", error.toString());
-          })
-          ..setBackgroundColor(bgcolor ?? Colors.transparent)
-          ..loadUrl(url);
-      }
-
-      return FutureBuilder(
-          future: initializeController(),
-          builder: (BuildContext context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return constrainedControl(
-                  context, web_windows.Webview(controller), parent, control);
-            }
-            return Container();
-          });
+      winController.setBackgroundColor(bgcolor ?? Colors.transparent);
+      return Visibility(
+        visible: windowsWebInitialized,
+        replacement: constrainedControl(
+            context,
+            Container(
+              color: bgcolor,
+            ),
+            widget.parent,
+            widget.control),
+        child: constrainedControl(context, web_windows.Webview(winController),
+            widget.parent, widget.control),
+      );
     } else {
       result = const ErrorControl(
           "WebView control is not supported on this platform yet.");
 
-      return constrainedControl(context, result, parent, control);
+      return constrainedControl(
+          context, result!, widget.parent, widget.control);
     }
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the web view controller for Windows if initialized
+    if (Platform.isWindows && windowsWebInitialized) {
+      winController.dispose();
+    }
+
+    // You might also need to dispose other controllers or streams
+    // if they are being used on other platforms or in other scenarios.
+
+    super.dispose();
   }
 }
