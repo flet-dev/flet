d97a135782a18fe935a6ec7e70e16175ab243531
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2024-05-23 21:25:20 +0200

    `Map` Control (#3093)
    * Map: initial commit
    
    * SafeArea: deprecate minimum and rename to minimum_padding
    
    * parseDoubleFromJson, parseIntFromJson, parseBoolFromJson
    
    * parseColorFromJson
    
    * Map utils: parseMapOptions, parseMapChildren
    
    * Map: MapOption, MapTileLayer
    
    * RichAttribution: initial commit
    
    * TextSourceAttribution: initial commit
    
    * MapTileLayer: separate from map
    
    * Map: update create_control.dart
    
    * alignment utils: add defValue
    
    * Stack: create before_update()
    
    * MapSimpleAttribution: initial commit
    
    * MapRichAttribution: renamed
    
    * MapCircleLayer: initial commit
    
    * MapMarkerLayer: initial commit
    
    * MapTileLayer: update props
    
    * MapTextSourceAttribution: renamed
    
    * Cleanup
    
    * MapOption: renamed to MapConfiguration
    
    * Create map module
    
    * TileLayer: error_image_src, on_image_error
    
    * MapConfiguration: implemented as control separate module
    
    * MapEventSource enum
    
    * rename point to location
    
    * rename Circle to CircleMarker, add docstrings, remove opacity
    
    * remove parseIntFromJson, parseBoolFromJson, parseDoubleFromJson
    
    * MapInteractionConfiguration and parseInteractionOptions
    
    * add default values
    
    * TileLayer: enable_retina_mode, tile_bounds, evict_error_tile_strategy, subdomains, additional_options
    
    * MapInteractionConfiguration: flags, rotation_win_gestures, pinch_move_win_gestures, pinch_zoom_win_gestures
    
    * move StrokeCap to types.py | create StrokeJoin
    
    * implement Polyline* and Polygon*
    
    * Bump deps
    
    ---------
    
    Co-authored-by: Feodor Fitsner <feodor@appveyor.com>

---
+++ packages/flet_map/lib/src/create_control.dart
@@ -0,0 +1,74 @@
+import 'package:flet/flet.dart';
+
+import 'circle_layer.dart';
+import 'map.dart';
+import 'marker_layer.dart';
+import 'polygon_layer.dart';
+import 'polyline_layer.dart';
+import 'rich_attribution.dart';
+import 'simple_attribution.dart';
+import 'tile_layer.dart';
+
+CreateControlFactory createControl = (CreateControlArgs args) {
+  switch (args.control.type) {
+    case "map":
+      return MapControl(
+        parent: args.parent,
+        control: args.control,
+        children: args.children,
+        parentDisabled: args.parentDisabled,
+        backend: args.backend,
+      );
+    case "maprichattribution":
+      return RichAttributionControl(
+        parent: args.parent,
+        control: args.control,
+        children: args.children,
+        backend: args.backend,
+      );
+    case "mapsimpleattribution":
+      return SimpleAttributionControl(
+        parent: args.parent,
+        control: args.control,
+        children: args.children,
+        backend: args.backend,
+      );
+    case "maptilelayer":
+      return TileLayerControl(
+        parent: args.parent,
+        control: args.control,
+        backend: args.backend,
+      );
+    case "mapmarkerlayer":
+      return MarkerLayerControl(
+        parent: args.parent,
+        control: args.control,
+        children: args.children,
+        parentDisabled: args.parentDisabled,
+      );
+    case "mapcirclelayer":
+      return CircleLayerControl(
+        parent: args.parent,
+        control: args.control,
+        children: args.children,
+      );
+    case "mappolygonlayer":
+      return PolygonLayerControl(
+        parent: args.parent,
+        control: args.control,
+        children: args.children,
+      );
+    case "mappolylinelayer":
+      return PolylineLayerControl(
+        parent: args.parent,
+        control: args.control,
+        children: args.children,
+      );
+    default:
+      return null;
+  }
+};
+
+void ensureInitialized() {
+  // nothing to initialize
+}
