d9492192ba545a041fb8f479e2951827f2ded758
ndonkoHenri <robotcoder4@protonmail.com>
2024-05-31 17:38:56 +0200

    Usercontrol: deprecation well done

--- sdk/python/packages/flet-core/src/flet_core/user_control.py
+++ sdk/python/packages/flet-core/src/flet_core/user_control.py
@@ -2,15 +2,12 @@ from flet_core.stack import Stack
 from flet_core.utils import deprecated
 
 
+@deprecated(
+    reason="UserControl is deprecated. See https://flet.dev/docs/getting-started/custom-controls.",
+    version="0.21.0",
+    delete_version="1.0",
+)
 class UserControl(Stack):
-    @deprecated(
-        reason="UserControl is deprecated. See https://flet.dev/docs/getting-started/custom-controls.",
-        version="0.21.0",
-        delete_version="1.0",
-    )
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def build(self):
         pass
 
