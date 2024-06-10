1adcf9f8ce78c5c8bb2e816fd353b9798389bbef
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2024-06-10 19:46:40 +0200

    TextStyle: overflow, word_spacing, baseline (#3435)

--- sdk/python/packages/flet-core/src/flet_core/text_style.py
+++ sdk/python/packages/flet-core/src/flet_core/text_style.py
@@ -8,6 +8,36 @@ from flet_core.shadow import BoxShadow
 from flet_core.types import FontWeight
 
 
+class TextOverflow(Enum):
+    CLIP = "clip"
+    ELLIPSIS = "ellipsis"
+    FADE = "fade"
+    VISIBLE = "visible"
+
+
+class TextBaseline(Enum):
+    ALPHABETIC = "alphabetic"
+    IDEOGRAPHIC = "ideographic"
+
+
+class TextThemeStyle(Enum):
+    DISPLAY_LARGE = "displayLarge"
+    DISPLAY_MEDIUM = "displayMedium"
+    DISPLAY_SMALL = "displaySmall"
+    HEADLINE_LARGE = "headlineLarge"
+    HEADLINE_MEDIUM = "headlineMedium"
+    HEADLINE_SMALL = "headlineSmall"
+    TITLE_LARGE = "titleLarge"
+    TITLE_MEDIUM = "titleMedium"
+    TITLE_SMALL = "titleSmall"
+    LABEL_LARGE = "labelLarge"
+    LABEL_MEDIUM = "labelMedium"
+    LABEL_SMALL = "labelSmall"
+    BODY_LARGE = "bodyLarge"
+    BODY_MEDIUM = "bodyMedium"
+    BODY_SMALL = "bodySmall"
+
+
 class TextDecoration(IntFlag):
     NONE = 0
     UNDERLINE = 1
@@ -39,3 +69,6 @@ class TextStyle:
     shadow: Union[None, BoxShadow, List[BoxShadow]] = field(default=None)
     foreground: Optional[Paint] = field(default=None)
     letter_spacing: OptionalNumber = field(default=None)
+    word_spacing: OptionalNumber = field(default=None)
+    overflow: Optional[TextOverflow] = field(default=None)
+    baseline: Optional[TextBaseline] = field(default=None)
