c9789f0b16de43951bd5892e57075b391a5a4d4f
Feodor Fitsner <feodor@appveyor.com>
2024-06-12 10:05:22 -0700

    New property: `dropdown.Option.content`

--- sdk/python/packages/flet-core/src/flet_core/dropdown.py
+++ sdk/python/packages/flet-core/src/flet_core/dropdown.py
@@ -1,5 +1,5 @@
 import time
-from typing import Any, Optional, Union, List
+from typing import Any, List, Optional, Union
 
 from flet_core.alignment import Alignment
 from flet_core.control import Control, OptionalNumber
@@ -23,6 +23,7 @@ class Option(Control):
         self,
         key: Optional[str] = None,
         text: Optional[str] = None,
+        content: Optional[Control] = None,
         alignment: Optional[Alignment] = None,
         text_style: Optional[TextStyle] = None,
         on_click=None,
@@ -38,12 +39,20 @@ class Option(Control):
         assert key is not None or text is not None, "key or text must be specified"
         self.key = key
         self.text = text
+        self.content = content
         self.on_click = on_click
         self.alignment = alignment
         self.text_style = text_style
 
     def _get_control_name(self):
         return "dropdownoption"
+
+    def _get_children(self):
+        children = []
+        if self.__content is not None:
+            self.__content._set_attr_internal("n", "content")
+            children.append(self.__content)
+        return children
 
     def before_update(self):
         super().before_update()
@@ -68,6 +77,15 @@ class Option(Control):
     @text.setter
     def text(self, value: Optional[str]):
         self._set_attr("text", value)
+
+    # content
+    @property
+    def content(self) -> Optional[Control]:
+        return self.__content
+
+    @content.setter
+    def content(self, value: Optional[Control]):
+        self.__content = value
 
     # alignment
     @property
