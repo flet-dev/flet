4eb75b7a5679d545a4b24a1591cfae9b1b1ec39b
ndonkoHenri <robotcoder4@protonmail.com>
2024-07-15 07:02:47 +0200

    switch to ConstrainedControl

--- sdk/python/packages/flet-core/src/flet_core/placeholder.py
+++ sdk/python/packages/flet-core/src/flet_core/placeholder.py
@@ -1,10 +1,20 @@
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
-from flet_core.control import Control, OptionalNumber
+from flet_core.constrained_control import ConstrainedControl
+from flet_core.control import Control
+from flet_core.control import OptionalNumber
 from flet_core.ref import Ref
+from flet_core.types import (
+    AnimationValue,
+    OffsetValue,
+    ResponsiveNumber,
+    RotateValue,
+    ScaleValue,
+    OptionalEventCallable,
+)
 
 
-class Placeholder(Control):
+class Placeholder(ConstrainedControl):
     """
     A placeholder box.
 
@@ -21,22 +31,67 @@ class Placeholder(Control):
         fallback_width: OptionalNumber = None,
         stroke_width: OptionalNumber = None,
         #
-        # Control
+        # ConstrainedControl
         #
         ref: Optional[Ref] = None,
+        key: Optional[str] = None,
+        width: OptionalNumber = None,
+        height: OptionalNumber = None,
+        left: OptionalNumber = None,
+        top: OptionalNumber = None,
+        right: OptionalNumber = None,
+        bottom: OptionalNumber = None,
+        expand: Union[None, bool, int] = None,
+        expand_loose: Optional[bool] = None,
+        col: Optional[ResponsiveNumber] = None,
         opacity: OptionalNumber = None,
+        rotate: RotateValue = None,
+        scale: ScaleValue = None,
+        offset: OffsetValue = None,
+        aspect_ratio: OptionalNumber = None,
+        animate_opacity: AnimationValue = None,
+        animate_size: AnimationValue = None,
+        animate_position: AnimationValue = None,
+        animate_rotation: AnimationValue = None,
+        animate_scale: AnimationValue = None,
+        animate_offset: AnimationValue = None,
+        on_animation_end: OptionalEventCallable = None,
         tooltip: Optional[str] = None,
         visible: Optional[bool] = None,
+        disabled: Optional[bool] = None,
         data: Any = None,
+        rtl: Optional[bool] = None,
     ):
-
-        Control.__init__(
+        ConstrainedControl.__init__(
             self,
             ref=ref,
+            key=key,
+            width=width,
+            height=height,
+            left=left,
+            top=top,
+            right=right,
+            bottom=bottom,
+            expand=expand,
+            expand_loose=expand_loose,
+            col=col,
             opacity=opacity,
+            rotate=rotate,
+            scale=scale,
+            offset=offset,
+            aspect_ratio=aspect_ratio,
+            animate_opacity=animate_opacity,
+            animate_size=animate_size,
+            animate_position=animate_position,
+            animate_rotation=animate_rotation,
+            animate_scale=animate_scale,
+            animate_offset=animate_offset,
+            on_animation_end=on_animation_end,
             tooltip=tooltip,
             visible=visible,
+            disabled=disabled,
             data=data,
+            rtl=rtl,
         )
 
         self.content = content
@@ -53,7 +108,7 @@ class Placeholder(Control):
 
     # fallback_height
     @property
-    def fallback_height(self) -> OptionalNumber:
+    def fallback_height(self) -> float:
         return self._get_attr("fallbackHeight", data_type="float", def_value=400.0)
 
     @fallback_height.setter
@@ -63,7 +118,7 @@ class Placeholder(Control):
 
     # fallback_width
     @property
-    def fallback_width(self) -> OptionalNumber:
+    def fallback_width(self) -> float:
         return self._get_attr("fallbackWidth", data_type="float", def_value=400.0)
 
     @fallback_width.setter
@@ -73,7 +128,7 @@ class Placeholder(Control):
 
     # stroke_width
     @property
-    def stroke_width(self) -> OptionalNumber:
+    def stroke_width(self) -> float:
         return self._get_attr("strokeWidth", data_type="float", def_value=2.0)
 
     @stroke_width.setter
@@ -82,7 +137,7 @@ class Placeholder(Control):
 
     # color
     @property
-    def color(self) -> Optional[str]:
+    def color(self) -> str:
         return self._get_attr("color", def_value="bluegrey700")
 
     @color.setter
