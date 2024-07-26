c2d8dff4c45d56531707f4751220cf8543527c73
vv2006-mc <mullangivivekananda@gmail.com>
2024-07-26 23:12:09 +0530

    updated docstring

--- sdk/python/packages/flet-core/src/flet_core/local_authentication/local_authentication.py
+++ sdk/python/packages/flet-core/src/flet_core/local_authentication/local_authentication.py
@@ -9,7 +9,7 @@ from .linux_authentication import LinuxLocalAuth
 
 class LocalAuthentication(Control):
     """
-    A control to use native authentication. Works on iOS, Android and Windows. Based on local_auth Flutter widget (https://pub.dev/packages/local_auth).
+    A control to use native authentication. Works on iOS, Android, Windows, MacOS, Linux. Based on local_auth Flutter widget (https://pub.dev/packages/local_auth).
     LocalAuthentication control is non-visual and should be added to `page.overlay` list.
     Example:
     ```
@@ -19,7 +19,7 @@ class LocalAuthentication(Control):
         def on_click(e):
             print(
                 auth.authenticate(
-                    title="title", biometricsOnly=False, useErrorDialogs=True
+                    title="title", biometricsOnly=False
                 )
             )
         page.overlay.append(auth)
@@ -44,6 +44,8 @@ class LocalAuthentication(Control):
 
     def before_update(self):
         self.platform = self.page.platform
+        # Both macOS and Linux have platform dependant dependencies.
+        # Hence initiating MacLocalAuth and LinuxLocalAuth after checking the platform
         if self.platform == PagePlatform.MACOS:
             self.maclocalauth = MacLocalAuth()
         elif self.platform == PagePlatform.LINUX:
@@ -54,6 +56,8 @@ class LocalAuthentication(Control):
         if self.platform == PagePlatform.MACOS:
             sr = self.maclocalauth.is_available()
         elif self.platform == PagePlatform.LINUX:
+            # linux always has a user with password.
+            # Even if not so, a popup asking to authenticate without asking password will be shown
             sr = True
         else:
             sr = (
@@ -69,6 +73,8 @@ class LocalAuthentication(Control):
         if self.platform == PagePlatform.MACOS:
             sr = await self.maclocalauth.is_available()
         elif self.platform == PagePlatform.LINUX:
+            # linux always has a user with password.
+            # Even if not so, a popup asking to authenticate without asking password will be shown
             sr = True
         else:
             sr = (
