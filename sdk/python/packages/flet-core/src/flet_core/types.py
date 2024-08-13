33625df7ed306237e5e571ca4e523322ed2e61ae
ndonkoHenri <robotcoder4@protonmail.com>
2024-08-13 13:15:13 +0200

    revert FLET_APP_* variables

--- sdk/python/packages/flet-core/src/flet_core/types.py
+++ sdk/python/packages/flet-core/src/flet_core/types.py
@@ -18,6 +18,11 @@ from flet_core.margin import Margin
 from flet_core.padding import Padding
 from flet_core.transform import Offset, Rotate, Scale
 
+WEB_BROWSER = "web_browser"
+FLET_APP = "flet_app"
+FLET_APP_WEB = "flet_app_web"
+FLET_APP_HIDDEN = "flet_app_hidden"
+
 
 class AppView(Enum):
     WEB_BROWSER = "web_browser"
--- sdk/python/packages/flet-pyodide/src/flet/__init__.py
+++ sdk/python/packages/flet-pyodide/src/flet/__init__.py
@@ -1,6 +1,1 @@
-from flet.flet import app, app_async
-from flet_core import *
-from flet_core.types import (
-    AppView,
-    WebRenderer,
-)
+
