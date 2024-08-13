70d2a9de86dc4042b11aa41d8f6f52a6f4aa00c5
ndonkoHenri <robotcoder4@protonmail.com>
2024-08-13 13:14:08 +0200

    Axis enum

--- sdk/python/packages/flet-core/src/flet_core/__init__.py
+++ sdk/python/packages/flet-core/src/flet_core/__init__.py
@@ -17,7 +17,7 @@ from flet_core import (
 )
 from flet_core.adaptive_control import AdaptiveControl
 from flet_core.alert_dialog import AlertDialog
-from flet_core.alignment import Alignment
+from flet_core.alignment import Alignment, Axis
 from flet_core.animated_switcher import AnimatedSwitcher, AnimatedSwitcherTransition
 from flet_core.animation import Animation, AnimationCurve
 from flet_core.app_bar import AppBar
