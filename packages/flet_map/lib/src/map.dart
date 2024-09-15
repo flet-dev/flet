ad89a991dcd5b7be1b9c6a7bc36faceb2a8f923b
ndonkoHenri <robotcoder4@protonmail.com>
2024-09-15 17:18:08 +0200

    make MapConfiguration a dataclass (previously a Control)

--- packages/flet_map/lib/src/map.dart
+++ packages/flet_map/lib/src/map.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
 import 'package:flet/flet.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_animations/flutter_map_animations.dart';
 import 'package:latlong2/latlong.dart';
@@ -38,6 +37,12 @@ class _MapControlState extends State<MapControl>
     super.dispose();
   }
 
+  Duration? durationFromString(String? duration, [Duration? defaultValue]) {
+    return duration != null
+        ? durationFromJSON(json.decode(duration), defaultValue)
+        : defaultValue;
+  }
+
   @override
   Widget build(BuildContext context) {
     debugPrint("Map build: ${widget.control.id} (${widget.control.hashCode})");
@@ -55,250 +60,87 @@ class _MapControlState extends State<MapControl>
         .where((c) => c.isVisible && (acceptedChildrenTypes.contains(c.type)))
         .toList();
 
-    void triggerEvent(Control ctrl, String eventName, dynamic eventData) {
-      var d = "";
-      if (eventData is String) {
-        d = eventData;
-      } else if (eventData is Map) {
-        d = json.encode(eventData);
-      }
-
-      widget.backend.triggerControlEvent(ctrl.id, eventName, d);
-    }
-
     Curve? defaultAnimationCurve;
     Duration? defaultAnimationDuration;
-    var onTap = widget.control.attrBool("onTap", false)!;
-    var onLongPress = widget.control.attrBool("onLongPress", false)!;
-    var onSecondaryTap = widget.control.attrBool("onSecondaryTap", false)!;
-    var onMapEvent = widget.control.attrBool("onEvent", false)!;
-    var onInit = widget.control.attrBool("onInit", false)!;
-    var onPointerDown = widget.control.attrBool("onPointerDown", false)!;
-    var onPointerHover = widget.control.attrBool("onHover", false)!;
-    var onPointerCancel = widget.control.attrBool("onPointerCancel", false)!;
-    var onPointerUp = widget.control.attrBool("onPointerUp", false)!;
-    var onPositionChange = widget.control.attrBool("onPositionChange", false)!;
+    var configuration = parseConfiguration(widget.control, "configuration",
+        widget.backend, Theme.of(context), const MapOptions())!;
 
-    return withControls(widget.control.childIds, (context, configurationsView) {
-      var configuration = configurationsView.controlViews
-          .where((c) => c.control.type == "map_configuration")
-          .map((config) {
-        defaultAnimationCurve =
-            parseCurve(config.control.attrString("animationCurve"));
-        defaultAnimationDuration =
-            parseDuration(config.control, "animationDuration");
-        return MapOptions(
-          initialCenter: parseLatLng(
-              config.control, "initialCenter", const LatLng(50.5, 30.51))!,
-          interactionOptions: parseInteractionOptions(config.control,
-              "interactionConfiguration", const InteractionOptions())!,
-          backgroundColor: config.control
-              .attrColor("backgroundColor", context, const Color(0x00000000))!,
-          initialRotation: config.control.attrDouble("initialRotation", 0.0)!,
-          initialZoom: config.control.attrDouble("initialZoom", 13.0)!,
-          keepAlive: config.control.attrBool("keepAlive", false)!,
-          maxZoom: config.control.attrDouble("maxZoom"),
-          minZoom: config.control.attrDouble("minZoom"),
-          onPointerHover: onPointerHover
-              ? (PointerHoverEvent e, LatLng latlng) {
-                  triggerEvent(widget.control, "hover", {
-                    "lat": latlng.latitude,
-                    "long": latlng.longitude,
-                    "gx": e.position.dx,
-                    "gy": e.position.dy,
-                    "lx": e.localPosition.dx,
-                    "ly": e.localPosition.dy,
-                    "kind": e.kind.name,
-                  });
-                }
-              : null,
-          onTap: onTap
-              ? (TapPosition pos, LatLng latlng) {
-                  triggerEvent(widget.control, "tap", {
-                    "lat": latlng.latitude,
-                    "long": latlng.longitude,
-                    "gx": pos.global.dx,
-                    "gy": pos.global.dy,
-                    "lx": pos.relative?.dx,
-                    "ly": pos.relative?.dy,
-                  });
-                }
-              : null,
-          onLongPress: onLongPress
-              ? (TapPosition pos, LatLng latlng) {
-                  triggerEvent(widget.control, "long_press", {
-                    "lat": latlng.latitude,
-                    "long": latlng.longitude,
-                    "gx": pos.global.dx,
-                    "gy": pos.global.dy,
-                    "lx": pos.relative?.dx,
-                    "ly": pos.relative?.dy,
-                  });
-                }
-              : null,
-          onPositionChanged: onPositionChange
-              ? (MapCamera camera, bool hasGesture) {
-                  triggerEvent(widget.control, "position_change", {
-                    "lat": camera.center.latitude,
-                    "long": camera.center.longitude,
-                    "min_zoom": camera.minZoom,
-                    "max_zoom": camera.maxZoom,
-                    "rot": camera.rotation,
-                  });
-                }
-              : null,
-          onPointerDown: onPointerDown
-              ? (PointerDownEvent e, LatLng latlng) {
-                  triggerEvent(widget.control, "pointer_down", {
-                    "lat": latlng.latitude,
-                    "long": latlng.longitude,
-                    "gx": e.position.dx,
-                    "gy": e.position.dy,
-                    "kind": e.kind.name,
-                  });
-                }
-              : null,
-          onPointerCancel: onPointerCancel
-              ? (PointerCancelEvent e, LatLng latlng) {
-                  triggerEvent(widget.control, "pointer_cancel", {
-                    "lat": latlng.latitude,
-                    "long": latlng.longitude,
-                    "gx": e.position.dx,
-                    "gy": e.position.dy,
-                    "kind": e.kind.name,
-                  });
-                }
-              : null,
-          onPointerUp: onPointerUp
-              ? (PointerUpEvent e, LatLng latlng) {
-                  triggerEvent(widget.control, "pointer_up", {
-                    "lat": latlng.latitude,
-                    "long": latlng.longitude,
-                    "gx": e.position.dx,
-                    "gy": e.position.dy,
-                    "kind": e.kind.name,
-                  });
-                }
-              : null,
-          onSecondaryTap: onSecondaryTap
-              ? (TapPosition pos, LatLng latlng) {
-                  triggerEvent(widget.control, "secondary_tap", {
-                    "lat": latlng.latitude,
-                    "long": latlng.longitude,
-                    "gx": pos.global.dx,
-                    "gy": pos.global.dy,
-                    "lx": pos.relative?.dx,
-                    "ly": pos.relative?.dy,
-                  });
-                }
-              : null,
-          onMapEvent: onMapEvent
-              ? (MapEvent e) {
-                  triggerEvent(widget.control, "event", {
-                    "src": e.source.name,
-                    "c_lat": e.camera.center.latitude,
-                    "c_long": e.camera.center.longitude,
-                    "zoom": e.camera.zoom,
-                    "min_zoom": e.camera.minZoom,
-                    "max_zoom": e.camera.maxZoom,
-                    "rot": e.camera.rotation,
-                  });
-                }
-              : null,
-          onMapReady: onInit
-              ? () {
-                  debugPrint("Map ${widget.control.id} init");
-                  widget.backend.triggerControlEvent(widget.control.id, "init");
-                }
-              : null,
-        );
-      });
+    Widget map = FlutterMap(
+      mapController: _animatedMapController.mapController,
+      options: configuration,
+      children: ctrls
+          .map((c) => createControl(widget.control, c.id, disabled))
+          .toList(),
+    );
 
-      Widget map = FlutterMap(
-        mapController: _animatedMapController.mapController,
-        options: configuration.first,
-        children: ctrls
-            .map((c) => createControl(widget.control, c.id, disabled))
-            .toList(),
-      );
-
-      Duration? durationFromString(String? duration, [Duration? defaultValue]) {
-        if (duration != null) {
-          return durationFromJSON(json.decode(duration), defaultValue);
-        }
-        return null;
-      }
-
-      () async {
-        widget.backend.subscribeMethods(widget.control.id,
-            (methodName, args) async {
-          switch (methodName) {
-            case "rotate_from":
-              var degree = parseDouble(args["degree"]);
-              if (degree != null) {
-                _animatedMapController.animatedRotateFrom(
-                  degree,
-                  curve: parseCurve(args["curve"]) ?? defaultAnimationCurve,
-                );
-              }
-            case "reset_rotation":
-              _animatedMapController.animatedRotateReset(
+    () async {
+      widget.backend.subscribeMethods(widget.control.id,
+          (methodName, args) async {
+        switch (methodName) {
+          case "rotate_from":
+            var degree = parseDouble(args["degree"]);
+            if (degree != null) {
+              _animatedMapController.animatedRotateFrom(
+                degree,
+                curve: parseCurve(args["curve"]) ?? defaultAnimationCurve,
+              );
+            }
+          case "reset_rotation":
+            _animatedMapController.animatedRotateReset(
+                curve: parseCurve(args["curve"], defaultAnimationCurve),
+                duration: durationFromString(
+                    args["duration"], defaultAnimationDuration));
+          case "zoom_in":
+            _animatedMapController.animatedZoomIn(
+                curve: parseCurve(args["curve"], defaultAnimationCurve),
+                duration: durationFromString(
+                    args["duration"], defaultAnimationDuration));
+          case "zoom_out":
+            _animatedMapController.animatedZoomOut(
+                curve: parseCurve(args["curve"], defaultAnimationCurve),
+                duration: durationFromString(
+                    args["duration"], defaultAnimationDuration));
+          case "zoom_to":
+            var zoom = parseDouble(args["zoom"]);
+            if (zoom != null) {
+              _animatedMapController.animatedZoomTo(zoom,
                   curve: parseCurve(args["curve"], defaultAnimationCurve),
                   duration: durationFromString(
                       args["duration"], defaultAnimationDuration));
-            case "zoom_in":
-              _animatedMapController.animatedZoomIn(
-                  curve: parseCurve(args["curve"], defaultAnimationCurve),
-                  duration: durationFromString(
-                      args["duration"], defaultAnimationDuration));
-            case "zoom_out":
-              _animatedMapController.animatedZoomOut(
-                  curve: parseCurve(args["curve"], defaultAnimationCurve),
-                  duration: durationFromString(
-                      args["duration"], defaultAnimationDuration));
-            case "zoom_to":
-              var zoom = parseDouble(args["zoom"]);
-              if (zoom != null) {
-                _animatedMapController.animatedZoomTo(zoom,
-                    curve: parseCurve(args["curve"], defaultAnimationCurve),
-                    duration: durationFromString(
-                        args["duration"], defaultAnimationDuration));
-              }
-            case "move_to":
-              var zoom = parseDouble(args["zoom"]);
-              var lat = parseDouble(args["lat"]);
-              var long = parseDouble(args["long"]);
-              var ox = parseDouble(args["ox"]);
-              var oy = parseDouble(args["oy"]);
-              _animatedMapController.animateTo(
+            }
+          case "move_to":
+            var zoom = parseDouble(args["zoom"]);
+            var lat = parseDouble(args["lat"]);
+            var long = parseDouble(args["long"]);
+            var ox = parseDouble(args["ox"]);
+            var oy = parseDouble(args["oy"]);
+            _animatedMapController.animateTo(
+              zoom: zoom,
+              curve: parseCurve(args["curve"], defaultAnimationCurve),
+              rotation: parseDouble(args["rot"]),
+              duration: durationFromString(
+                  args["duration"], defaultAnimationDuration),
+              dest: (lat != null && long != null) ? LatLng(lat, long) : null,
+              offset: (ox != null && oy != null) ? Offset(ox, oy) : Offset.zero,
+            );
+          case "center_on":
+            var zoom = parseDouble(args["zoom"]);
+            var lat = parseDouble(args["lat"]);
+            var long = parseDouble(args["long"]);
+            if (lat != null && long != null) {
+              _animatedMapController.centerOnPoint(
+                LatLng(lat, long),
                 zoom: zoom,
                 curve: parseCurve(args["curve"], defaultAnimationCurve),
-                rotation: parseDouble(args["rot"]),
                 duration: durationFromString(
                     args["duration"], defaultAnimationDuration),
-                dest: (lat != null && long != null) ? LatLng(lat, long) : null,
-                offset:
-                    (ox != null && oy != null) ? Offset(ox, oy) : Offset.zero,
               );
-            case "center_on":
-              var zoom = parseDouble(args["zoom"]);
-              var lat = parseDouble(args["lat"]);
-              var long = parseDouble(args["long"]);
-              if (lat != null && long != null) {
-                _animatedMapController.centerOnPoint(
-                  LatLng(lat, long),
-                  zoom: zoom,
-                  curve: parseCurve(args["curve"], defaultAnimationCurve),
-                  duration: durationFromString(
-                      args["duration"], defaultAnimationDuration),
-                );
-              }
-          }
-          return null;
-        });
-      }();
+            }
+        }
+        return null;
+      });
+    }();
 
-      return constrainedControl(context, map, widget.parent, widget.control);
-    });
+    return constrainedControl(context, map, widget.parent, widget.control);
   }
 }
--- sdk/python/packages/flet-core/src/flet_core/map/map_configuration.py
+++ sdk/python/packages/flet-core/src/flet_core/map/map_configuration.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass, field
-from dataclasses import dataclass, field
 from enum import IntFlag
 from typing import Optional, Union
 
 from flet_core.animation import AnimationCurve
-from flet_core.control import OptionalNumber, Control
+from flet_core.control import OptionalNumber
 from flet_core.types import (
     DurationValue,
 )
@@ -65,131 +64,15 @@ class MapInteractionConfiguration:
     pinch_zoom_win_gestures: Optional[MapMultiFingerGesture] = field(default=None)
 
 
-class MapConfiguration(Control):
-    def __init__(
-        self,
-        initial_center: Optional[MapLatitudeLongitude] = None,
-        initial_rotation: OptionalNumber = None,
-        initial_zoom: OptionalNumber = None,
-        interaction_configuration: Optional[MapInteractionConfiguration] = None,
-        bgcolor: Optional[str] = None,
-        keep_alive: Optional[bool] = None,
-        max_zoom: OptionalNumber = None,
-        min_zoom: OptionalNumber = None,
-        animation_curve: Optional[AnimationCurve] = None,
-        animation_duration: DurationValue = None,
-    ):
-        Control.__init__(self)
-
-        self.bgcolor = bgcolor
-        self.initial_center = initial_center
-        self.initial_rotation = initial_rotation
-        self.initial_zoom = initial_zoom
-        self.interaction_configuration = interaction_configuration
-        self.keep_alive = keep_alive
-        self.max_zoom = max_zoom
-        self.min_zoom = min_zoom
-        self.animation_curve = animation_curve
-        self.animation_duration = animation_duration
-
-    def _get_control_name(self):
-        return "map_configuration"
-
-    def before_update(self):
-        super().before_update()
-        self._set_attr_json("initialCenter", self.__initial_center)
-        self._set_attr_json(
-            "interactionConfiguration", self.__interaction_configuration
-        )
-        self._set_attr_json("animationDuration", self.__animation_duration)
-
-    # bgcolor
-    @property
-    def bgcolor(self) -> Optional[str]:
-        return self._get_attr("bgcolor")
-
-    @bgcolor.setter
-    def bgcolor(self, value: Optional[str]):
-        self._set_attr("bgcolor", value)
-
-    # initial_center
-    @property
-    def initial_center(self) -> Optional[MapLatitudeLongitude]:
-        return self.__initial_center
-
-    @initial_center.setter
-    def initial_center(self, value: Optional[MapLatitudeLongitude]):
-        self.__initial_center = value
-
-    # interaction_configuration
-    @property
-    def interaction_configuration(self) -> Optional[MapInteractionConfiguration]:
-        return self.__interaction_configuration
-
-    @interaction_configuration.setter
-    def interaction_configuration(self, value: Optional[MapInteractionConfiguration]):
-        self.__interaction_configuration = value
-
-    # initial_rotation
-    @property
-    def initial_rotation(self) -> float:
-        return self._get_attr("initialRotation", data_type="float", def_value=0.0)
-
-    @initial_rotation.setter
-    def initial_rotation(self, value: OptionalNumber):
-        self._set_attr("initialRotation", value)
-
-    # initial_zoom
-    @property
-    def initial_zoom(self) -> float:
-        return self._get_attr("initialZoom", data_type="float", def_value=13.0)
-
-    @initial_zoom.setter
-    def initial_zoom(self, value: OptionalNumber):
-        self._set_attr("initialZoom", value)
-
-    # keep_alive
-    @property
-    def keep_alive(self) -> bool:
-        return self._get_attr("keepAlive", data_type="bool", def_value=False)
-
-    @keep_alive.setter
-    def keep_alive(self, value: Optional[bool]):
-        self._set_attr("keepAlive", value)
-
-    # max_zoom
-    @property
-    def max_zoom(self) -> OptionalNumber:
-        return self._get_attr("maxZoom", data_type="float")
-
-    @max_zoom.setter
-    def max_zoom(self, value: OptionalNumber):
-        self._set_attr("maxZoom", value)
-
-    # min_zoom
-    @property
-    def min_zoom(self) -> OptionalNumber:
-        return self._get_attr("minZoom", data_type="float")
-
-    @min_zoom.setter
-    def min_zoom(self, value: OptionalNumber):
-        self._set_attr("minZoom", value)
-
-    # animation_duration
-    @property
-    def animation_duration(self) -> DurationValue:
-        return self.__animation_duration
-
-    @animation_duration.setter
-    def animation_duration(self, value: DurationValue):
-        self.__animation_duration = value
-
-    # animation_curve
-    @property
-    def animation_curve(self) -> AnimationCurve:
-        return self.__animation_curve
-
-    @animation_curve.setter
-    def animation_curve(self, value: AnimationCurve):
-        self.__animation_curve = value
-        self._set_enum_attr("animationCurve", value, AnimationCurve)
+@dataclass
+class MapConfiguration:
+    initial_center: Optional[MapLatitudeLongitude] = None
+    initial_rotation: OptionalNumber = None
+    initial_zoom: OptionalNumber = None
+    interaction_configuration: Optional[MapInteractionConfiguration] = None
+    bgcolor: Optional[str] = None
+    keep_alive: Optional[bool] = None
+    max_zoom: OptionalNumber = None
+    min_zoom: OptionalNumber = None
+    animation_curve: Optional[AnimationCurve] = None
+    animation_duration: DurationValue = None
