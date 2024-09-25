9d583f878c92cc30f57f24df52d381b0e18ff420
Feodor Fitsner <feodor@appveyor.com>
2024-09-25 13:47:53 -0700

    Publish non-main packages to gemfury

--- ci/common.sh
+++ ci/common.sh
@@ -22,5 +22,9 @@ function patch_flet_desktop_package_name() {
 function publish_to_pypi() {
     if [[ ("$APPVEYOR_REPO_BRANCH" == "main" || "$APPVEYOR_REPO_TAG_NAME" != "") && "$APPVEYOR_PULL_REQUEST_NUMBER" == "" ]]; then
         twine upload "$@"
+    elif [[ "$APPVEYOR_PULL_REQUEST_NUMBER" == "" ]]; then
+        for wheel in "$@"; do
+            curl -F package=@$wheel https://$GEMFURY_TOKEN@push.fury.io/flet/
+        done
     fi
 }
