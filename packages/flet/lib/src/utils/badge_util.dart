8d5b9a271cab383ef7cc3020bd35e65f903ec9db
InesaFitsner <inesa@appveyor.com>
2024-09-26 12:41:00 -0700

    Update badge_util.dart

--- packages/flet/lib/src/utils/badge_util.dart
+++ packages/flet/lib/src/utils/badge_util.dart
@@ -7,6 +7,7 @@ Badge? parseBadge(Control control, String propName, Widget widget) {
   var v = control.attrString(propName, null);
   if (v == null) {
     return null;
+    //return Badge(label: const Text("v==null Badge on a widget"), child: widget);
   }
   final j = json.decode(v);
   return badgeFromJSON(j, widget);
@@ -16,7 +17,7 @@ Badge? badgeFromJSON(dynamic j, Widget widget) {
   if (j == null) {
     return null;
   } else if (j is String) {
-    return Badge(label: const Text("Badge on a widget"), child: widget);
+    return Badge(label: const Text("Text Badge on a widget"), child: widget);
   }
-  return null;
+  return Badge(label: const Text("Control Badge on a widget"), child: widget);
 }
