4a0dcb59c001876a330ed0ab3da0341460bb85d1
ndonkoHenri <robotcoder4@protonmail.com>
2024-11-07 17:22:44 +0100

    client generated files

--- client/linux/flutter/generated_plugins.cmake
+++ client/linux/flutter/generated_plugins.cmake
@@ -7,7 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_libs_linux
   media_kit_video
   record_linux
-  screen_retriever
+  screen_retriever_linux
   url_launcher_linux
   window_manager
   window_to_front
