816753bf3b262e1b97af9d0487720f49c245a31c
ndonkoHenri <robotcoder4@protonmail.com>
2024-11-07 02:35:00 +0100

    remove unused import

--- packages/flet_map/lib/src/map.dart
+++ packages/flet_map/lib/src/map.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flet/flet.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_animations/flutter_map_animations.dart';
