f62b5066ab79f3b99241e9c234baeac71fd60f95
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2024-11-29 19:09:59 +0100

    fix imports in flet-cli/hook-flet.py (#4450)

--- sdk/python/packages/flet-cli/src/flet_cli/__pyinstaller/hook-flet.py
+++ sdk/python/packages/flet-cli/src/flet_cli/__pyinstaller/hook-flet.py
@@ -1,7 +1,7 @@
 import os
 
-import flet.__pyinstaller.config as hook_config
-from flet.__pyinstaller.utils import get_flet_bin_path
+import flet_cli.__pyinstaller.config as hook_config
+from flet_cli.__pyinstaller.utils import get_flet_bin_path
 
 bin_path = hook_config.temp_bin_dir
 if not bin_path:
