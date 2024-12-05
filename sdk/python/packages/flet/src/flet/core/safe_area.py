a44df7154a1c86f8e84e6c6d40270ead46b08e9c
ndonkoHenri <robotcoder4@protonmail.com>
2024-12-05 20:31:08 +0100

    remove minimum from SafeArea

--- sdk/python/packages/flet/src/flet/core/safe_area.py
+++ sdk/python/packages/flet/src/flet/core/safe_area.py
@@ -27,7 +27,6 @@ class SafeArea(ConstrainedControl, AdaptiveControl):
         right: Optional[bool] = None,
         bottom: Optional[bool] = None,
         maintain_bottom_view_padding: Optional[bool] = None,
-        minimum: PaddingValue = None,
         minimum_padding: PaddingValue = None,
         #
         # ConstrainedControl
@@ -99,7 +98,6 @@ class SafeArea(ConstrainedControl, AdaptiveControl):
         self.right = right
         self.bottom = bottom
         self.maintain_bottom_view_padding = maintain_bottom_view_padding
-        self.minimum = minimum
         self.minimum_padding = minimum_padding
 
     def _get_control_name(self):
@@ -108,7 +106,6 @@ class SafeArea(ConstrainedControl, AdaptiveControl):
     def before_update(self):
         super().before_update()
         assert self.__content.visible, "content must be visible"
-        self._set_attr_json("minimum", self.__minimum)
         self._set_attr_json("minimumPadding", self.__minimum_padding)
 
     def _get_children(self):
