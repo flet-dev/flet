18bfd092816edae2da5784287a142edcb6e3fa04
ndonkoHenri <robotcoder4@protonmail.com>
2024-12-12 21:46:54 +0100

    resolve ControlState on user-defined order

--- packages/flet/lib/src/utils/material_state.dart
+++ packages/flet/lib/src/utils/material_state.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 
 WidgetStateProperty<T?>? getWidgetStateProperty<T>(
@@ -8,43 +9,42 @@ WidgetStateProperty<T?>? getWidgetStateProperty<T>(
   }
   var j = jsonDictValue;
   if (j is! Map<String, dynamic>) {
-    j = {"": j};
+    j = {"default": j};
   }
   return WidgetStateFromJSON(j, converterFromJson, defaultValue);
 }
 
 class WidgetStateFromJSON<T> extends WidgetStateProperty<T?> {
-  late final Map<String, T> _states;
+  late final LinkedHashMap<String, T> _states;
   late final T? _defaultValue;
 
   WidgetStateFromJSON(Map<String, dynamic>? jsonDictValue,
       T Function(dynamic) converterFromJson, T? defaultValue) {
     _defaultValue = defaultValue;
-    _states = {};
-    if (jsonDictValue != null) {
-      jsonDictValue.forEach((stateStr, jv) {
-        stateStr.split(",").map((s) => s.trim().toLowerCase()).forEach((state) {
-          _states[state] = converterFromJson(jv);
-        });
-      });
-    }
+
+    // preserve user-defined order
+    _states = LinkedHashMap<String, T>.from(
+      jsonDictValue?.map((key, value) {
+            var normalizedKey = key.trim().toLowerCase();
+            // "" is now deprecated; use "default" instead
+            if (normalizedKey == "") normalizedKey = "default";
+            return MapEntry(normalizedKey, converterFromJson(value));
+          }) ??
+          {},
+    );
   }
 
   @override
   T? resolve(Set<WidgetState> states) {
-    //debugPrint("WidgetStateFromJSON states: $states, _states: $_states");
-    // find specific state
-    for (var state in states) {
-      if (_states.containsKey(state.name)) {
-        return _states[state.name]!;
+    // Resolve using user-defined order in _states
+    for (var stateName in _states.keys) {
+      if (stateName == "default") continue; // Skip "default"; handled last
+      if (states.any((state) => state.name == stateName)) {
+        return _states[stateName];
       }
     }
 
-    // catch-all value
-    if (_states.containsKey("")) {
-      return _states[""];
-    }
-
-    return _defaultValue;
+    // Default state
+    return _states["default"] ?? _defaultValue;
   }
 }
