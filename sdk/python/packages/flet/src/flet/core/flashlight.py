f784865b5c251dc22703b0be12ee9c03168ff68b
Osama Mohammed Al-zabidi <57198110+omamkaz@users.noreply.github.com>
2024-12-16 23:09:26 +0300

    Update flashlight.py
    ## Just some tricks that I think might improve the code! :)

--- sdk/python/packages/flet/src/flet/core/flashlight.py
+++ sdk/python/packages/flet/src/flet/core/flashlight.py
@@ -42,41 +42,39 @@ class Flashlight(Control):
     def _get_control_name(self):
         return "flashlight"
 
+    def _toggle_state(self, sr: str, on: bool=True) -> bool:
+        self.turned_on = on if ("1" == sr) else self.turned_on
+        return self.turned_on
+
     def turn_on(self, wait_timeout: Optional[int] = 5) -> bool:
         sr = self.invoke_method("on", wait_for_result=True, wait_timeout=wait_timeout)
+        return self._toggle_state(sr)
 
-        if int(sr) == 1:
-            self.turned_on = True
-        return self.turned_on
 
     async def turn_on_async(self, wait_timeout: Optional[int] = 5) -> bool:
         sr = await self.invoke_method_async(
             "on", wait_for_result=True, wait_timeout=wait_timeout
         )
-        if int(sr) == 1:
-            self.turned_on = True
-        return self.turned_on
+        return self._toggle_state(sr)
+
 
     def turn_off(self, wait_timeout: Optional[int] = 5) -> bool:
         sr = self.invoke_method("off", wait_for_result=True, wait_timeout=wait_timeout)
-
-        if int(sr) == 1:
-            self.turned_on = False
-        return self.turned_on
+        return self._toggle_state(sr, False)
+        
 
     async def turn_off_async(self, wait_timeout: Optional[int] = 5) -> bool:
         sr = await self.invoke_method_async(
             "off", wait_for_result=True, wait_timeout=wait_timeout
         )
-        if int(sr) == 1:
-            self.turned_on = False
-        return self.turned_on
+        return self._toggle_state(sr, False)
 
+    
     def toggle(self, wait_timeout: Optional[int] = 5) -> bool:
-        if self.turned_on:
-            return self.turn_off(wait_timeout)
-        return self.turn_on(wait_timeout)
+        _func = self.turn_off if self.turned_on else self.turn_on
+        return _func(wait_timeout)
 
+    
     async def toggle_async(self, wait_timeout: Optional[int] = 5) -> bool:
         if self.turned_on:
             return await self.turn_off_async(wait_timeout)
