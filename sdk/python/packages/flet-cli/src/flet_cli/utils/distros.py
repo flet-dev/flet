7f6ba454eee438280d74cf62aee6de100afb4607
Feodor Fitsner <feodor@appveyor.com>
2025-01-09 10:31:12 -0800

    Replace tqdm with Rich

--- sdk/python/packages/flet-cli/src/flet_cli/utils/distros.py
+++ sdk/python/packages/flet-cli/src/flet_cli/utils/distros.py
@@ -3,7 +3,7 @@ import tarfile
 import urllib.request
 import zipfile
 
-from tqdm import tqdm
+from rich.progress import Progress
 
 
 def download_with_progress(url, dest_path):
@@ -12,12 +12,12 @@ def download_with_progress(url, dest_path):
         total_size = int(response.info().get("Content-Length").strip())
         block_size = 8192  # 8 KB chunks
 
-        with open(dest_path, "wb") as out_file, tqdm(
-            total=total_size, unit="B", unit_scale=True, desc="Downloading"
-        ) as pbar:
-            while chunk := response.read(block_size):
-                out_file.write(chunk)
-                pbar.update(len(chunk))
+        with Progress(transient=True) as progress:
+            task = progress.add_task("Downloading...", total=total_size)
+            with open(dest_path, "wb") as out_file:
+                while chunk := response.read(block_size):
+                    out_file.write(chunk)
+                    progress.update(task, advance=len(chunk))
 
 
 def extract_with_progress(archive_path, extract_to):
@@ -25,11 +25,12 @@ def extract_with_progress(archive_path, extract_to):
     if archive_path.endswith(".zip"):
         with zipfile.ZipFile(archive_path, "r") as archive:
             total_files = len(archive.namelist())
-            with tqdm(total=total_files, unit="files", desc="Extracting") as pbar:
+            with Progress(transient=True) as progress:
+                task = progress.add_task("Extracting...", total=total_files)
                 for member in archive.namelist():
                     member = archive.getinfo(member)
                     archive.extract(member, extract_to)
-                    pbar.update(1)
+                    progress.update(task, advance=1)
                     # preserve permissions
                     extracted_path = os.path.join(extract_to, member.filename)
                     if member.external_attr > 0xFFFF:
@@ -38,10 +39,11 @@ def extract_with_progress(archive_path, extract_to):
         with tarfile.open(archive_path, "r:*") as archive:
             members = archive.getmembers()
             total_files = len(members)
-            with tqdm(total=total_files, unit="files", desc="Extracting") as pbar:
+            with Progress(transient=True) as progress:
+                task = progress.add_task("Extracting...", total=total_files)
                 for member in members:
                     archive.extract(member, extract_to)
-                    pbar.update(1)
+                    progress.update(task, advance=1)
                     # Preserve permissions (executable flags, etc.)
                     extracted_path = os.path.join(extract_to, member.name)
                     if member.isfile():
