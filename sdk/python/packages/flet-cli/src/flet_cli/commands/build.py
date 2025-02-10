20b49910ad66041ee66c727886fda3b94f3a601e
Feodor Fitsner <feodor@appveyor.com>
2025-02-09 21:18:35 -0800

    Pyodide 0.27.2
    Close #4840

--- sdk/python/packages/flet-cli/src/flet_cli/commands/build.py
+++ sdk/python/packages/flet-cli/src/flet_cli/commands/build.py
@@ -30,7 +30,7 @@ from rich.style import Style
 from rich.table import Column, Table
 from rich.theme import Theme
 
-PYODIDE_ROOT_URL = "https://cdn.jsdelivr.net/pyodide/v0.25.0/full"
+PYODIDE_ROOT_URL = "https://cdn.jsdelivr.net/pyodide/v0.27.2/full"
 DEFAULT_TEMPLATE_URL = "gh:flet-dev/flet-build-template"
 
 MINIMAL_FLUTTER_VERSION = version.Version("3.27.4")
