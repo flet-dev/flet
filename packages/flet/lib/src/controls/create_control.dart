df90c3f674e08570d10f927cd247909cb9a91abd
ndonkoHenri <robotcoder4@protonmail.com>
2025-02-10 16:23:37 +0100

    remove Stepper widget import

--- packages/flet/lib/src/controls/create_control.dart
+++ packages/flet/lib/src/controls/create_control.dart
@@ -107,7 +107,6 @@ import 'shake_detector.dart';
 import 'slider.dart';
 import 'snack_bar.dart';
 import 'stack.dart';
-import 'stepper.dart';
 import 'submenu_button.dart';
 import 'switch.dart';
 import 'tabs.dart';
@@ -382,14 +381,6 @@ Widget createWidget(
           parent: parent,
           control: controlView.control,
           children: controlView.children,
-          parentDisabled: parentDisabled,
-          parentAdaptive: parentAdaptive,
-          backend: backend);
-    case "stepper":
-      return StepperControl(
-          key: key,
-          parent: parent,
-          control: controlView.control,
           parentDisabled: parentDisabled,
           parentAdaptive: parentAdaptive,
           backend: backend);
