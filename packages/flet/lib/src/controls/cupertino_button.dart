1f1c238b496c4faba8e45d1634bee02ded7e4029
ndonkoHenri <robotcoder4@protonmail.com>
2025-02-20 14:37:14 +0100

    `CupertinoButton` & `CupertinoFilledButton`: autofocus, focus_color

--- packages/flet/lib/src/controls/cupertino_button.dart
+++ packages/flet/lib/src/controls/cupertino_button.dart
@@ -169,7 +169,8 @@ class _CupertinoButtonControlState extends State<CupertinoButtonControl> {
       pressedOpacity: pressedOpacity,
       alignment: alignment,
       minSize: minSize,
-      child: content,
+      autofocus: widget.control.attrBool("autofocus", false)!,
+      focusColor: widget.control.attrColor("focusColor", context),
       onLongPress: !disabled
           ? () {
               widget.backend
@@ -180,6 +181,7 @@ class _CupertinoButtonControlState extends State<CupertinoButtonControl> {
         widget.backend
             .triggerControlEvent(widget.control.id, focused ? "focus" : "blur");
       },
+      child: content,
     );
 
     return constrainedControl(context, button, widget.parent, widget.control);
