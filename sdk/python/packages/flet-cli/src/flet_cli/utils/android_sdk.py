733bc8c314f5897e2a3a1c4f9fbb73fdd30cefd3
Feodor Fitsner <feodor@appveyor.com>
2025-02-27 12:41:47 -0800

    Try echo y

--- sdk/python/packages/flet-cli/src/flet_cli/utils/android_sdk.py
+++ sdk/python/packages/flet-cli/src/flet_cli/utils/android_sdk.py
@@ -164,7 +164,15 @@ class AndroidSDK:
             (
                 ["sh", "-c", f'yes | {self.sdkmanager_exe(home_dir)} "{package_name}"']
                 if platform.system() != "Windows"
-                else ["cmd.exe", "/C", self.sdkmanager_exe(home_dir), package_name]
+                else [
+                    "cmd.exe",
+                    "/C",
+                    "echo",
+                    "y",
+                    "|",
+                    self.sdkmanager_exe(home_dir),
+                    package_name,
+                ]
             ),
             env={"ANDROID_HOME": str(home_dir)},
             capture_output=False,
@@ -181,7 +189,15 @@ class AndroidSDK:
             (
                 ["sh", "-c", f"yes | {self.sdkmanager_exe(home_dir)} --licenses"]
                 if platform.system() != "Windows"
-                else ["cmd.exe", "/C", self.sdkmanager_exe(home_dir), "--licenses"]
+                else [
+                    "cmd.exe",
+                    "/C",
+                    "echo",
+                    "y",
+                    "|",
+                    self.sdkmanager_exe(home_dir),
+                    "--licenses",
+                ]
             ),
             env={"ANDROID_HOME": str(home_dir)},
             capture_output=False,
