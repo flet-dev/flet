7a189df82da86bb8e11268e2f845cc1ca4e4754d
Feodor Fitsner <feodor@appveyor.com>
2025-04-03 16:12:54 -0700

    Divider docs

--- sdk/python/packages/flet/src/flet/core/divider.py
+++ sdk/python/packages/flet/src/flet/core/divider.py
@@ -18,20 +18,29 @@ class Divider(Control):
     ```
     import flet as ft
 
-
     def main(page: ft.Page):
 
         page.add(
             ft.Column(
                 [
                     ft.Container(
-                        bgcolor=ft.colors.AMBER,
+                        bgcolor=ft.Colors.AMBER,
                         alignment=ft.alignment.center,
                         expand=True,
                     ),
                     ft.Divider(),
+                    ft.Container(bgcolor=ft.Colors.PINK, alignment=ft.alignment.center, expand=True),
+                    ft.Divider(height=1, color="white"),
                     ft.Container(
-                        bgcolor=ft.colors.PINK, alignment=ft.alignment.center, expand=True
+                        bgcolor=ft.Colors.BLUE_300,
+                        alignment=ft.alignment.center,
+                        expand=True,
+                    ),
+                    ft.Divider(height=9, thickness=3),
+                    ft.Container(
+                        bgcolor=ft.Colors.DEEP_PURPLE_200,
+                        alignment=ft.alignment.center,
+                        expand=True,
                     ),
                 ],
                 spacing=0,
@@ -39,9 +48,7 @@ class Divider(Control):
             ),
         )
 
-
-    ft.app(target=main)
-
+    ft.app(main)
     ```
 
     -----
@@ -49,11 +56,38 @@ class Divider(Control):
     Online docs: https://flet.dev/docs/controls/divider
     """
 
-    height: OptionalNumber = field(default=16.0)
-    thickness: OptionalNumber = field(default=0.0)
     color: Optional[ColorValue] = None
+    """The color to use when painting the line."""
+
+    height: OptionalNumber = field(default=16.0)
+    """
+    The divider's height extent. The divider itself is always drawn as a horizontal line that is centered
+    within the height specified by this value.
+
+    Defaults to `16.0`.
+    """
+
     leading_indent: OptionalNumber = field(default=0.0)
+    """
+    The amount of empty space to the leading edge of the divider.
+
+    Defaults to `0.0`.
+    """
+
+    thickness: OptionalNumber = field(default=0.0)
+    """
+    The thickness of the line drawn within the divider. A divider with a thickness of `0.0` is always drawn
+    as a line with a height of exactly one device pixel.
+
+    Defaults to `0.0`.
+    """
+
     trailing_indent: OptionalNumber = field(default=0.0)
+    """
+    The amount of empty space to the trailing edge of the divider.
+
+    Defaults to `0.0`.
+    """
 
     def before_update(self):
         super().before_update()
