cdb448da12816c5161b8b8a96e092bbed77de9c6
InesaFitsner <inesa@appveyor.com>
2025-04-04 11:24:38 -0700

    icon_button.dart

--- packages/flet/lib/src/controls/icon_button.dart
+++ packages/flet/lib/src/controls/icon_button.dart
@@ -1,36 +1,32 @@
+import 'package:flet/flet.dart';
 import 'package:flutter/material.dart';
 
-import '../flet_control_backend.dart';
-import '../models/control.dart';
-import '../utils/alignment.dart';
-import '../utils/box.dart';
-import '../utils/buttons.dart';
-import '../utils/edge_insets.dart';
-import '../utils/icons.dart';
+//import '../flet_control_backend.dart';
 import '../utils/launch_url.dart';
-import '../utils/mouse.dart';
-import '../utils/theme.dart';
-import 'create_control.dart';
+import '../widgets/error.dart';
+import 'base_controls.dart';
+//import 'create_control.dart';
 import 'cupertino_button.dart';
-import 'error.dart';
-import 'flet_store_mixin.dart';
+//import 'error.dart';
+//import 'flet_store_mixin.dart';
 
 class IconButtonControl extends StatefulWidget {
-  final Control? parent;
+  //final Control? parent;
   final Control control;
-  final List<Control> children;
-  final bool parentDisabled;
-  final bool? parentAdaptive;
-  final FletControlBackend backend;
+  //final List<Control> children;
+  //final bool parentDisabled;
+  //final bool? parentAdaptive;
+  //final FletControlBackend backend;
 
-  const IconButtonControl(
-      {super.key,
-      this.parent,
-      required this.control,
-      required this.children,
-      required this.parentDisabled,
-      required this.parentAdaptive,
-      required this.backend});
+  const IconButtonControl({
+    super.key,
+    //this.parent,
+    required this.control,
+    //required this.children,
+    //required this.parentDisabled,
+    //required this.parentAdaptive,
+    //required this.backend,
+  });
 
   @override
   State<IconButtonControl> createState() => _IconButtonControlState();
@@ -56,8 +52,8 @@ class _IconButtonControlState extends State<IconButtonControl>
   }
 
   void _onFocusChange() {
-    widget.backend.triggerControlEvent(
-        widget.control.id, _focusNode.hasFocus ? "focus" : "blur");
+    FletBackend.of(context).triggerControlEvent(
+        widget.control, _focusNode.hasFocus ? "focus" : "blur");
   }
 
   @override
@@ -66,21 +62,22 @@ class _IconButtonControlState extends State<IconButtonControl>
 
     return withPagePlatform((context, platform) {
       bool? adaptive =
-          widget.control.getBool("adaptive") ?? widget.parentAdaptive;
+          widget.control.adaptive ?? widget.control.parent?.adaptive;
       if (adaptive == true &&
           (platform == TargetPlatform.iOS ||
               platform == TargetPlatform.macOS)) {
         return CupertinoButtonControl(
-            control: widget.control,
-            parentDisabled: widget.parentDisabled,
-            parentAdaptive: adaptive,
-            children: widget.children,
-            backend: widget.backend);
+          control: widget.control,
+          //parentDisabled: widget.parentDisabled,
+          //parentAdaptive: adaptive,
+          //children: widget.children,
+          //backend: widget.backend,
+        );
       }
 
-      IconData? icon = parseIcon(widget.control.getString("icon"));
-      IconData? selectedIcon =
-          parseIcon(widget.control.getString("selectedIcon"));
+      // IconData? icon = parseIcon(widget.control.getString("icon"));
+      // IconData? selectedIcon =
+      //     parseIcon(widget.control.getString("selectedIcon"));
       Color? iconColor = widget.control.getColor("iconColor", context);
       Color? highlightColor =
           widget.control.getColor("highlightColor", context);
@@ -97,14 +94,15 @@ class _IconButtonControlState extends State<IconButtonControl>
       var alignment = parseAlignment(widget.control, "alignment");
       var sizeConstraints =
           parseBoxConstraints(widget.control, "sizeConstraints");
-      var contentCtrls =
-          widget.children.where((c) => c.name == "content" && c.visible);
+      // var contentCtrls =
+      //     widget.children.where((c) => c.name == "content" && c.visible);
       bool autofocus = widget.control.getBool("autofocus", false)!;
       bool enableFeedback = widget.control.getBool("enableFeedback", true)!;
       bool selected = widget.control.getBool("selected", false)!;
       String url = widget.control.getString("url", "")!;
       String? urlTarget = widget.control.getString("urlTarget");
-      bool disabled = widget.control.disabled || widget.parentDisabled;
+      bool disabled =
+          widget.control.disabled || widget.control.parent!.disabled;
       var mouseCursor =
           parseMouseCursor(widget.control.getString("mouseCursor"));
       var visualDensity =
@@ -117,7 +115,8 @@ class _IconButtonControlState extends State<IconButtonControl>
               if (url != "") {
                 openWebBrowser(url, webWindowName: urlTarget);
               }
-              widget.backend.triggerControlEvent(widget.control.id, "click");
+              FletBackend.of(context)
+                  .triggerControlEvent(widget.control, "click");
             };
 
       Widget? button;
@@ -164,31 +163,31 @@ class _IconButtonControlState extends State<IconButtonControl>
                 ? Icon(selectedIcon, color: selectedIconColor)
                 : null,
             onPressed: onPressed);
-      } else if (contentCtrls.isNotEmpty) {
-        button = IconButton(
-            autofocus: autofocus,
-            focusNode: _focusNode,
-            highlightColor: highlightColor,
-            disabledColor: highlightColor,
-            hoverColor: highlightColor,
-            enableFeedback: enableFeedback,
-            padding: padding,
-            alignment: alignment,
-            focusColor: focusColor,
-            splashColor: splashColor,
-            splashRadius: splashRadius,
-            onPressed: onPressed,
-            iconSize: iconSize,
-            mouseCursor: mouseCursor,
-            visualDensity: visualDensity,
-            style: style,
-            isSelected: selected,
-            constraints: sizeConstraints,
-            selectedIcon: selectedIcon != null
-                ? Icon(selectedIcon, color: selectedIconColor)
-                : null,
-            icon: createControl(widget.control, contentCtrls.first.id, disabled,
-                parentAdaptive: widget.parentAdaptive));
+        // } else if (contentCtrls.isNotEmpty) {
+        //   button = IconButton(
+        //       autofocus: autofocus,
+        //       focusNode: _focusNode,
+        //       highlightColor: highlightColor,
+        //       disabledColor: highlightColor,
+        //       hoverColor: highlightColor,
+        //       enableFeedback: enableFeedback,
+        //       padding: padding,
+        //       alignment: alignment,
+        //       focusColor: focusColor,
+        //       splashColor: splashColor,
+        //       splashRadius: splashRadius,
+        //       onPressed: onPressed,
+        //       iconSize: iconSize,
+        //       mouseCursor: mouseCursor,
+        //       visualDensity: visualDensity,
+        //       style: style,
+        //       isSelected: selected,
+        //       constraints: sizeConstraints,
+        //       selectedIcon: selectedIcon != null
+        //           ? Icon(selectedIcon, color: selectedIconColor)
+        //           : null,
+        //       icon: createControl(widget.control, contentCtrls.first.id, disabled,
+        //           parentAdaptive: widget.parentAdaptive));
       } else {
         return const ErrorControl(
             "IconButton must have either icon or a visible content specified.");
@@ -208,7 +207,7 @@ class _IconButtonControlState extends State<IconButtonControl>
         _focusNode.requestFocus();
       }
 
-      return constrainedControl(context, button, widget.parent, widget.control);
+      return ConstrainedControl(control: widget.control, child: button);
     });
   }
 }
