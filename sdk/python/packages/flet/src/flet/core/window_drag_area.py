5088eeac2cd3945b12b655854bcebf3a10bc2f6a
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2025-04-05 02:04:32 +0200

    feat: expose events (`on_double_tap`, `on_pan_start`) in `WindowDragArea` (#5043)
    * improve events typing in gesture_detector.py
    
    * window.start_dragging
    
    * delete window_drag_area.dart
    
    * rework WindowDragArea inheriting from GestureDetector
    
    * export more utils

--- sdk/python/packages/flet/src/flet/core/window_drag_area.py
+++ sdk/python/packages/flet/src/flet/core/window_drag_area.py
@@ -1,11 +1,11 @@
-from flet.core.constrained_control import ConstrainedControl
-from flet.core.control import Control, control
+from typing import Any
 
-__all__ = ["WindowDragArea"]
+from flet.core.control import Control
+from flet.core.gesture_detector import DragStartEvent, GestureDetector, TapEvent
+from flet.core.types import OptionalEventCallable
 
 
-@control("WindowDragArea")
-class WindowDragArea(ConstrainedControl):
+class WindowDragArea(GestureDetector):
     """
     A control for drag to move, maximize and restore application window.
 
@@ -36,9 +36,42 @@ class WindowDragArea(ConstrainedControl):
     Online docs: https://flet.dev/docs/controls/windowdragarea
     """
 
-    content: Control
-    maximizable: bool = True
+    def __init__(
+        self,
+        content: Control,
+        maximizable: bool = True,
+        on_double_tap: OptionalEventCallable["TapEvent"] = None,
+        on_pan_start: OptionalEventCallable["DragStartEvent"] = None,
+        **kwargs: Any,
+    ):
+        GestureDetector.__init__(
+            self,
+            content=content,
+            on_double_tap=self.handle_double_tap,
+            on_pan_start=self.handle_pan_start,
+            **kwargs,
+        )
+
+        self.maximizable = maximizable
+        self.on_double_tap = on_double_tap
+        self.on_pan_start = on_pan_start
 
     def before_update(self):
         super().before_update()
         assert self.content.visible, "content must be visible"
+
+    def handle_double_tap(self, e: TapEvent):
+        if self.maximizable and self.page.window.maximizable:
+            if not self.page.window.maximized:
+                self.page.window.maximized = True
+            else:
+                self.page.window.maximized = False
+            self.page.update()
+
+        if self.on_double_tap is not None and self.page.window.maximized:
+            self.on_double_tap(e)
+
+    def handle_pan_start(self, e: DragStartEvent):
+        self.page.window.start_dragging()
+        if self.on_pan_start is not None:
+            self.on_pan_start(e)
