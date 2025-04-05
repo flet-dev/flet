880c857b4e7a2976dfcdc3f5c4f02609d0664ddf
Feodor Fitsner <feodor@appveyor.com>
2025-04-05 13:08:28 -0700

    Python Charts - converted

--- sdk/python/packages/flet/src/flet/controls/core/canvas/rect.py
+++ sdk/python/packages/flet/src/flet/controls/core/canvas/rect.py
@@ -1,6 +1,7 @@
 from typing import Any, Optional
 
 from flet.controls.border_radius import OptionalBorderRadiusValue
+from flet.controls.control import control
 from flet.controls.core.canvas.shape import Shape
 from flet.controls.painting import Paint
 from flet.controls.types import OptionalNumber
--- sdk/python/packages/flet/src/flet/controls/core/charts/bar_chart_rod_stack_item.py
+++ sdk/python/packages/flet/src/flet/controls/core/charts/bar_chart_rod_stack_item.py
@@ -1,80 +1,13 @@
-from typing import Any, Optional
+from typing import Optional
 
 from flet.controls.border import BorderSide
-from flet.controls.control import Control
-from flet.controls.ref import Ref
-from flet.controls.types import ColorEnums, OptionalColorValue, OptionalNumber
+from flet.controls.control import Control, control
+from flet.controls.types import OptionalColorValue, OptionalNumber
 
 
+@control("i")
 class BarChartRodStackItem(Control):
-    def __init__(
-        self,
-        from_y: OptionalNumber = None,
-        to_y: OptionalNumber = None,
-        color: OptionalColorValue = None,
-        border_side: Optional[BorderSide] = None,
-        #
-        # Control
-        #
-        ref: Optional[Ref] = None,
-        disabled: Optional[bool] = None,
-        visible: Optional[bool] = None,
-        data: Any = None,
-    ):
-
-        Control.__init__(
-            self,
-            ref=ref,
-            disabled=disabled,
-            visible=visible,
-            data=data,
-        )
-
-        self.from_y = from_y
-        self.to_y = to_y
-        self.color = color
-        self.border_side = border_side
-
-    def _get_control_name(self):
-        return "stack_item"
-
-    def before_update(self):
-        super().before_update()
-        self._set_attr_json("borderSide", self.__border_side)
-
-    # from_y
-    @property
-    def from_y(self) -> OptionalNumber:
-        return self._get_attr("fromY", data_type="float")
-
-    @from_y.setter
-    def from_y(self, value: OptionalNumber):
-        self._set_attr("fromY", value)
-
-    # to_y
-    @property
-    def to_y(self) -> OptionalNumber:
-        return self._get_attr("toY", data_type="float")
-
-    @to_y.setter
-    def to_y(self, value: OptionalNumber):
-        self._set_attr("toY", value)
-
-    # color
-    @property
-    def color(self) -> OptionalColorValue:
-        return self.__color
-
-    @color.setter
-    def color(self, value: OptionalColorValue):
-        self.__color = value
-        self._set_enum_attr("color", value, ColorEnums)
-
-    # border_side
-    @property
-    def border_side(self) -> Optional[BorderSide]:
-        return self.__border_side
-
-    @border_side.setter
-    def border_side(self, value: Optional[BorderSide]):
-        self.__border_side = value
+    from_y: OptionalNumber = None
+    to_y: OptionalNumber = None
+    color: OptionalColorValue = None
+    border_side: Optional[BorderSide] = None
