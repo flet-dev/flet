1fb07092126706ca227e88503a03ef14c1f2afca
Feodor Fitsner <feodor@appveyor.com>
2025-04-08 10:01:23 -0700

    Banner - done

--- packages/flet/lib/src/controls/banner.dart
+++ packages/flet/lib/src/controls/banner.dart
@@ -1,6 +1,11 @@
-import 'package:flet/flet.dart';
+import 'package:flet/src/utils/colors.dart';
+import 'package:flet/src/utils/edge_insets.dart';
+import 'package:flet/src/utils/numbers.dart';
 import 'package:flutter/material.dart';
 
+import '../extensions/control.dart';
+import '../models/control.dart';
+import '../utils/text.dart';
 import '../widgets/error.dart';
 
 class BannerControl extends StatefulWidget {
@@ -36,7 +41,7 @@ class _BannerControlState extends State<BannerControl> {
     _toggleBanner();
   }
 
-  Widget _createBanner(FletBackend backend) {
+  Widget _createBanner() {
     var leading = widget.control.buildWidget("leading");
     var content = widget.control.buildWidget("content");
     var actions = widget.control.buildWidgets("actions");
@@ -56,8 +61,8 @@ class _BannerControlState extends State<BannerControl> {
       actions: actions,
       forceActionsBelow: widget.control.getBool("force_actions_below", false)!,
       backgroundColor: widget.control.getColor("bgcolor", context),
-      contentTextStyle: parseTextStyle(
-        widget.control.get("content_text_style"),
+      contentTextStyle: widget.control.getTextStyle(
+        "content_text_style",
         Theme.of(context),
       ),
       surfaceTintColor: widget.control.getColor("surface_tint_color", context),
@@ -68,26 +73,23 @@ class _BannerControlState extends State<BannerControl> {
           widget.control.getDouble("min_action_bar_height", 52.0)!,
       margin: widget.control.getMargin("margin"),
       onVisible: () {
-        backend.triggerControlEvent(widget.control, "visible");
+        widget.control.triggerEvent("visible");
       },
     );
   }
 
   void _toggleBanner() {
     var dismissed = widget.control.get("_dismissed");
-    final lastOpen = widget.control.getBool("_open", false)!;
 
     debugPrint("Banner build: ${widget.control.id}, _dismissed=$dismissed");
-    debugPrint("STATE: $hashCode");
 
     if (dismissed == true) return;
 
+    final lastOpen = widget.control.getBool("_open", false)!;
     var open = widget.control.getBool("open", false)!;
 
-    var backend = FletBackend.of(context);
-
     if (open && (open != lastOpen)) {
-      _dialog = _createBanner(FletBackend.of(context));
+      _dialog = _createBanner();
 
       if (_dialog is ErrorControl) {
         debugPrint(
@@ -95,10 +97,7 @@ class _BannerControlState extends State<BannerControl> {
         return;
       }
 
-      debugPrint(
-          "Banner build 2: ${widget.control.id}, _open=$lastOpen, open=$open");
-
-      backend.updateControl(widget.control.id, {"_open": open}, python: false);
+      widget.control.updateProperties({"_open": open}, python: false);
 
       WidgetsBinding.instance.addPostFrameCallback((_) {
         ScaffoldMessenger.of(context).removeCurrentMaterialBanner();
@@ -108,25 +107,22 @@ class _BannerControlState extends State<BannerControl> {
             .then((reason) {
           debugPrint(
               "Closing Banner(${widget.control.id}) with reason: $reason");
-          debugPrint("STATE: $hashCode");
           if (widget.control.get("_dismissed") != true) {
-            backend.updateControl(widget.control.id, {"_dismissed": true},
-                python: false);
+            widget.control
+                .updateProperties({"_dismissed": true}, python: false);
             debugPrint(
                 "Dismissing Banner(${widget.control.id}) with reason: $reason");
             //_open = false;
-            backend.updateControl(widget.control.id, {"_open": false},
-                python: false);
-            backend.updateControl(widget.control.id, {"open": false});
-            backend.triggerControlEvent(widget.control, "dismiss");
+            widget.control.updateProperties({"_open": false}, python: false);
+            widget.control.updateProperties({"open": false});
+            widget.control.triggerEvent("dismiss");
           }
         });
       });
     } else if (!open && lastOpen) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
         ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
-        backend.updateControl(widget.control.id, {"_open": false},
-            python: false);
+        widget.control.updateProperties({"_open": false}, python: false);
       });
     }
   }
