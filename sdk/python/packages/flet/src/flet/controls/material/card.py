21a73e3b6bccddbe367a6871bdf5e9dc06766b5c
InesaFitsner <inesa@appveyor.com>
2025-04-22 10:31:04 -0700

    Card description

--- sdk/python/packages/flet/src/flet/controls/material/card.py
+++ sdk/python/packages/flet/src/flet/controls/material/card.py
@@ -23,42 +23,6 @@ class Card(ConstrainedControl, AdaptiveControl):
     """
     A material design card: a panel with slightly rounded corners and an elevation shadow.
 
-    Example:
-    ```
-    import flet as ft
-
-    def main(page):
-        page.title = "Card Example"
-        page.add(
-            ft.Card(
-                content=ft.Container(
-                    content=ft.Column(
-                        [
-                            ft.ListTile(
-                                leading=ft.Icon(ft.icons.ALBUM),
-                                title=ft.Text("The Enchanted Nightingale"),
-                                subtitle=ft.Text(
-                                    "Music by Julie Gable. Lyrics by Sidney Stein."
-                                ),
-                            ),
-                            ft.Row(
-                                [ft.TextButton("Buy tickets"), ft.TextButton("Listen")],
-                                alignment=ft.MainAxisAlignment.END,
-                            ),
-                        ]
-                    ),
-                    width=400,
-                    padding=10,
-                )
-            )
-        )
-
-    ft.app(target=main)
-
-    ```
-
-    -----
-
     Online docs: https://flet.dev/docs/controls/card
     """
 
