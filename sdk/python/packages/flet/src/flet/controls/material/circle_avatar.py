9d3a42f7adf53df8593c530647be608b72b61758
InesaFitsner <inesa@appveyor.com>
2025-04-22 10:57:43 -0700

    deleted example from description

--- sdk/python/packages/flet/src/flet/controls/material/circle_avatar.py
+++ sdk/python/packages/flet/src/flet/controls/material/circle_avatar.py
@@ -21,53 +21,6 @@ class CircleAvatar(ConstrainedControl):
     If `foreground_image_src` fails then `background_image_src` is used. If `background_image_src` fails too,
     then `bgcolor` is used.
 
-    Example:
-    ```
-    import flet as ft
-
-    def main(page):
-        # a "normal" avatar with background image
-        a1 = ft.CircleAvatar(
-            foreground_image_src="https://avatars.githubusercontent.com/u/5041459?s=88&v=4",
-            content=ft.Text("FF"),
-        )
-        # avatar with failing foreground image and fallback text
-        a2 = ft.CircleAvatar(
-            foreground_image_src="https://avatars.githubusercontent.com/u/_5041459?s=88&v=4",
-            content=ft.Text("FF"),
-        )
-        # avatar with icon, aka icon with inverse background
-        a3 = ft.CircleAvatar(
-            content=ft.Icon(ft.icons.ABC),
-        )
-        # avatar with icon and custom colors
-        a4 = ft.CircleAvatar(
-            content=ft.Icon(ft.icons.WARNING_ROUNDED),
-            color=ft.colors.YELLOW_200,
-            bgcolor=ft.colors.AMBER_700,
-        )
-        # avatar with online status
-        a5 = ft.Stack(
-            [
-                ft.CircleAvatar(
-                    foreground_image_src="https://avatars.githubusercontent.com/u/5041459?s=88&v=4"
-                ),
-                ft.Container(
-                    content=ft.CircleAvatar(bgcolor=ft.colors.GREEN, radius=5),
-                    alignment=ft.alignment.bottom_left,
-                ),
-            ],
-            width=40,
-            height=40,
-        )
-        page.add(a1, a2, a3, a4, a5)
-
-
-    ft.app(target=main)
-    ```
-
-    -----
-
     Online docs: https://flet.dev/docs/controls/circleavatar
     """
 
