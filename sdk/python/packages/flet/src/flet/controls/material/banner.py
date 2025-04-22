7d8ff4fe2ee4319de28a7db2927c5ec7c85ce84d
InesaFitsner <inesa@appveyor.com>
2025-04-22 10:03:48 -0700

    remove example

--- sdk/python/packages/flet/src/flet/controls/material/banner.py
+++ sdk/python/packages/flet/src/flet/controls/material/banner.py
@@ -24,39 +24,6 @@ class Banner(DialogControl):
 
     Banners are displayed at the top of the screen, below a top app bar. They are persistent and non-modal, allowing the user to either ignore them or interact with them at any time.
 
-    Example:
-    ```
-    import flet as ft
-
-
-    def main(page):
-        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-        def close_banner(e):
-            page.close(banner)
-            page.add(ft.Text("Action clicked: " + e.control.text))
-
-        action_button_style = ft.ButtonStyle(color=ft.colors.BLUE)
-        banner = ft.Banner(
-            bgcolor=ft.colors.AMBER_100,
-            leading=ft.Icon(ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.AMBER, size=40),
-            content=ft.Text(
-                value="Oops, there were some errors while trying to delete the file. What would you like me to do?",
-                color=ft.colors.BLACK,
-            ),
-            actions=[
-                ft.TextButton(text="Retry", style=action_button_style, on_click=close_banner),
-                ft.TextButton(text="Ignore", style=action_button_style, on_click=close_banner),
-                ft.TextButton(text="Cancel", style=action_button_style, on_click=close_banner),
-            ],
-        )
-
-        page.add(ft.ElevatedButton("Show Banner", on_click=lambda e: page.open(banner)))
-
-
-    ft.app(main)
-    ```
-
     -----
 
     Online docs: https://flet.dev/docs/controls/banner
