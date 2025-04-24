cccebebadc2fadea8ea5ac776ee376cbe2b4d678
InesaFitsner <inesa@appveyor.com>
2025-04-24 09:21:16 -0700

    removed example from control description

--- sdk/python/packages/flet/src/flet/controls/material/elevated_button.py
+++ sdk/python/packages/flet/src/flet/controls/material/elevated_button.py
@@ -22,26 +22,9 @@ __all__ = ["ElevatedButton"]
 @control("ElevatedButton")
 class ElevatedButton(ConstrainedControl, AdaptiveControl):
     """
-        Elevated buttons are essentially filled tonal buttons with a shadow. To prevent shadow creep, only use them when absolutely necessary, such as when the button requires visual separation from a patterned background.
+    Elevated buttons are essentially filled tonal buttons with a shadow. To prevent shadow creep, only use them when absolutely necessary, such as when the button requires visual separation from a patterned background.
 
-        Example:
-        ```
-        import flet as ft
-    import warnings
-
-        def main(page: ft.Page):
-            page.title = "Basic elevated buttons"
-            page.add(
-                ft.ElevatedButton(text="Elevated button"),
-                ft.ElevatedButton("Disabled button", disabled=True),
-            )
-
-        ft.app(target=main)
-        ```
-
-        -----
-
-        Online docs: https://flet.dev/docs/controls/elevatedbutton
+    Online docs: https://flet.dev/docs/controls/elevatedbutton
     """
 
     content: Optional[StrOrControl] = None
