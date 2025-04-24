e3f094c832a60f0591c729d47cf52aa5daa275eb
InesaFitsner <inesa@appveyor.com>
2025-04-24 14:37:05 -0700

    Alignment.center() and other methods
    deleted constants (breaking change)

--- sdk/python/packages/flet/src/flet/controls/alignment.py
+++ sdk/python/packages/flet/src/flet/controls/alignment.py
@@ -1,4 +1,4 @@
-import dataclasses
+from dataclasses import dataclass, field
 from enum import Enum
 from typing import Optional
 
@@ -26,22 +26,58 @@ class Axis(Enum):
     VERTICAL = "vertical"
 
 
-@dataclasses.dataclass
+@dataclass
 class Alignment:
     x: Number
     y: Number
 
+    @classmethod
+    def bottom_center(cls) -> "Alignment":
+        return Alignment(0, 1)
+
+    @classmethod
+    def bottom_left(cls) -> "Alignment":
+        return Alignment(-1, 1)
+
+    @classmethod
+    def bottom_right(cls) -> "Alignment":
+        return Alignment(1, 1)
+
+    @classmethod
+    def center(cls) -> "Alignment":
+        return Alignment(0, 0)
+
+    @classmethod
+    def center_left(cls) -> "Alignment":
+        return Alignment(-1, 0)
+
+    @classmethod
+    def center_right(cls) -> "Alignment":
+        return Alignment(1, 0)
+
+    @classmethod
+    def top_center(cls) -> "Alignment":
+        return Alignment(0, -1)
+
+    @classmethod
+    def top_left(cls) -> "Alignment":
+        return Alignment(-1, -1)
+
+    @classmethod
+    def top_right(cls) -> "Alignment":
+        return Alignment(1, -1)
+
 
 # Constants
-bottom_center = Alignment(0, 1)
-bottom_left = Alignment(-1, 1)
-bottom_right = Alignment(1, 1)
-center = Alignment(0, 0)
-center_left = Alignment(-1, 0)
-center_right = Alignment(1, 0)
-top_center = Alignment(0, -1)
-top_left = Alignment(-1, -1)
-top_right = Alignment(1, -1)
+# bottom_center = Alignment(0, 1)
+# bottom_left = Alignment(-1, 1)
+# bottom_right = Alignment(1, 1)
+# center = Alignment(0, 0)
+# center_left = Alignment(-1, 0)
+# center_right = Alignment(1, 0)
+# top_center = Alignment(0, -1)
+# top_left = Alignment(-1, -1)
+# top_right = Alignment(1, -1)
 
 # Typing
 OptionalAlignment = Optional[Alignment]
