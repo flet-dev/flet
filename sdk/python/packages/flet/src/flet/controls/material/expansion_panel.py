f1089397e4e976605e20ba84e6fee09601897660
InesaFitsner <inesa@appveyor.com>
2025-04-24 09:25:30 -0700

    ExpansionPanel description

--- sdk/python/packages/flet/src/flet/controls/material/expansion_panel.py
+++ sdk/python/packages/flet/src/flet/controls/material/expansion_panel.py
@@ -21,8 +21,6 @@ class ExpansionPanel(ConstrainedControl, AdaptiveControl):
     """
     A material expansion panel. It can either be expanded or collapsed. Its body is only visible when it is expanded.
 
-    -----
-
     Online docs: https://flet.dev/docs/controls/expansionpanel
     """
 
@@ -39,8 +37,6 @@ class ExpansionPanel(ConstrainedControl, AdaptiveControl):
 class ExpansionPanelList(ConstrainedControl):
     """
     A material expansion panel list that lays out its children and animates expansions.
-
-    -----
 
     Online docs: https://flet.dev/docs/controls/expansionpanellist
     """
