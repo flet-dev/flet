2d8abb4bab77d9ae645375b2aaae32562d2348e2
Feodor Fitsner <feodor@appveyor.com>
2025-05-06 09:58:03 -0700

    Autocomplete.selected_index

--- sdk/python/packages/flet/src/flet/utils/patch_dataclass.py
+++ sdk/python/packages/flet/src/flet/utils/patch_dataclass.py
@@ -21,46 +21,48 @@ def patch_dataclass(obj: Any, patch: dict):
         localns = frame.f_globals.copy()
         localns.update(frame.f_locals)
         hints = get_type_hints(cls, globalns=globalns, localns=localns)
-    except Exception as e:
+    except Exception:
         hints = {f.name: f.type for f in dataclasses.fields(cls)}  # fallback
         # print("ERROR: ", cls.__name__, e)
 
-    for field_name, field_type in hints.items():
-        if field_name not in patch:
-            continue
+    for field_name, value in patch.items():
+        if field_name in hints:
+            field_type = hints[field_name]
+            current_value = getattr(obj, field_name, None)
+            actual_type = resolve_actual_type(field_type)
 
-        value = patch[field_name]
-        current_value = getattr(obj, field_name, None)
-        actual_type = resolve_actual_type(field_type)
+            if isinstance(actual_type, str):
+                continue  # unresolved forward ref
 
-        if isinstance(actual_type, str):
-            continue  # unresolved forward ref
+            # Nested dataclass patching
+            if dataclasses.is_dataclass(actual_type) and isinstance(value, dict):
+                if current_value is None:
+                    setattr_with_no_changes(
+                        obj, field_name, from_dict(actual_type, value)
+                    )
+                else:
+                    patch_dataclass(current_value, value)
 
-        # Nested dataclass patching
-        if dataclasses.is_dataclass(actual_type) and isinstance(value, dict):
-            if current_value is None:
-                setattr_with_no_changes(obj, field_name, from_dict(actual_type, value))
-            else:
-                patch_dataclass(current_value, value)
+            # List of dataclasses or values
+            elif get_origin(actual_type) is list and isinstance(value, list):
+                item_type = get_args(actual_type)[0]
+                if dataclasses.is_dataclass(item_type):
+                    setattr_with_no_changes(
+                        obj, field_name, [from_dict(item_type, item) for item in value]
+                    )
+                else:
+                    setattr_with_no_changes(obj, field_name, value)
 
-        # List of dataclasses or values
-        elif get_origin(actual_type) is list and isinstance(value, list):
-            item_type = get_args(actual_type)[0]
-            if dataclasses.is_dataclass(item_type):
-                setattr_with_no_changes(
-                    obj, field_name, [from_dict(item_type, item) for item in value]
-                )
+            # Enum
+            elif is_enum(actual_type):
+                enum_value = actual_type(value)
+                setattr_with_no_changes(obj, field_name, enum_value)
+
+            # Simple literal or other value
             else:
                 setattr_with_no_changes(obj, field_name, value)
-
-        # Enum
-        elif is_enum(actual_type):
-            enum_value = actual_type(value)
-            setattr_with_no_changes(obj, field_name, enum_value)
-
-        # Simple literal or other value
-        else:
-            setattr_with_no_changes(obj, field_name, value)
+        elif field_name.startswith("_"):
+            setattr(obj, field_name, value)
 
 
 def resolve_actual_type(tp: Any) -> Any:
