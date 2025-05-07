9c11f7e1f6633bbe95b35ae472a21ca02b7e0f57
bleudev <aitiiigg1@gmail.com>
2025-05-07 22:00:15 +0000

    Simplify `deprecated.py` + add docs for `deprecated_class()` and add abillity to not provide `delete_version`

--- sdk/python/packages/flet/src/flet/utils/deprecated.py
+++ sdk/python/packages/flet/src/flet/utils/deprecated.py
@@ -1,24 +1,45 @@
 import functools
 import warnings
-from typing import Optional
+from typing import TypeVar, Type, Optional, Callable
 
+def __get_deprecated_message(
+    name: str,
+    version: str,
+    delete_version: Optional[str],
+    reason: str
+):
+    delete_version_message = f' and will be removed in version {delete_version}' if delete_version else ''
+    return f"{name} is deprecated since version {version}{delete_version_message}. {reason}"
 
-def deprecated(reason: str, version: str, delete_version: str, is_method=True):
+F = TypeVar('F', bound=Callable[..., Any])
+T = TypeVar('T')
+
+def deprecated(
+    reason: str,
+    version: str,
+    delete_version: Optional[str] = None,
+    is_method: bool = True,
+) -> Callable[[F], F]:
     """
     A decorator function that marks a function/method/property/event as deprecated.
 
     :param reason: The reason for deprecation.
     :param version: The version from which the function was deprecated.
     :param delete_version: The version in which the function will be removed from the API.
-    :param is_method: if the deprecated item is a method (True) or property/function/event (False)
+    :param is_method: if the deprecated item is a method (True) or property/function/event (False).
+    :return: A decorator that wraps the function/method/property/event and emits a deprecation warning.
     """
 
     def decorator(func):
         @functools.wraps(func)
         def new_func(*args, **kwargs):
             warnings.warn(
-                f"{func.__name__}{'()' if is_method else ''} is deprecated since version {version} "
-                f"and will be removed in version {delete_version}. {reason}",
+                __get_deprecated_message(
+                    name=f"{func.__name__}{'()' if is_method else ''}",
+                    version=version,
+                    delete_version=delete_version,
+                    reason=reason
+                ),
                 category=DeprecationWarning,
                 stacklevel=2,
             )
@@ -29,16 +50,35 @@ def deprecated(reason: str, version: str, delete_version: str, is_method=True):
     return decorator
 
 
-def deprecated_class(reason: str, version: str, delete_version: str):
-    def decorator(cls):
-        msg = f"{cls.__name__} is deprecated since version {version} and will be removed in version {delete_version}. {reason}"
+def deprecated_class(
+    reason: str,
+    version: str,
+    delete_version: Optional[str] = None,
+) -> Callable[[Type[T]], Type[T]]:
+    """
+    A decorator that marks a class as deprecated.
 
+    :param reason: The reason for deprecation.
+    :param version: The version from which the class was deprecated.
+    :param delete_version: The version in which the class will be removed.
+    :return: A decorator that wraps the class and emits a deprecation warning.
+    """
+    def decorator(cls):
         # Wrap the original __init__ method
         orig_init = cls.__init__
 
         @functools.wraps(orig_init)
         def new_init(self, *args, **kwargs):
-            warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
+            warnings.warn(
+                __get_deprecated_message(
+                    name=cls.__name__,
+                    version=version,
+                    delete_version=delete_version,
+                    reason=reason
+                ),
+                category=DeprecationWarning,
+                stacklevel=2
+            )
             orig_init(self, *args, **kwargs)
 
         cls.__init__ = new_init
@@ -48,11 +88,18 @@ def deprecated_class(reason: str, version: str, delete_version: str):
 
 
 def deprecated_property(
-    name: str, reason: str, version: str, delete_version: Optional[str] = None
-):
+    name: str,
+    reason: str,
+    version: str,
+    delete_version: Optional[str] = None,
+) -> None:
     warnings.warn(
-        f"{name} property is deprecated since version {version}"
-        f"{' and will be removed in version ' + delete_version if delete_version else ''}. {reason}",
+        __get_deprecated_message(
+            name=f'{name} property',
+            version=version,
+            delete_version=delete_version,
+            reason=reason
+        ),
         category=DeprecationWarning,
         stacklevel=2,
     )
