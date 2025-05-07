3a352542823678f5dc48d6f11423f57dfa6fb17c
InesaFitsner <inesa@appveyor.com>
2025-05-07 09:51:32 -0700

    removed example

--- sdk/python/packages/flet/src/flet/controls/material/navigation_rail.py
+++ sdk/python/packages/flet/src/flet/controls/material/navigation_rail.py
@@ -1,6 +1,6 @@
 from dataclasses import field
 from enum import Enum
-from typing import List, Optional
+from typing import Optional
 
 from flet.controls.base_control import control
 from flet.controls.buttons import OutlinedBorder
@@ -39,61 +39,13 @@ class NavigationRailDestination(Control):
 @control("NavigationRail")
 class NavigationRail(ConstrainedControl):
     """
-    A material widget that is meant to be displayed at the left or right of an app to navigate between a small number of views, typically between three and five.
-
-    Example:
-
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-
-        rail = ft.NavigationRail(
-            selected_index=1,
-            label_type=ft.NavigationRailLabelType.ALL,
-            # extended=True,
-            min_width=100,
-            min_extended_width=400,
-            leading=ft.FloatingActionButton(icon=ft.icons.CREATE, text="Add"),
-            group_alignment=-0.9,
-            destinations=[
-                ft.NavigationRailDestination(
-                    icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="First"
-                ),
-                ft.NavigationRailDestination(
-                    icon_content=ft.Icon(ft.icons.BOOKMARK_BORDER),
-                    selected_icon_content=ft.Icon(ft.icons.BOOKMARK),
-                    label="Second",
-                ),
-                ft.NavigationRailDestination(
-                    icon=ft.icons.SETTINGS_OUTLINED,
-                    selected_icon_content=ft.Icon(ft.icons.SETTINGS),
-                    label_content=ft.Text("Settings"),
-                ),
-            ],
-            on_change=lambda e: print("Selected destination:", e.control.selected_index),
-        )
-
-        page.add(
-            ft.Row(
-                [
-                    rail,
-                    ft.VerticalDivider(width=1),
-                    ft.Column([ ft.Text("Body!")], alignment=ft.MainAxisAlignment.START, expand=True),
-                ],
-                expand=True,
-            )
-        )
-
-    ft.app(target=main)
-    ```
-
-    -----
+    A material widget that is meant to be displayed at the left or right of an app to
+    navigate between a small number of views, typically between three and five.
 
     Online docs: https://flet.dev/docs/controls/navigationrail
     """
 
-    destinations: List[NavigationRailDestination] = field(default_factory=list)
+    destinations: list[NavigationRailDestination] = field(default_factory=list)
     elevation: OptionalNumber = None
     selected_index: int = 0
     extended: bool = False
