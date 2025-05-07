557e225d8e5ff430af70ecf58fe9bbf657444bf9
InesaFitsner <inesa@appveyor.com>
2025-05-07 09:58:50 -0700

    removed example

--- sdk/python/packages/flet/src/flet/controls/material/outlined_button.py
+++ sdk/python/packages/flet/src/flet/controls/material/outlined_button.py
@@ -21,23 +21,9 @@ __all__ = ["OutlinedButton"]
 @control("OutlinedButton")
 class OutlinedButton(ConstrainedControl, AdaptiveControl):
     """
-    Outlined buttons are medium-emphasis buttons. They contain actions that are important, but aren’t the primary action in an app. Outlined buttons pair well with filled buttons to indicate an alternative, secondary action.
-
-    Example:
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        page.title = "Basic outlined buttons"
-        page.add(
-            ft.OutlinedButton(text="Outlined button"),
-            ft.OutlinedButton("Disabled button", disabled=True),
-        )
-
-    ft.app(target=main)
-    ```
-
-    -----
+    Outlined buttons are medium-emphasis buttons. They contain actions that are
+    important, but aren’t the primary action in an app. Outlined buttons pair well with
+    filled buttons to indicate an alternative, secondary action.
 
     Online docs: https://flet.dev/docs/controls/outlinedbutton
     """
