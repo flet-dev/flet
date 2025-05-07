8a979e42c0a1194bb43a93121b8ddd95a5dab2a8
InesaFitsner <inesa@appveyor.com>
2025-05-07 11:36:57 -0700

    deleted example

--- sdk/python/packages/flet/src/flet/controls/material/progress_ring.py
+++ sdk/python/packages/flet/src/flet/controls/material/progress_ring.py
@@ -16,35 +16,6 @@ class ProgressRing(ConstrainedControl):
 
     A control that shows progress along a circle.
 
-    Example:
-
-    ```
-    from time import sleep
-    import flet as ft
-
-    def main(page: ft.Page):
-        pr = ft.ProgressRing(width=16, height=16, stroke_width = 2)
-
-        page.add(
-            ft.Text("Circular progress indicator", style="headlineSmall"),
-            ft.Row([pr, ft.Text("Wait for the completion...")]),
-            ft.Text("Indeterminate circular progress", style="headlineSmall"),
-            ft.Column(
-                [ft.ProgressRing(), ft.Text("I'm going to run for ages...")],
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            ),
-        )
-
-        for i in range(0, 101):
-            pr.value = i * 0.01
-            sleep(0.1)
-            page.update()
-
-    ft.app(target=main)
-    ```
-
-    -----
-
     Online docs: https://flet.dev/docs/controls/progressring
     """
 
