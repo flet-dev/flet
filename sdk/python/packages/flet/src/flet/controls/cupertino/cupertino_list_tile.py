f10fb649a84873b2fa5c12818b6eecfedbde0e13
InesaFitsner <inesa@appveyor.com>
2025-05-08 11:48:08 -0700

    Control description

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_list_tile.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_list_tile.py
@@ -17,36 +17,8 @@ __all__ = ["CupertinoListTile"]
 @control("CupertinoListTile")
 class CupertinoListTile(ConstrainedControl):
     """
-    An iOS-style list tile. The CupertinoListTile is a Cupertino equivalent of Material ListTile.
-
-    Example:
-
-    ```
-    import flet as ft
-
-
-    def main(page: ft.Page):
-        def tile_clicked(e):
-            print("Tile Clicked!")
-
-        page.add(
-            ft.CupertinoListTile(
-                notched=True,
-                additional_info=ft.Text("Wed Jan 25"),
-                bgcolor_activated=ft.colors.AMBER_ACCENT,
-                leading=ft.Icon(name=ft.cupertino_icons.GAME_CONTROLLER),
-                title=ft.Text("CupertinoListTile not notched"),
-                subtitle=ft.Text("Subtitle"),
-                trailing=ft.Icon(name=ft.cupertino_icons.ALARM),
-                on_click=tile_clicked,
-            ),
-
-        )
-
-    ft.app(target=main)
-    ```
-
-    -----
+    An iOS-style list tile. The CupertinoListTile is a Cupertino equivalent of Material
+    ListTile.
 
     Online docs: https://flet.dev/docs/controls/cupertinolisttile
     """
