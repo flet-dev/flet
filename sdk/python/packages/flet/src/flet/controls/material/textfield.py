d91ca5522274037baec3c632098f696fb8e17b24
InesaFitsner <inesa@appveyor.com>
2025-05-08 09:17:24 -0700

    TextField description

--- sdk/python/packages/flet/src/flet/controls/material/textfield.py
+++ sdk/python/packages/flet/src/flet/controls/material/textfield.py
@@ -134,30 +134,8 @@ class TextOnlyInputFilter(InputFilter):
 @control("TextField")
 class TextField(FormFieldControl, AdaptiveControl):
     """
-    A text field lets the user enter text, either with hardware keyboard or with an onscreen keyboard.
-
-    Example:
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        def button_clicked(e):
-            t.value = f"Textboxes values are:  '{tb1.value}', '{tb2.value}', '{tb3.value}', '{tb4.value}', '{tb5.value}'."
-            page.update()
-
-        t = ft.Text()
-        tb1 = ft.TextField(label="Standard")
-        tb2 = ft.TextField(label="Disabled", disabled=True, value="First name")
-        tb3 = ft.TextField(label="Read-only", read_only=True, value="Last name")
-        tb4 = ft.TextField(label="With placeholder", hint_text="Please enter text here")
-        tb5 = ft.TextField(label="With an icon", icon=ft.icons.EMOJI_EMOTIONS)
-        b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
-        page.add(tb1, tb2, tb3, tb4, tb5, b, t)
-
-    ft.app(target=main)
-    ```
-
-    -----
+    A text field lets the user enter text, either with hardware keyboard or with an
+    onscreen keyboard.
 
     Online docs: https://flet.dev/docs/controls/textfield
     """
