7b93f526602f1752ddf2369702fafd842f6baeea
InesaFitsner <inesa@appveyor.com>
2025-05-08 12:50:29 -0700

    Control description

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_slider.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_slider.py
@@ -17,11 +17,12 @@ class CupertinoSlider(ConstrainedControl):
     """
     An iOS-type slider.
 
-    It provides a visual indication of adjustable content, as well as the current setting in the total range of content.
+    It provides a visual indication of adjustable content, as well as the current
+    setting in the total range of content.
 
-    Use a slider when you want people to set defined values (such as volume or brightness), or when people would benefit from instant feedback on the effect of setting changes.
-
-    -----
+    Use a slider when you want people to set defined values (such as volume or
+    brightness), or when people would benefit from instant feedback on the effect of
+    setting changes.
 
     Online docs: https://flet.dev/docs/controls/cupertinoslider
     """
