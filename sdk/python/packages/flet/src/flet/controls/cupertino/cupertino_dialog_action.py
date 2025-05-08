bb856e04049f362dd23f450bbfd5861d31c4b5a8
InesaFitsner <inesa@appveyor.com>
2025-05-08 12:07:05 -0700

    CupertinoDialogAction description

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_dialog_action.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_dialog_action.py
@@ -13,52 +13,6 @@ class CupertinoDialogAction(Control):
     """
     A button typically used in a CupertinoAlertDialog.
 
-    Example:
-    ```
-    import flet as ft
-
-
-    def main(page: ft.Page):
-        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-        def dialog_dismissed(e):
-            page.add(ft.Text("Dialog dismissed"))
-
-        def handle_action_click(e):
-            page.add(ft.Text(f"Action clicked: {e.control.text}"))
-            page.close(cupertino_alert_dialog)
-
-        cupertino_alert_dialog = ft.CupertinoAlertDialog(
-            title=ft.Text("Cupertino Alert Dialog"),
-            content=ft.Text("Do you want to delete this file?"),
-            on_dismiss=dialog_dismissed,
-            actions=[
-                ft.CupertinoDialogAction(
-                    content="Yes",
-                    destructive=True,
-                    on_click=handle_action_click,
-                ),
-                ft.CupertinoDialogAction(
-                    content="No",
-                    default=True,
-                    on_click=handle_action_click
-                ),
-            ],
-        )
-
-        page.add(
-            ft.CupertinoFilledButton(
-                text="Open CupertinoAlertDialog",
-                on_click=lambda e: page.open(cupertino_alert_dialog),
-            )
-        )
-
-
-    ft.app(target=main)
-    ```
-
-    -----
-
     Online docs: https://flet.dev/docs/controls/cupertinodialogaction
     """
 
