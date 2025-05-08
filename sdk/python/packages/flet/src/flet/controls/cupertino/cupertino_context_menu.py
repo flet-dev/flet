290447bdbeee330afce9c33701dffaf62a22d0d5
InesaFitsner <inesa@appveyor.com>
2025-05-08 11:27:03 -0700

    docstrings

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_context_menu.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_context_menu.py
@@ -1,5 +1,4 @@
 from dataclasses import field
-from typing import List
 
 from flet.controls.adaptive_control import AdaptiveControl
 from flet.controls.base_control import control
@@ -13,14 +12,29 @@ class CupertinoContextMenu(AdaptiveControl):
     """
     A full-screen modal route that opens up when the content is long-pressed.
 
-    -----
-
     Online docs: https://flet.dev/docs/controls/cupertinocontextmenu
     """
 
     content: Control
-    actions: List[Control] = field(default_factory=list)
+    """
+    The child control to be shown. This is a required property.
+  
+    When the `CupertinoContextMenu` is long-pressed, the menu will open and this widget
+    will be moved to the new route and be expanded. This allows the child to resize to 
+    fit in its place in the new route, if it doesn't size itself.
+    """
+
+    actions: list[Control] = field(default_factory=list)
+    """
+    A list of action buttons to be shown in the menu. These actions are typically 
+    [`CupertinoContextMenuAction`](/docs/controls/cupertinocontextmenuaction)s. 
+    This list must have at least one action.
+    """
+
     enable_haptic_feedback: bool = True
+    """
+    Whether a click on the `actions` should produce haptic feedback.
+    """
 
     def before_update(self):
         super().before_update()
