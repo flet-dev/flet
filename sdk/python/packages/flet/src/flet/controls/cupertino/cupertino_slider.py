91cb9314f9c75fb85b9df6be20e47a25b06f0a27
InesaFitsner <inesa@appveyor.com>
2025-05-08 12:52:07 -0700

    docstrings

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_slider.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_slider.py
@@ -28,17 +28,80 @@ class CupertinoSlider(ConstrainedControl):
     """
 
     value: OptionalNumber = None
+    """
+    The currently selected value for this slider.
+
+    The slider's thumb is drawn at a position that corresponds to this value.
+    """
+
     min: Number = 0.0
+    """
+    The minimum value the user can select.
+
+    Defaults to `0.0`. Must be less than or equal to `max`.
+
+    If the `max` is equal to the `min`, then the slider is disabled.
+    """
+
     max: Number = 1.0
+    """
+    The maximum value the user can select.
+
+    Defaults to `1.0`. Must be greater than or equal to `min`.
+
+    If the `max` is equal to the `min`, then the slider is disabled.
+    """
+
     divisions: Optional[int] = None
+    """
+    The number of discrete divisions.
+
+    If not set, the slider is continuous.
+    """
+
     round: int = 0
+    """
+    TBD
+    """
+
     active_color: OptionalColorValue = None
+    """
+    The [color](https://flet.dev/docs/reference/colors) to use for the portion of the 
+    slider track that is active.
+
+    The "active" side of the slider is the side between the thumb and the minimum 
+    value.
+    """
+
     thumb_color: OptionalColorValue = None
+    """
+    The [color](https://flet.dev/docs/reference/colors) of the thumb.
+    """
+
     on_change: OptionalControlEventCallable = None
+    """
+    Fires when the state of the Slider is changed.
+    """
+
     on_change_start: OptionalControlEventCallable = None
+    """
+    Fires when the user starts selecting a new value for the slider.
+    """
+
     on_change_end: OptionalControlEventCallable = None
+    """
+    Fires when the user is done selecting a new value for the slider.
+    """
+
     on_focus: OptionalControlEventCallable = None
+    """
+    Fires when the control has received focus.
+    """
+
     on_blur: OptionalControlEventCallable = None
+    """
+    Fires when the control has lost focus.
+    """
 
     def before_update(self):
         super().before_update()
