53147fa17e2c74284499246ab333936722300bfc
InesaFitsner <inesa@appveyor.com>
2025-05-08 11:19:28 -0700

    CupertinoCheckbox description

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_checkbox.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_checkbox.py
@@ -19,25 +19,9 @@ __all__ = ["CupertinoCheckbox"]
 @control("CupertinoCheckbox")
 class CupertinoCheckbox(ConstrainedControl):
     """
-    A macOS style checkbox. Checkbox allows to select one or more items from a group, or switch between two mutually exclusive options (checked or unchecked, on or off).
+    A macOS style checkbox. Checkbox allows to select one or more items from a group,
+    or switch between two mutually exclusive options (checked or unchecked, on or off).
 
-    Example:
-    ```
-    import flet as ft
-
-    def main(page):
-        c = ft.CupertinoCheckbox(
-            label="Cupertino Checkbox",
-            active_color=ft.colors.GREEN,
-            inactive_color=ft.colors.RED,
-            check_color=ft.colors.BLUE,
-        ),
-        page.add(c)
-
-    ft.app(target=main)
-    ```
-
-    -----
     Online docs: https://flet.dev/docs/controls/cupertinocheckbox
     """
 
