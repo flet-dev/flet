0f679d9fcb19b0cfaeb582f5c50ee7339d056261
InesaFitsner <inesa@appveyor.com>
2025-05-08 12:54:39 -0700

    Update cupertino_switch.py

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_switch.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_switch.py
@@ -20,26 +20,6 @@ class CupertinoSwitch(ConstrainedControl):
     """
     An iOS-style switch. Used to toggle the on/off state of a single setting.
 
-    Example:
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        page.add(
-            ft.CupertinoSwitch(label="Cupertino Switch", value=True),
-            ft.Switch(label="Material Checkbox", value=True),
-            ft.Container(height=20),
-            ft.Text(
-                "Adaptive Switch shows as CupertinoSwitch on macOS and iOS and "
-                "as Switch on other platforms:"
-            ),
-            ft.Switch(adaptive=True, label="Adaptive Switch", value=True),
-        )
-
-    ft.app(target=main)
-    ```
-    -----
-
     Online docs: https://flet.dev/docs/controls/cupertinoswitch
     """
 
