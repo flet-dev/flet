ef961bff57d75bf15857c8f997b4bbf52e492e4f
InesaFitsner <inesa@appveyor.com>
2025-05-08 12:12:06 -0700

    Update cupertino_navigation_bar.py

--- sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_navigation_bar.py
+++ sdk/python/packages/flet/src/flet/controls/cupertino/cupertino_navigation_bar.py
@@ -1,5 +1,5 @@
 from dataclasses import field
-from typing import List, Optional
+from typing import Optional
 
 from flet.controls.base_control import control
 from flet.controls.border import Border
@@ -21,42 +21,13 @@ class CupertinoNavigationBar(ConstrainedControl):
     """
     An iOS-styled bottom navigation tab bar.
 
-    Navigation bars offer a persistent and convenient way to switch between primary destinations in an app.
-
-    Example:
-
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        page.title = "CupertinoNavigationBar Example"
-        page.navigation_bar = ft.CupertinoNavigationBar(
-            bgcolor=ft.colors.AMBER_100,
-            inactive_color=ft.colors.GREY,
-            active_color=ft.colors.BLACK,
-            on_change=lambda e: print("Selected tab:", e.control.selected_index),
-            destinations=[
-                ft.NavigationBarDestination(icon=ft.icons.EXPLORE, label="Explore"),
-                ft.NavigationBarDestination(icon=ft.icons.COMMUTE, label="Commute"),
-                ft.NavigationBarDestination(
-                    icon=ft.icons.BOOKMARK_BORDER,
-                    selected_icon=ft.icons.BOOKMARK,
-                    label="Explore",
-                ),
-            ]
-        )
-        page.add(ft.SafeArea(ft.Text("Body!")))
-
-
-    ft.app(target=main)
-    ```
-
-    -----
+    Navigation bars offer a persistent and convenient way to switch between primary
+    destinations in an app.
 
     Online docs: https://flet.dev/docs/controls/cupertinonavigationbar
     """
 
-    destinations: List[NavigationBarDestination] = field(default_factory=list)
+    destinations: list[NavigationBarDestination] = field(default_factory=list)
     selected_index: int = 0
     bgcolor: OptionalColorValue = None
     active_color: OptionalColorValue = None
