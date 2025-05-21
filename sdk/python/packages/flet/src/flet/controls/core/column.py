b2ae4a4c82c553202a8a3bf223d5a3239174b645
InesaFitsner <inesa@appveyor.com>
2025-05-21 10:26:18 -0700

    Column description

--- sdk/python/packages/flet/src/flet/controls/core/column.py
+++ sdk/python/packages/flet/src/flet/controls/core/column.py
@@ -1,5 +1,4 @@
 from dataclasses import field
-from typing import List
 
 from flet.controls.adaptive_control import AdaptiveControl
 from flet.controls.base_control import control
@@ -14,41 +13,13 @@ __all__ = ["Column"]
 @control("Column")
 class Column(ConstrainedControl, ScrollableControl, AdaptiveControl):
     """
-    Container allows to decorate a control with background color and border and position it with padding, margin and alignment.
-
-    Example:
-
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        page.title = "Column example"
-
-        page.add(
-            ft.Column(
-                expand=True,
-                controls=[
-                    ft.Container(
-                        expand=1,
-                        content=ft.Text("Container 1"),
-                        bgcolor=ft.colors.GREEN_100,
-                    ),
-                    ft.Container(
-                        expand=2, content=ft.Text("Container 2"), bgcolor=ft.colors.RED_100
-                    ),
-                ],
-            ),
-        ),
-
-    ft.app(target=main)
-    ```
-
-    -----
+    Container allows to decorate a control with background color and border and 
+    position it with padding, margin and alignment.
 
     Online docs: https://flet.dev/docs/controls/column
     """
 
-    controls: List[Control] = field(default_factory=list)
+    controls: list[Control] = field(default_factory=list)
     alignment: MainAxisAlignment = MainAxisAlignment.START
     horizontal_alignment: CrossAxisAlignment = CrossAxisAlignment.START
     spacing: Number = 10
