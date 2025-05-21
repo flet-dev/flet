2c574fd4dfbf797f8ccd745df5fcae1604158057
InesaFitsner <inesa@appveyor.com>
2025-05-21 10:50:46 -0700

    FletApp properties docstrings

--- sdk/python/packages/flet/src/flet/controls/core/flet_app.py
+++ sdk/python/packages/flet/src/flet/controls/core/flet_app.py
@@ -9,11 +9,47 @@ __all__ = ["FletApp"]
 
 @control("FletApp")
 class FletApp(ConstrainedControl):
+    """
+    Renders another Flet app in the current app, similar to HTML IFrame, but for Flet.
+    """
+
     url: Optional[str] = None
+    """
+    Flet app URL, e.g. `http://localhost:8550` or `flet.sock`.
+    """
+
     args: Optional[dict[str, Any]] = None
+    """
+    TBD
+    """
+
     force_pyodide: bool = False
+    """
+    TBD
+    """
+
     reconnect_interval_ms: Optional[int] = None
+    """
+    Delay, in milliseconds, between reconnection attempts.
+    """
+
     reconnect_timeout_ms: Optional[int] = None
+    """
+    Total time to try reconnecting.
+    """
+
     show_app_startup_screen: bool = False
+    """
+    TBD
+    """
+
     app_startup_screen_message: Optional[str] = None
+    """
+    TBD
+    """
+
     on_error: OptionalControlEventCallable = None
+    """
+    Fires when a connection or any unhandled error occurs.
+    """
+
