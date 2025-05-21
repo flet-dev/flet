2b68f5a48bd4c98fb0cf88d16ba837437ca7b391
InesaFitsner <inesa@appveyor.com>
2025-05-21 12:47:48 -0700

    GridView description

--- sdk/python/packages/flet/src/flet/controls/core/grid_view.py
+++ sdk/python/packages/flet/src/flet/controls/core/grid_view.py
@@ -1,5 +1,5 @@
 from dataclasses import field
-from typing import List, Optional
+from typing import Optional
 
 from flet.controls.adaptive_control import AdaptiveControl
 from flet.controls.base_control import control
@@ -17,50 +17,13 @@ class GridView(ConstrainedControl, ScrollableControl, AdaptiveControl):
     """
     A scrollable, 2D array of controls.
 
-    GridView is very effective for large lists (thousands of items). Prefer it over wrapping `Column` or `Row` for smooth scrolling.
-
-    Example:
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        page.title = "GridView Example"
-        page.theme_mode = ft.ThemeMode.DARK
-        page.padding = 50
-        page.update()
-
-        images = ft.GridView(
-            expand=1,
-            runs_count=5,
-            max_extent=150,
-            child_aspect_ratio=1.0,
-            spacing=5,
-            run_spacing=5,
-        )
-
-        page.add(images)
-
-        for i in range(0, 60):
-            images.controls.append(
-                ft.Image(
-                    src=f"https://picsum.photos/150/150?{i}",
-                    fit=ft.ImageFit.NONE,
-                    repeat=ft.ImageRepeat.NO_REPEAT,
-                    border_radius=ft.border_radius.all(10),
-                )
-            )
-        page.update()
-
-    ft.app(target=main)
-
-    ```
-
-    -----
+    GridView is very effective for large lists (thousands of items). Prefer it over 
+    wrapping `Column` or `Row` for smooth scrolling.
 
     Online docs: https://flet.dev/docs/controls/gridview
     """
 
-    controls: List[Control] = field(default_factory=list)
+    controls: list[Control] = field(default_factory=list)
     horizontal: bool = False
     runs_count: int = 1
     max_extent: Optional[int] = None
