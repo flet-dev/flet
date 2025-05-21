cd8c965c923f0f993f144aa9a4c1f0c4355b9b5b
InesaFitsner <inesa@appveyor.com>
2025-05-21 10:18:56 -0700

    AnimatedSwitcher description

--- sdk/python/packages/flet/src/flet/controls/core/animated_switcher.py
+++ sdk/python/packages/flet/src/flet/controls/core/animated_switcher.py
@@ -19,50 +19,8 @@ class AnimatedSwitcherTransition(Enum):
 @control("AnimatedSwitcher")
 class AnimatedSwitcher(ConstrainedControl):
     """
-    A control that by default does a cross-fade between a new control and the control previously set on the AnimatedSwitcher as a `content`.
-
-    Example:
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-
-        c1 = ft.Container(
-            ft.Text("Hello!", style=ft.TextThemeStyle.HEADLINE_MEDIUM),
-            alignment=ft.Alignment.center(),
-            width=200,
-            height=200,
-            bgcolor=ft.colors.GREEN,
-        )
-        c2 = ft.Container(
-            ft.Text("Bye!", size=50),
-            alignment=ft.Alignment.center(),
-            width=200,
-            height=200,
-            bgcolor=ft.colors.YELLOW,
-        )
-        c = ft.AnimatedSwitcher(
-            content=c1,
-            transition=ft.AnimatedSwitcherTransition.SCALE,
-            duration=500,
-            reverse_duration=100,
-            switch_in_curve=ft.AnimationCurve.BOUNCE_OUT,
-            switch_out_curve=ft.AnimationCurve.BOUNCE_IN,
-        )
-
-        def animate(e):
-            c.content = c2 if c.content == c1 else c1
-            c.update()
-
-        page.add(
-            c,
-            ft.ElevatedButton("Animate!", on_click=animate),
-        )
-
-    ft.app(target=main)
-    ```
-
-    -----
+    A control that by default does a cross-fade between a new control and the control 
+    previously set on the AnimatedSwitcher as a `content`.
 
     Online docs: https://flet.dev/docs/controls/animatedswitcher
     """
