4900fa005cdbec4c9453acd4e1df2e2156cbf4f6
InesaFitsner <inesa@appveyor.com>
2025-05-21 12:40:01 -0700

    GestureDetector description

--- sdk/python/packages/flet/src/flet/controls/core/gesture_detector.py
+++ sdk/python/packages/flet/src/flet/controls/core/gesture_detector.py
@@ -1,20 +1,20 @@
-from typing import List, Optional
+from typing import Optional
 
 from flet.controls.adaptive_control import AdaptiveControl
 from flet.controls.base_control import control
 from flet.controls.constrained_control import ConstrainedControl
 from flet.controls.control import Control
 from flet.controls.events import (
-    TapEvent,
-    LongPressEndEvent,
+    DragEndEvent,
     DragStartEvent,
     DragUpdateEvent,
-    DragEndEvent,
+    HoverEvent,
+    LongPressEndEvent,
+    ScaleEndEvent,
     ScaleStartEvent,
     ScaleUpdateEvent,
-    ScaleEndEvent,
-    HoverEvent,
     ScrollEvent,
+    TapEvent,
 )
 from flet.controls.types import MouseCursor, OptionalEventCallable, PointerDeviceType
 
@@ -28,46 +28,8 @@ class GestureDetector(ConstrainedControl, AdaptiveControl):
 
     Attempts to recognize gestures that correspond to its non-null callbacks.
 
-    If this control has a content, it defers to that child control for its sizing behavior. If it does not have a content, it grows to fit the parent instead.
-
-    Example:
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        def on_pan_update1(e: ft.DragUpdateEvent):
-            c.top = max(0, c.top + e.delta_y)
-            c.left = max(0, c.left + e.delta_x)
-            c.update()
-
-        def on_pan_update2(e: ft.DragUpdateEvent):
-            e.control.top = max(0, e.control.top + e.delta_y)
-            e.control.left = max(0, e.control.left + e.delta_x)
-            e.control.update()
-
-        gd = ft.GestureDetector(
-            mouse_cursor=ft.MouseCursor.MOVE,
-            drag_interval=50,
-            on_pan_update=on_pan_update1,
-        )
-
-        c = ft.Container(gd, bgcolor=ft.colors.AMBER, width=50, height=50, left=0, top=0)
-
-        gd1 = ft.GestureDetector(
-            mouse_cursor=ft.MouseCursor.MOVE,
-            drag_interval=10,
-            on_vertical_drag_update=on_pan_update2,
-            left=100,
-            top=100,
-            content=ft.Container(bgcolor=ft.colors.BLUE, width=50, height=50),
-        )
-
-        page.add( ft.Stack([c, gd1], width=1000, height=500))
-
-    ft.app(target=main)
-    ```
-
-    -----
+    If this control has a content, it defers to that child control for its sizing 
+    behavior. If it does not have a content, it grows to fit the parent instead.
 
     Online docs: https://flet.dev/docs/controls/gesturedetector
     """
@@ -79,7 +41,7 @@ class GestureDetector(ConstrainedControl, AdaptiveControl):
     multi_tap_touches: int = 0
     exclude_from_semantics: bool = False
     trackpad_scroll_causes_scale: bool = False
-    allowed_devices: Optional[List[PointerDeviceType]] = None
+    allowed_devices: Optional[list[PointerDeviceType]] = None
     on_tap: OptionalEventCallable[TapEvent] = None
     on_tap_down: OptionalEventCallable[TapEvent] = None
     on_tap_up: OptionalEventCallable[TapEvent] = None
