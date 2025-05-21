b706181d4e0a5ed0400c8574c20bbd0db839bdfb
InesaFitsner <inesa@appveyor.com>
2025-05-21 09:58:07 -0700

    removed example from description

--- sdk/python/packages/flet/src/flet/controls/core/alert_dialog.py
+++ sdk/python/packages/flet/src/flet/controls/core/alert_dialog.py
@@ -1,5 +1,5 @@
 from dataclasses import field
-from typing import List, Optional
+from typing import Optional
 
 from flet.controls.alignment import Alignment
 from flet.controls.base_control import control
@@ -23,49 +23,9 @@ __all__ = ["AlertDialog"]
 @control("AlertDialog")
 class AlertDialog(DialogControl):
     """
-    An alert dialog informs the user about situations that require acknowledgement. An alert dialog has an optional title and an optional list of actions. The title is displayed above the content and the actions are displayed below the content.
-
-    Example:
-    ```
-    import flet as ft
-
-
-    def main(page: ft.Page):
-        page.title = "AlertDialog examples"
-        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-        dlg = ft.AlertDialog(
-            title=ft.Text("Hi, this is a non-modal dialog!"),
-            on_dismiss=lambda e: page.add(ft.Text("Non-modal dialog dismissed")),
-        )
-
-        def handle_close(e):
-            page.close(dlg_modal)
-            page.add(ft.Text(f"Modal dialog closed with action: {e.control.text}"))
-
-        dlg_modal = ft.AlertDialog(
-            modal=True,
-            title=ft.Text("Please confirm"),
-            content=ft.Text("Do you really want to delete all those files?"),
-            actions=[
-                ft.TextButton("Yes", on_click=handle_close),
-                ft.TextButton("No", on_click=handle_close),
-            ],
-            actions_alignment=ft.MainAxisAlignment.END,
-            on_dismiss=lambda e: page.add(
-                ft.Text("Modal dialog dismissed"),
-            ),
-        )
-
-        page.add(
-            ft.ElevatedButton("Open dialog", on_click=lambda e: page.open(dlg)),
-            ft.ElevatedButton("Open modal dialog", on_click=lambda e: page.open(dlg_modal)),
-        )
-
-
-    ft.app(target=main)
-    ```
-    -----
+    An alert dialog informs the user about situations that require acknowledgement. An 
+    alert dialog has an optional title and an optional list of actions. The title is 
+    displayed above the content and the actions are displayed below the content.
 
     Online docs: https://flet.dev/docs/controls/alertdialog
     """
@@ -73,7 +33,7 @@ class AlertDialog(DialogControl):
     content: Optional[Control] = None
     modal: bool = False
     title: Optional[StrOrControl] = None
-    actions: List[Control] = field(default_factory=list)
+    actions: list[Control] = field(default_factory=list)
     bgcolor: OptionalColorValue = None
     elevation: OptionalNumber = None
     icon: Optional[Control] = None
@@ -101,4 +61,5 @@ class AlertDialog(DialogControl):
         super().before_update()
         assert (
             self.title or self.content or self.actions
-        ), "AlertDialog has nothing to display. Provide at minimum one of the following: title, content, actions"
+        ), "AlertDialog has nothing to display. Provide at minimum one of the "
+        "following: title, content, actions"
