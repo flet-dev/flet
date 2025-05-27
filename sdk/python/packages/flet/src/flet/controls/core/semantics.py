30ca4b31caee2ba60efc52604775cb67d2c08872
InesaFitsner <inesa@appveyor.com>
2025-05-27 10:59:20 -0700

    Semantics description

--- sdk/python/packages/flet/src/flet/controls/core/semantics.py
+++ sdk/python/packages/flet/src/flet/controls/core/semantics.py
@@ -11,11 +11,11 @@ __all__ = ["Semantics"]
 @control("Semantics")
 class Semantics(Control):
     """
-    A control that annotates the control tree with a description of the meaning of the widgets.
+    A control that annotates the control tree with a description of the meaning of the 
+    widgets.
 
-    Used by accessibility tools, search engines, and other semantic analysis software to determine the meaning of the application.
-
-    -----
+    Used by accessibility tools, search engines, and other semantic analysis software 
+    to determine the meaning of the application.
 
     Online docs: https://flet.dev/docs/controls/semantics
     """
