f84886b285267e912d54db44012379943ca22ca0
InesaFitsner <inesa@appveyor.com>
2025-05-27 09:35:04 -0700

    Image description

--- sdk/python/packages/flet/src/flet/controls/core/image.py
+++ sdk/python/packages/flet/src/flet/controls/core/image.py
@@ -22,27 +22,6 @@ class Image(ConstrainedControl):
     """
     A control that displays an image.
 
-    Example:
-    ```
-    import flet as ft
-
-    def main(page: ft.Page):
-        page.title = "Image Example"
-
-        img = ft.Image(
-            src=f"/icons/icon-512.png",
-            width=100,
-            height=100,
-            fit=ft.ImageFit.CONTAIN,
-        )
-
-        page.add(img)
-
-    ft.app(target=main)
-    ```
-
-    -----
-
     Online docs: https://flet.dev/docs/controls/image
     """
 
