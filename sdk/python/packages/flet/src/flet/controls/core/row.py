cde014175b79b3c3a310235dba76ea65969edc30
InesaFitsner <inesa@appveyor.com>
2025-05-27 10:51:55 -0700

    Row description

--- sdk/python/packages/flet/src/flet/controls/core/row.py
+++ sdk/python/packages/flet/src/flet/controls/core/row.py
@@ -1,5 +1,4 @@
 from dataclasses import field
-from typing import List
 
 from flet.controls.adaptive_control import AdaptiveControl
 from flet.controls.base_control import control
@@ -16,42 +15,13 @@ class Row(ConstrainedControl, ScrollableControl, AdaptiveControl):
     """
     A control that displays its children in a horizontal array.
 
-    To cause a child control to expand and fill the available horizontal space, set its `expand` property.
-
-    Example:
-
-    ```
-    import flet as ft
-
-
-    def main(page: ft.Page):
-        page.title = "Row example"
-
-        page.add(
-            ft.Row(
-                controls=[
-                    ft.Container(
-                        expand=1,
-                        content=ft.Text("Container 1"),
-                        bgcolor=ft.colors.GREEN_100,
-                    ),
-                    ft.Container(
-                        expand=2, content=ft.Text("Container 2"), bgcolor=ft.colors.RED_100
-                    ),
-                ],
-            ),
-        ),
-
-
-    ft.app(target=main)
-    ```
-
-    -----
+    To cause a child control to expand and fill the available horizontal space, set its 
+    `expand` property.
 
     Online docs: https://flet.dev/docs/controls/row
     """
 
-    controls: List[Control] = field(default_factory=list)
+    controls: list[Control] = field(default_factory=list)
     alignment: MainAxisAlignment = MainAxisAlignment.START
     vertical_alignment: CrossAxisAlignment = CrossAxisAlignment.CENTER
     spacing: Number = 10
