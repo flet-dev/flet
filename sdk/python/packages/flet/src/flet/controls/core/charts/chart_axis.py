054d67b39db253edd05587099be4770e2af92ee8
InesaFitsner <inesa@appveyor.com>
2025-05-28 11:52:37 -0700

    ChartAxis docstrings

--- sdk/python/packages/flet/src/flet/controls/core/charts/chart_axis.py
+++ sdk/python/packages/flet/src/flet/controls/core/charts/chart_axis.py
@@ -10,8 +10,34 @@ from flet.controls.types import OptionalNumber
 @control("a")
 class ChartAxis(Control):
     title: Optional[Control] = None
+    """
+    A `Control` to display as axis title.
+    """
+
     title_size: OptionalNumber = None
+    """
+    Width or height of title area.
+    """
+
     show_labels: Optional[bool] = None
+    """
+    If `True`, displays the `labels` along the axis. If `labels` is empty then
+    automatic labels are displayed.
+    """
+
     labels: list[ChartAxisLabel] = field(default_factory=list)
+    """
+    The list of [`ChartAxisLabel`](https://flet.dev/docs/reference/types/chartaxislabel)
+    objects to set custom axis labels for only specific values.
+    """
+
     labels_interval: OptionalNumber = None
+    """
+    The interval between automatic labels.
+    """
+
     labels_size: OptionalNumber = None
+    """
+    Width or height of labels area.
+    """
+
