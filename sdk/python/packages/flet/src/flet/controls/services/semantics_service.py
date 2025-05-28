2bc9a927a1ccc6026f04db3b0b6ab37423f115e6
InesaFitsner <inesa@appveyor.com>
2025-05-28 10:04:44 -0700

    SemanticsService async methods docstrings

--- sdk/python/packages/flet/src/flet/controls/services/semantics_service.py
+++ sdk/python/packages/flet/src/flet/controls/services/semantics_service.py
@@ -21,6 +21,11 @@ class SemanticsService(Service):
     """
 
     async def announce_tooltip_async(self, message: str):
+        """
+        Sends a semantic announcement of a tooltip. Currently honored on Android only.
+        
+        The provided `message` will be read by TalkBack.
+        """
         await self._invoke_method_async(
             "announce_tooltip", arguments={"message": message}
         )
@@ -39,6 +44,17 @@ class SemanticsService(Service):
         rtl: bool = False,
         assertiveness: Assertiveness = Assertiveness.POLITE,
     ):
+        """
+        Sends a semantic announcement with the given `message`. This should preferably
+        be used for announcements that are not seamlessly announced by the system as a
+        result of a UI state change.
+
+        `rtl` is a boolean and indicates the text direction of the `message`.
+
+        The `assertiveness` level of the announcement is only supported by the web
+        engine and has no effect on other platforms. Value is an `Assertiveness` enum
+        and can either be `Assertiveness.ASSERTIVE` or `Assertiveness.POLITE` (default).
+        """
         await self._invoke_method_async(
             "announce_message",
             arguments={
