3ab31e06475d1723d8bde76734ae56107d60dbb1
Feodor Fitsner <feodor@appveyor.com>
2025-05-28 18:01:53 -0700

    Center control

--- packages/flet/lib/src/controls/center.dart
+++ packages/flet/lib/src/controls/center.dart
@@ -1,7 +1,5 @@
+import 'package:flet/flet.dart';
 import 'package:flutter/widgets.dart';
-
-import '../models/control.dart';
-import 'control_widget.dart';
 
 class CenterControl extends StatelessWidget {
   final Control control;
@@ -11,12 +9,6 @@ class CenterControl extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     debugPrint("Center.build: ${control.id}");
-
-    Widget child = SizedBox.shrink();
-    var value = control.properties["child"];
-    if (value is Control) {
-      child = ControlWidget(control: value);
-    }
-    return Center(child: child);
+    return Center(child: control.buildWidget("content"));
   }
 }
