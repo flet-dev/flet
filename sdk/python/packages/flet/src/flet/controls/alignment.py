34b37c2ecdcdbc2410827c853a7fb57cadf1d1c8
InesaFitsner <inesa@appveyor.com>
2025-05-29 11:28:42 -0700

    Alignment docstrings

--- sdk/python/packages/flet/src/flet/controls/alignment.py
+++ sdk/python/packages/flet/src/flet/controls/alignment.py
@@ -19,56 +19,85 @@ class Axis(Enum):
 
 @dataclass
 class Alignment:
+    """
+    Used to define an alignment relative to the center.
+    """
+
     x: Number
+    """
+    Represents the horizontal distance from the center. It's value ranges between
+    `-1.0` and `1.0`.
+    """
+
     y: Number
+    """
+    Represents the vertical distance from the center. It's value ranges between
+    `-1.0` and `1.0`.
+    """
 
     @classmethod
     def bottom_center(cls) -> "Alignment":
+        """
+        Represents the bottom center and is equivalent to `Alignment(0.0, 1.0)`.
+        """
         return Alignment(0, 1)
 
     @classmethod
     def bottom_left(cls) -> "Alignment":
+        """
+        Represents the bottom left corner and is equivalent to `Alignment(-1.0, 1.0)`.
+        """
         return Alignment(-1, 1)
 
     @classmethod
     def bottom_right(cls) -> "Alignment":
+        """
+        Represents the bottom right corner and is equivalent to `Alignment(1.0, 1.0)`.
+        """
         return Alignment(1, 1)
 
     @classmethod
     def center(cls) -> "Alignment":
+        """
+        Represents the center and is equivalent to `Alignment(0.0, 0.0)`.
+        """
         return Alignment(0, 0)
 
     @classmethod
     def center_left(cls) -> "Alignment":
+        """
+        Represents the center left and is equivalent to `Alignment(-1.0, 0.0)`.
+        """
         return Alignment(-1, 0)
 
     @classmethod
     def center_right(cls) -> "Alignment":
+        """
+        Represents the center right and is equivalent to `Alignment(1.0, 0.0)`.
+        """
         return Alignment(1, 0)
 
     @classmethod
     def top_center(cls) -> "Alignment":
+        """
+        Represents the top center and is equivalent to `Alignment(0.0, -1.0)`.
+        """
         return Alignment(0, -1)
 
     @classmethod
     def top_left(cls) -> "Alignment":
+        """
+        Represents the top left corner and is equivalent to `Alignment(-1.0, -1.0)`.
+        """
         return Alignment(-1, -1)
 
     @classmethod
     def top_right(cls) -> "Alignment":
+        """
+        Represents the top right corner and is equivalent to `Alignment(1.0, -1.0)`.
+        """
         return Alignment(1, -1)
-
-
-# Constants
-# bottom_center = Alignment(0, 1)
-# bottom_left = Alignment(-1, 1)
-# bottom_right = Alignment(1, 1)
-# center = Alignment(0, 0)
-# center_left = Alignment(-1, 0)
-# center_right = Alignment(1, 0)
-# top_center = Alignment(0, -1)
-# top_left = Alignment(-1, -1)
-# top_right = Alignment(1, -1)
+    
 
 # Typing
 OptionalAlignment = Optional[Alignment]
