69ef0688a5b96d2dbfd167c9c7d90a79a7652e82
InesaFitsner <inesa@appveyor.com>
2025-06-03 11:08:40 -0700

    Duration and Gradient docstrings

--- sdk/python/packages/flet/src/flet/controls/gradients.py
+++ sdk/python/packages/flet/src/flet/controls/gradients.py
@@ -1,7 +1,7 @@
 import math
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import List, Optional
+from typing import Optional
 
 from flet.controls.alignment import Alignment
 
@@ -25,20 +25,69 @@ class GradientTileMode(Enum):
 
 @dataclass
 class Gradient:
-    colors: List[str]
+    """
+    A shader that renders a color gradient.
+
+    There are several types of gradients: `LinearGradient`, `RadialGradient` and 
+    `SweepGradient`.
+    """
+
+    colors: list[str]
+    """
+    The [colors](https://flet.dev/docs/reference/colors) the gradient should obtain at 
+    each of the stops. This list must contain at least two colors.
+
+    If `stops` is provided, this list must have the same length as `stops`.
+    """
+
     tile_mode: GradientTileMode = GradientTileMode.CLAMP
+    """
+    How this gradient should tile the plane beyond in the region before `begin` and 
+    after `end`. The value is of type
+    [GradientTileMode](https://flet.dev/docs/reference/types/gradienttilemode).
+    """
+
     rotation: OptionalNumber = None
-    stops: Optional[List[Number]] = None
+    """
+    The rotation of the gradient in
+    [radians](https://en.wikipedia.org/wiki/Radian), around the center-point of its
+    bounding box.
+    """
+
+    stops: Optional[list[Number]] = None
+    """
+    A list of values from `0.0` to `1.0` that denote fractions along the gradient.
+
+    If provided, this list must have the same length as `colors`. If the first value is
+    not `0.0`, then a stop with position `0.0` and a color equal to the first color in
+    `colors` is implied. If the last value is not `1.0`, then a stop with position `1.0`
+    and a color equal to the last color in `colors` is implied.
+    """
+
     type: str = ""
 
 
 @dataclass
 class LinearGradient(Gradient):
+    """
+    More information on Linear gradient [here](https://api.flutter.dev/flutter/painting/LinearGradient-class.html).
+    """
+
     begin: Alignment = field(default_factory=lambda: Alignment.center_left())
+    """
+    An instance of [Alignment](https://flet.dev/docs/reference/types/alignment). The
+    offset at which stop `0.0` of the gradient is placed.
+    """
+
     end: Alignment = field(default_factory=lambda: Alignment.center_right())
+    """
+    An instance of [Alignment](https://flet.dev/docs/reference/types/alignment). The
+    offset at which stop `1.0` of the gradient is placed.
+    """
 
     def __post_init__(self):
         self.type = "linear"
+
 
 
 @dataclass
