2e623ccd3e99b3301c46fd03dfca437a45ae01bb
InesaFitsner <inesa@appveyor.com>
2025-06-03 12:31:41 -0700

    Page description

--- sdk/python/packages/flet/src/flet/controls/page.py
+++ sdk/python/packages/flet/src/flet/controls/page.py
@@ -94,22 +94,6 @@ class Page(PageView):
     A page instance and the root view are automatically created when a new
     user session started.
 
-    Example:
-
-    ```
-    import flet as ft
-
-
-    def main(page: ft.Page):
-        page.title = "New page"
-        page.add(ft.Text("Hello"))
-
-
-    ft.app(target=main)
-    ```
-
-    -----
-
     Online docs: https://flet.dev/docs/controls/page
     """
 
