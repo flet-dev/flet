29a5ffa04334a145e478d415e0714a88f9c547ae
InesaFitsner <inesa@appveyor.com>
2025-06-03 10:48:44 -0700

    DialogControl properties

--- sdk/python/packages/flet/src/flet/controls/dialog_control.py
+++ sdk/python/packages/flet/src/flet/controls/dialog_control.py
@@ -6,5 +6,18 @@ from flet.controls.types import OptionalControlEventCallable
 
 @dataclass(kw_only=True)
 class DialogControl(AdaptiveControl):
+    """
+    TBD
+    """
+    
     open: bool = False
+    """
+    Set to `True` to display a dialog.
+
+    Value is of type `bool` and defaults to `False`.
+    """
+    
     on_dismiss: OptionalControlEventCallable = None
+    """
+    Fires when dialog is dismissed.
+    """
