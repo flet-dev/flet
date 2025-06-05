cb42cc352d39dd85c50b19f1321149129728bc80
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2025-06-05 09:18:50 +0200

    Update window_drag_area.dart

--- packages/flet/lib/src/controls/window_drag_area.dart
+++ packages/flet/lib/src/controls/window_drag_area.dart
@@ -59,32 +59,3 @@ class WindowDragAreaControl extends StatelessWidget {
     return ConstrainedControl(control: control, child: wda);
   }
 }
-
-class WindowDragArea extends StatelessWidget {
-  final Widget child;
-  final bool maximizable;
-
-  const WindowDragArea(
-      {super.key, required this.child, required this.maximizable});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onPanStart: (DragStartDetails details) {
-        windowManager.startDragging();
-      },
-      onDoubleTap: maximizable
-          ? () async {
-              bool isMaximized = await windowManager.isMaximized();
-              if (!isMaximized) {
-                windowManager.maximize();
-              } else {
-                windowManager.unmaximize();
-              }
-            }
-          : null,
-      child: child,
-    );
-  }
-}
