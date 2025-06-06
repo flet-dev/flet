02a6c5d9a477b591226efd8641768de6f2908257
InesaFitsner <inesa@appveyor.com>
2025-06-06 11:24:28 -0700

    TextStyle docstrings

--- sdk/python/packages/flet/src/flet/controls/text_style.py
+++ sdk/python/packages/flet/src/flet/controls/text_style.py
@@ -71,23 +71,116 @@ class TextDecorationStyle(Enum):
 
 @dataclass
 class TextStyle:
+    """
+    A style describing how to format and paint text. It has the following properties:
+    """
+
     size: OptionalNumber = None
+    """
+    The size of glyphs (in logical pixels) to use when painting the text.
+
+    Defaults to `14`.
+    """
+
     height: OptionalNumber = None
+    """
+    The height of this text span, as a multiple of the font size.
+
+    See detailed explanation here:
+    https://api.flutter.dev/flutter/painting/TextStyle/height.html
+    """
+
     weight: Optional[FontWeight] = None
+    """
+    Value is of type https://flet.dev/docs/reference/types/fontweight and defaults to
+    `FontWeight.NORMAL`.
+    """
+
     italic: Optional[bool] = None
+    """
+    `True` to use italic typeface.
+    """
+
     decoration: Optional[TextDecoration] = None
+    """
+    The decorations to paint near the text (e.g., an underline).
+
+    Value is of type https://flet.dev/docs/reference/types/textdecoration.
+    """
+
     decoration_color: OptionalColorValue = None
+    """
+    The https://flet.dev/docs/reference/colors in which to paint the text decorations.
+    """
+
     decoration_thickness: OptionalNumber = None
+    """
+    The thickness of the decoration stroke as a multiplier of the thickness defined by
+    the font.
+    """
+
     decoration_style: "OptionalTextDecorationStyle" = None
+    """
+    The style in which to paint the text decorations (e.g., dashed).
+
+    Value is of type https://flet.dev/docs/reference/types/textdecorationstyle and
+    defaults to `TextDecorationStyle.SOLID`.
+    """
+
     font_family: Optional[str] = None
+    """
+    See https://flet.dev/docs/controls/text#font_family.
+    """
+
     color: OptionalColorValue = None
+    """
+    Text foreground https://flet.dev/docs/reference/colors.
+    """
+
     bgcolor: OptionalColorValue = None
+    """
+    Text background https://flet.dev/docs/reference/colors.
+    """
+
     shadow: Optional[ShadowValue] = None
+    """
+    The value of this property is a single instance or a list of
+    https://flet.dev/docs/reference/types/boxshadow class instances.
+    """
+
     foreground: Optional[Paint] = None
+    """
+    The paint drawn as a foreground for the text.
+
+    Value is of type https://flet.dev/docs/reference/types/paint.
+    """
+
     letter_spacing: OptionalNumber = None
+    """
+    The amount of space (in logical pixels) to add between each letter. A negative
+    value can be used to bring the letters closer.
+    """
+
     word_spacing: OptionalNumber = None
+    """
+    The amount of space (in logical pixels) to add at each sequence of white-space
+    (i.e. between each word). A negative value can be used to bring the words closer.
+    """
+
     overflow: Optional[TextOverflow] = None
+    """
+    How visual text overflow should be handled.
+
+    Value is of type https://flet.dev/docs/reference/types/textoverflow.
+    """
+
     baseline: Optional[TextBaseline] = None
+    """
+    The common baseline that should be aligned between this text span and its parent
+    text span, or, for the root text spans, with the line box.
+
+    Value is of type https://flet.dev/docs/reference/types/textbaseline.
+    """
 
 
 @dataclass
