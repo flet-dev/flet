23bcf0ded65a9ae2dd7c496ff82e9bad45e2f07a
Feodor Fitsner <feodor@appveyor.com>
2025-07-25 11:42:58 -0700

    Check macOS build

--- ci/common.sh
+++ ci/common.sh
@@ -3,7 +3,6 @@ export flet_sdk_root=$root/sdk/python
 echo "flet_sdk_root: $flet_sdk_root"
 
 python --version
-pip install --upgrade setuptools wheel twine tomlkit
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
 
@@ -13,19 +12,19 @@ function patch_python_package_versions() {
     sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" $flet_sdk_root/packages/flet-cli/src/flet_cli/version.py
     sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" $flet_sdk_root/packages/flet-desktop/src/flet_desktop/version.py
     sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" $flet_sdk_root/packages/flet-web/src/flet_web/version.py
-    python3 $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet/pyproject.toml $PYPI_VER
-    python3 $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-cli/pyproject.toml $PYPI_VER
-    python3 $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-desktop/pyproject.toml $PYPI_VER
-    python3 $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-web/pyproject.toml $PYPI_VER
+    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet/pyproject.toml $PYPI_VER
+    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-cli/pyproject.toml $PYPI_VER
+    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-desktop/pyproject.toml $PYPI_VER
+    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-web/pyproject.toml $PYPI_VER
 }
 
 function patch_flet_desktop_package_name() {
-    python3 $root/ci/patch_toml_package_name.py $flet_sdk_root/packages/flet-desktop/pyproject.toml $1
+    uv run --with tomlkit $root/ci/patch_toml_package_name.py $flet_sdk_root/packages/flet-desktop/pyproject.toml $1
 }
 
 function publish_to_pypi() {
     if [[ ("$APPVEYOR_REPO_BRANCH" == "main" || "$APPVEYOR_REPO_TAG_NAME" != "") && "$APPVEYOR_PULL_REQUEST_NUMBER" == "" ]]; then
-        twine upload "$@"
+        uvx twine upload "$@"
     elif [[ "$APPVEYOR_PULL_REQUEST_NUMBER" == "" ]]; then
         for wheel in "$@"; do
             curl -F package=@$wheel https://$GEMFURY_TOKEN@push.fury.io/flet/
