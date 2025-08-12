bf4f5b85f4fc36048fc578cc6a934ec9e0108932
InesaFitsner <inesa@appveyor.com>
2025-08-12 13:07:10 -0700

    FloatingActionButton test

--- sdk/python/packages/flet/integration_tests/controls/test_floating_action_button.py
+++ sdk/python/packages/flet/integration_tests/controls/test_floating_action_button.py
@@ -4,9 +4,9 @@ import pytest
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_filled_button_basic(flet_app: ftt.FletTestApp, request):
+async def test_floating_action_button_basic(flet_app: ftt.FletTestApp, request):
     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
     await flet_app.assert_control_screenshot(
         request.node.name,
-        ft.FilledButton("Click me"),
+        ft.FloatingActionButton("OK"),
     )
