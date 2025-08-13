f9be866ee8d8db8a8f1e683270ebb363f2dab8df
InesaFitsner <inesa@appveyor.com>
2025-08-13 11:23:10 -0700

    CupertinoButton basic test

---
+++ sdk/python/packages/flet/integration_tests/controls/test_cupertino_button.py
@@ -0,0 +1,12 @@
+import flet as ft
+import flet.testing as ftt
+import pytest
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_cupertino_button_basic(flet_app: ftt.FletTestApp, request):
+    flet_app.page.theme_mode = ft.ThemeMode.LIGHT
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.CupertinoButton("Click me"),
+    )
