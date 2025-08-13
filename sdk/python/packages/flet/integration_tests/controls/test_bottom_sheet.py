7174e6f7f2b3d5db868e3683222a2ea31e837031
InesaFitsner <inesa@appveyor.com>
2025-08-13 14:47:38 -0700

    BottomSheet basic test

--- sdk/python/packages/flet/integration_tests/controls/test_bottom_sheet.py
+++ sdk/python/packages/flet/integration_tests/controls/test_bottom_sheet.py
@@ -6,23 +6,28 @@ import pytest
 @pytest.mark.asyncio(loop_scope="module")
 async def test_alert_dialog_basic(flet_app: ftt.FletTestApp, request):
     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
-    ad = ft.AlertDialog(
-        key="ad",
-        title=ft.Text("Hello"),
-        content=ft.Text("You are notified!"),
-        alignment=ft.Alignment.CENTER,
-        on_dismiss=lambda e: print("Dialog dismissed!"),
-        title_padding=ft.Padding.all(25),
+    bs = ft.BottomSheet(
+        content=ft.Container(
+            padding=50,
+            content=ft.Column(
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                tight=True,
+                controls=[
+                    ft.Text("Here is a bottom sheet!"),
+                    ft.ElevatedButton("Dismiss", on_click=lambda _: flet_app.page.pop_dialog()),
+                ],
+            ),
+        ),
     )
     flet_app.page.enable_screenshots = True
     flet_app.page.window.width = 400
     flet_app.page.window.height = 600
-    flet_app.page.show_dialog(ad)
+    flet_app.page.show_dialog(bs)
     flet_app.page.update()
     await flet_app.tester.pump_and_settle()
 
     flet_app.assert_screenshot(
-        "alert_dialog_basic",
+        "bottom_sheet_basic",
         await flet_app.page.take_screenshot(
             pixel_ratio=flet_app.screenshots_pixel_ratio
         ),
