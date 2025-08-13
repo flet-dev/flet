876a1c9d6a5a2cd9d2a9be0584e70f8ebf5572e7
InesaFitsner <inesa@appveyor.com>
2025-08-13 12:02:25 -0700

    CupertinoSlidingSegmentedButton basic test

--- sdk/python/packages/flet/integration_tests/controls/test_cupertino_sliding_segmented_button.py
+++ sdk/python/packages/flet/integration_tests/controls/test_cupertino_sliding_segmented_button.py
@@ -4,11 +4,12 @@ import pytest
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_cupertino_segmented_button_basic(flet_app: ftt.FletTestApp, request):
+async def test_cupertino_sliding_segmented_button_basic(flet_app: ftt.FletTestApp, 
+                                                        request):
     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
     await flet_app.assert_control_screenshot(
         request.node.name,
-        ft.CupertinoSegmentedButton(
+        ft.CupertinoSlidingSegmentedButton(
             controls=[
                 ft.Text("One"),
                 ft.Text("Two"),
