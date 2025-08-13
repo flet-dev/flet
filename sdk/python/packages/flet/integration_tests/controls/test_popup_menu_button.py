51929284f5cd55d62a4dddfa3f6616bb4b7169e2
InesaFitsner <inesa@appveyor.com>
2025-08-13 11:35:14 -0700

    CupertinoContextMenuAction basic test

--- sdk/python/packages/flet/integration_tests/controls/test_popup_menu_button.py
+++ sdk/python/packages/flet/integration_tests/controls/test_popup_menu_button.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_drop(flet_app: ftt.FletTestApp, request):
+async def test_popup_menu_basic(flet_app: ftt.FletTestApp, request):
     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
     
     pb = ft.PopupMenuButton(
