cf24214094e9c3469a2e25e583651d6d45c4b3a8
Feodor Fitsner <feodor@appveyor.com>
2025-08-14 15:59:11 -0700

    Added CupertinoAlertDialog for body update

--- sdk/python/packages/flet/integration_tests/controls/test_cupertino_alert_dialog.py
+++ sdk/python/packages/flet/integration_tests/controls/test_cupertino_alert_dialog.py
@@ -1,15 +1,18 @@
+import asyncio
+
+import pytest
+
 import flet as ft
 import flet.testing as ftt
-import pytest
 
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_cupertino_alert_dialog_basic(flet_app: ftt.FletTestApp, request):
     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
     cad = ft.CupertinoAlertDialog(
-                    title=ft.Text("Cupertino Alert Dialog"),
-                    content=ft.Text("Do you want to delete this file?"),
-                )
+        title=ft.Text("Cupertino Alert Dialog"),
+        content=ft.Text("Do you want to delete this file?"),
+    )
     flet_app.page.enable_screenshots = True
     flet_app.page.window.width = 400
     flet_app.page.window.height = 600
@@ -19,6 +22,54 @@ async def test_cupertino_alert_dialog_basic(flet_app: ftt.FletTestApp, request):
 
     flet_app.assert_screenshot(
         "cupertino_alert_dialog_basic",
+        await flet_app.page.take_screenshot(
+            pixel_ratio=flet_app.screenshots_pixel_ratio
+        ),
+    )
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_update_body(flet_app: ftt.FletTestApp, request):
+    flet_app.page.theme_mode = ft.ThemeMode.LIGHT
+    ad = ft.CupertinoAlertDialog(
+        key="ad",
+        title=ft.Text("Test"),
+        actions=[
+            ok := ft.TextButton(
+                "OK",
+                visible=True,
+            ),
+            cancel := ft.TextButton(
+                "Cancel",
+                visible=True,
+            ),
+        ],
+    )
+    flet_app.page.enable_screenshots = True
+    flet_app.page.window.width = 400
+    flet_app.page.window.height = 600
+    flet_app.page.show_dialog(ad)
+    flet_app.page.update()
+    await flet_app.tester.pump_and_settle()
+    assert (await flet_app.tester.find_by_text("OK")).count == 1
+
+    flet_app.assert_screenshot(
+        "update_body_1",
+        await flet_app.page.take_screenshot(
+            pixel_ratio=flet_app.screenshots_pixel_ratio
+        ),
+    )
+
+    # change dialog color and hide "OK" button
+    await asyncio.sleep(1)
+    ok.visible = not ok.visible  # hide button
+    cancel.disabled = not cancel.disabled  # disable button
+    flet_app.page.update()
+    await flet_app.tester.pump_and_settle()
+    assert (await flet_app.tester.find_by_text("OK")).count == 0
+
+    flet_app.assert_screenshot(
+        "update_body_2",
         await flet_app.page.take_screenshot(
             pixel_ratio=flet_app.screenshots_pixel_ratio
         ),
