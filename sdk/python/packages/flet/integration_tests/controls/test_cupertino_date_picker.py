d65c1fdc4e4ec177f78236096b37c490d9319b46
InesaFitsner <inesa@appveyor.com>
2025-08-14 12:20:11 -0700

    CupertinoPicker, CupertinoDatePicker basic tests

---
+++ sdk/python/packages/flet/integration_tests/controls/test_cupertino_date_picker.py
@@ -0,0 +1,27 @@
+import pytest
+
+import flet as ft
+import flet.testing as ftt
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_cupertino_date_picker_basic(flet_app: ftt.FletTestApp, request):
+    flet_app.page.theme_mode = ft.ThemeMode.LIGHT
+    date_picker = ft.CupertinoDatePicker(
+        date_picker_mode=ft.CupertinoDatePickerMode.DATE_AND_TIME,
+    )
+
+    cupertino_bottom_sheet = ft.CupertinoBottomSheet(date_picker)
+    flet_app.page.enable_screenshots = True
+    flet_app.page.window.width = 400
+    flet_app.page.window.height = 600
+    flet_app.page.show_dialog(cupertino_bottom_sheet)
+    flet_app.page.update()
+    await flet_app.tester.pump_and_settle()
+
+    flet_app.assert_screenshot(
+        "cupertino_date_picker_basic",
+        await flet_app.page.take_screenshot(
+            pixel_ratio=flet_app.screenshots_pixel_ratio
+        ),
+    )
