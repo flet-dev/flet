57f1841c467d4b6db3abc19b77368b89b0a9ce4c
InesaFitsner <inesa@appveyor.com>
2025-08-14 14:50:46 -0700

    NavigationBar test

---
+++ sdk/python/packages/flet/integration_tests/controls/test_navigation_bar.py
@@ -0,0 +1,23 @@
+import pytest
+
+import flet as ft
+import flet.testing as ftt
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_navigation_bar(flet_app: ftt.FletTestApp, request):
+    flet_app.page.theme_mode = ft.ThemeMode.LIGHT
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.NavigationBar(
+            destinations=[
+                ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explore"),
+                ft.NavigationBarDestination(icon=ft.Icons.COMMUTE, label="Commute"),
+                ft.NavigationBarDestination(
+                    icon=ft.Icons.BOOKMARK_BORDER,
+                    selected_icon=ft.Icons.BOOKMARK,
+                    label="Favorites",
+                ),
+            ]
+        ),
+    )
