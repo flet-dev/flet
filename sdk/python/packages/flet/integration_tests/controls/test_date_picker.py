e516cc4cd0f6229faba8e31c54957a0fc5da8147
InesaFitsner <inesa@appveyor.com>
2025-08-14 12:48:27 -0700

    CupertinoDatePicker and DatePicker tests

--- sdk/python/packages/flet/integration_tests/controls/test_date_picker.py
+++ sdk/python/packages/flet/integration_tests/controls/test_date_picker.py
@@ -7,23 +7,22 @@ import flet.testing as ftt
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_cupertino_date_picker_basic(flet_app: ftt.FletTestApp, request):
+async def test_date_picker_basic(flet_app: ftt.FletTestApp, request):
     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
-    date_picker = ft.CupertinoDatePicker(
-        value=datetime.datetime(year=2024, month=8, day=15),
-        date_picker_mode=ft.CupertinoDatePickerMode.DATE_AND_TIME,
-    )
-
-    cupertino_bottom_sheet = ft.CupertinoBottomSheet(date_picker)
+    dp = ft.DatePicker(
+                    current_date=datetime.datetime(year=2025, month=8, day=15),
+                    first_date=datetime.datetime(year=2000, month=10, day=1),
+                    last_date=datetime.datetime(year=2025, month=10, day=1),
+                )
     flet_app.page.enable_screenshots = True
     flet_app.page.window.width = 400
     flet_app.page.window.height = 600
-    flet_app.page.show_dialog(cupertino_bottom_sheet)
+    flet_app.page.show_dialog(dp)
     flet_app.page.update()
     await flet_app.tester.pump_and_settle()
 
     flet_app.assert_screenshot(
-        "cupertino_date_picker_basic",
+        "date_picker_basic",
         await flet_app.page.take_screenshot(
             pixel_ratio=flet_app.screenshots_pixel_ratio
         ),
