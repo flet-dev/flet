e48a4a8d7657068483a293551cb39b434e91758c
Owen McDonnell <omcdonnell@gmail.com>
2025-08-20 15:12:03 -0700

    add CupertinoSwitch test and fix slider image names

--- sdk/python/packages/flet/integration_tests/controls/test_cupertino_switch.py
+++ sdk/python/packages/flet/integration_tests/controls/test_cupertino_switch.py
@@ -5,35 +5,32 @@ import flet.testing as ftt
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_cupertino_slider(flet_app: ftt.FletTestApp, request):
-    sl = ft.CupertinoSlider(
-        divisions=20,
-        min=0,
-        max=100,
-        active_color=ft.Colors.PURPLE,
-        thumb_color=ft.Colors.PURPLE,
+async def test_cupertino_switch(flet_app: ftt.FletTestApp, request):
+    sw = ft.CupertinoSwitch(
+        label="Cupertino Switch",
+        value=True,
     )
 
     flet_app.page.enable_screenshots = True
     flet_app.page.window.width = 400
     flet_app.page.window.height = 600
-    flet_app.page.controls = [sl]
+    flet_app.page.controls = [sw]
     flet_app.page.update()
     await flet_app.tester.pump_and_settle()
 
-    await flet_app.tester.pump_and_settle()
     flet_app.assert_screenshot(
-        "slider_start",
+        "switch_on",
         await flet_app.page.take_screenshot(
             pixel_ratio=flet_app.screenshots_pixel_ratio
         ),
     )
 
-    sl.value = 50.0
+    sw.value = False
+    await flet_app.tester.pump_and_settle()
     flet_app.page.update()
     await flet_app.tester.pump_and_settle()
     flet_app.assert_screenshot(
-        "slider_middle",
+        "switch_off",
         await flet_app.page.take_screenshot(
             pixel_ratio=flet_app.screenshots_pixel_ratio
         ),
