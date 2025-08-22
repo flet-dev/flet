f7a4ed1f1da854684a22b64988ea56c6a631e0f5
ndonkoHenri <robotcoder4@protonmail.com>
2025-08-22 22:53:51 +0200

    updates

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -1,33 +1,32 @@
-export root=$APPVEYOR_BUILD_FOLDER
+export root=$GITHUB_WORKSPACE
 export flet_sdk_root=$root/sdk/python
 echo "flet_sdk_root: $flet_sdk_root"
 
-curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH="$HOME/.local/bin:$PATH"
-uv python install $PYTHON_VERSION
-uv python pin $PYTHON_VERSION
-uv run python --version
+# export PATH=$HOME/.local/bin:$PATH
 
 function patch_python_package_versions() {
-    PYPI_VER="${APPVEYOR_BUILD_VERSION/+/.dev}"
     sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" $flet_sdk_root/packages/flet/src/flet/version.py
     sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" $flet_sdk_root/packages/flet-cli/src/flet_cli/version.py
     sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" $flet_sdk_root/packages/flet-desktop/src/flet_desktop/version.py
     sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" $flet_sdk_root/packages/flet-web/src/flet_web/version.py
-    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet/pyproject.toml $PYPI_VER
-    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-cli/pyproject.toml $PYPI_VER
-    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-desktop/pyproject.toml $PYPI_VER
-    uv run --with tomlkit $root/ci/patch_toml_versions.py $flet_sdk_root/packages/flet-web/pyproject.toml $PYPI_VER
+
+    uv run --directory $flet_sdk_root --no-dev
+    uv version --package flet $PYPI_VER
+    uv version --package flet-cli $PYPI_VER
+    uv version --package flet-desktop $PYPI_VER
+    uv version --package flet-web $PYPI_VER
 }
 
 function patch_flet_desktop_package_name() {
-    uv run --with tomlkit $root/ci/patch_toml_package_name.py $flet_sdk_root/packages/flet-desktop/pyproject.toml $1
+    uv run $root/ci/patch_toml_package_name.py $flet_sdk_root/packages/flet-desktop/pyproject.toml $1
 }
 
 function publish_to_pypi() {
-    if [[ ("$APPVEYOR_REPO_BRANCH" == "main" || "$APPVEYOR_REPO_TAG_NAME" != "") && "$APPVEYOR_PULL_REQUEST_NUMBER" == "" ]]; then
+    # If (branch == main OR tag) AND not a PR -> upload to PyPI
+    # Else if not a PR -> upload to Gemfury
+    if { [[ ${GITHUB_EVENT_NAME:-} != "pull_request" ]] && { [[ ${GITHUB_REF_NAME:-} == "main" ]] || [[ ${GITHUB_REF_TYPE:-} == "tag" ]]; }; }; then
         uvx twine upload "$@"
-    elif [[ "$APPVEYOR_PULL_REQUEST_NUMBER" == "" ]]; then
+    elif [[ ${GITHUB_EVENT_NAME:-} != "pull_request" ]]; then
         for wheel in "$@"; do
             curl -F package=@$wheel https://$GEMFURY_TOKEN@push.fury.io/flet/
         done
