789522cd9ddd6dece99d03a8223e13a821d1bed6
Feodor Fitsner <feodor@appveyor.com>
2025-08-23 15:59:58 -0700

    Add reactive dropdown example using Flet
    Introduces a new example demonstrating a reactive dropdown control in Flet. The example uses a dataclass to manage state and updates the selected color dynamically.

---
+++ sdk/python/examples/controls/dropdown/reactive.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from typing import cast
+
+import flet as ft
+
+
+@dataclass
+class Form:
+    color: str = "red"
+
+    def change_color(self, new_color: str):
+        print("New color:", new_color)
+        self.color = new_color
+
+
+def main(page: ft.Page):
+    form = Form()
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    page.add(
+        ft.SelectionArea(
+            ft.StateView(
+                form,
+                lambda state: ft.Column(
+                    cast(
+                        list[ft.Control],
+                        [
+                            ft.Text(f"Selected color: {form.color}"),
+                            ft.Column(
+                                [
+                                    ft.Dropdown(
+                                        editable=True,
+                                        label="Color",
+                                        value=form.color,
+                                        on_change=lambda e: form.change_color(
+                                            cast(str, e.control.value)
+                                        ),
+                                        options=[
+                                            ft.DropdownOption(key="red", text="Red"),
+                                            ft.DropdownOption(
+                                                key="green", text="Green"
+                                            ),
+                                            ft.DropdownOption(key="blue", text="Blue"),
+                                        ],
+                                    ),
+                                ]
+                            ),
+                        ],
+                    )
+                ),
+            )
+        )
+    )
+
+
+ft.run(main)
