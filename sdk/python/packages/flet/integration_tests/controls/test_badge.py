f9021bc3231e91ce21494d218ccc7a63de9a0a45
InesaFitsner <inesa@appveyor.com>
2025-09-16 11:25:50 -0700

    Add badge control integration tests for macOS
    Introduces integration tests for the Badge control, including basic and property-specific scenarios. Adds corresponding golden images for macOS to validate visual correctness.

---
+++ sdk/python/packages/flet/integration_tests/controls/test_badge.py
@@ -0,0 +1,70 @@
+import pytest
+
+import flet as ft
+import flet.testing as ftt
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_basic(flet_app: ftt.FletTestApp, request):
+    flet_app.page.theme_mode = ft.ThemeMode.LIGHT
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.IconButton(icon=ft.Icons.PHONE, badge=ft.Badge()),
+    )
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_properties1(flet_app: ftt.FletTestApp, request):
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.IconButton(
+            icon=ft.Icons.PHONE,
+            badge=ft.Badge(
+                label="1",
+                offset=ft.Offset(0, 0),
+                alignment=ft.Alignment(-1, -1),
+                bgcolor=ft.Colors.GREEN,
+            ),
+        ),
+    )
+
+
+# @pytest.mark.asyncio(loop_scope="module")
+# async def test_button_style(flet_app: ftt.FletTestApp, request):
+#     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
+#     await flet_app.assert_control_screenshot(
+#         request.node.name,
+#         ft.Button(
+#             content="Test Button",
+#             style=ft.ButtonStyle(
+#                 bgcolor=ft.Colors.BLUE,
+#                 shape=ft.RoundedRectangleBorder(radius=10),
+#                 side=ft.BorderSide(width=3, color=ft.Colors.YELLOW),
+#                 padding=ft.Padding.all(20),
+#                 text_style=ft.TextStyle(
+#                     size=15,
+#                     weight=ft.FontWeight.BOLD,
+#                     color=ft.Colors.WHITE,
+#                 ),
+#             ),
+#         ),
+#     )
+
+
+# @pytest.mark.asyncio(loop_scope="module")
+# async def test_button_style_conflicts(flet_app: ftt.FletTestApp, request):
+#     flet_app.page.theme_mode = ft.ThemeMode.LIGHT
+#     await flet_app.assert_control_screenshot(
+#         request.node.name,
+#         ft.Button(
+#             content="Test Button",
+#             elevation=10,
+#             color=ft.Colors.BLACK,
+#             bgcolor=ft.Colors.BLUE,
+#             style=ft.ButtonStyle(
+#                 elevation=2,
+#                 color=ft.Colors.WHITE,
+#                 bgcolor=ft.Colors.RED,
+#             ),
+#         ),
+#     )
