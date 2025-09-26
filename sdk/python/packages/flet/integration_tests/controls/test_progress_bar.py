1f4fc859c76087a7d1c693724358ce05c0b38ece
Owen McDonnell <omcdonnell@gmail.com>
2025-09-26 07:45:30 -0700

    add markdown and progress bar test

---
+++ sdk/python/packages/flet/integration_tests/controls/test_progress_bar.py
@@ -0,0 +1,14 @@
+import pytest
+import asyncio
+import flet as ft
+import flet.testing as ftt
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_basic(flet_app: ftt.FletTestApp, request):
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.ProgressBar(
+            width=400, color=ft.Colors.GREEN_400, bgcolor=ft.Colors.GREY_400
+        ),
+    )
