5ce134c7c65ecb4aea9331cbacb83ddff1b5fa11
ndonkoHenri <robotcoder4@protonmail.com>
2025-09-27 04:00:33 +0200

    add helper functions to common.sh

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -14,3 +14,23 @@ function patch_python_package_versions() {
     uv version --package flet-desktop $PYPI_VER
     uv version --package flet-web $PYPI_VER
 }
+
+update_flet_wheel_deps() {
+  uv run "$SCRIPTS/update_flet_wheel_deps.py" "$@"
+}
+
+repackage_wheel_with_tag() {
+  uv run "$SCRIPTS/repackage_wheel_with_tag.py" "$@"
+}
+
+patch_toml_versions() {
+  uv run "$SCRIPTS/patch_toml_versions.py" "$@"
+}
+
+patch_toml_package_name() {
+  uv run "$SCRIPTS/patch_toml_package_name.py" "$@"
+}
+
+patch_pubspec_version() {
+  uv run "$SCRIPTS/patch_pubspec_version.py" "$@"
+}
