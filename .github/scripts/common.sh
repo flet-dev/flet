66e882e02a3cd9e6ec4fce14d66af69b4466a25b
ndonkoHenri <robotcoder4@protonmail.com>
2025-09-27 04:30:43 +0200

    updates

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -1,18 +1,13 @@
 function patch_python_package_versions() {
     cd $SDK_PYTHON || exit 1
 
-    # Update package versions in version.py files
-    for pkg in flet flet-cli flet-desktop flet-web; do
-      sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" packages/$pkg/src/${pkg//-/_}/version.py
-    done
-
     uv sync --no-default-groups || true
 
-    # Bump pyproject versions
-    uv version --package flet $PYPI_VER
-    uv version --package flet-cli $PYPI_VER
-    uv version --package flet-desktop $PYPI_VER
-    uv version --package flet-web $PYPI_VER
+    # Update package versions in version.py and pyproject.toml files
+    for pkg in flet flet-cli flet-desktop flet-web; do
+      sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" packages/$pkg/src/${pkg//-/_}/version.py
+      uv version --package "$pkg" "$PYPI_VER"
+    done
 }
 
 update_flet_wheel_deps() {
