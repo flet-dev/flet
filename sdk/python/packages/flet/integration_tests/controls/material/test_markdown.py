e85d787a851bff4a2c8caa0b8525c0f21621e641
Owen McDonnell <omcdonnell@gmail.com>
2025-10-02 15:38:12 -0700

    simplify markdown tests

--- sdk/python/packages/flet/integration_tests/controls/material/test_markdown.py
+++ sdk/python/packages/flet/integration_tests/controls/material/test_markdown.py
@@ -78,42 +78,25 @@ void main() {
 ```
 """
 
+md = ft.Markdown(
+    value=sample1,
+    selectable=True,
+    extension_set=ft.MarkdownExtensionSet.GITHUB_WEB,
+)
+
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_basic(flet_app: ftt.FletTestApp, request):
-
-    md1 = ft.Markdown(
-        value=sample1,
-        selectable=True,
-        extension_set=ft.MarkdownExtensionSet.GITHUB_WEB,
-    )
-    md2 = ft.Markdown(
-        value=sample2,
-        selectable=True,
-        extension_set=ft.MarkdownExtensionSet.GITHUB_WEB,
+async def test_md_1(flet_app: ftt.FletTestApp, request):
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        md,
     )
 
-    flet_app.page.enable_screenshots = True
-    flet_app.page.window.width = 400
-    flet_app.page.window.height = 800
-    flet_app.page.controls = [md1]
-    flet_app.page.update()
-    await flet_app.tester.pump_and_settle()
 
-    flet_app.assert_screenshot(
-        "basic1",
-        await flet_app.page.take_screenshot(
-            pixel_ratio=flet_app.screenshots_pixel_ratio
-        ),
-    )
-
-    flet_app.page.controls = [md2]
-    flet_app.page.update()
-    await flet_app.tester.pump_and_settle()
-
-    flet_app.assert_screenshot(
-        "basic2",
-        await flet_app.page.take_screenshot(
-            pixel_ratio=flet_app.screenshots_pixel_ratio
-        ),
+@pytest.mark.asyncio(loop_scope="module")
+async def test_md_2(flet_app: ftt.FletTestApp, request):
+    md.value = sample2
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        md,
     )
