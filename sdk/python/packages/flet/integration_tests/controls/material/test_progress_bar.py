5c2b5d25dca816d26a177ac731084f4c767cb920
Owen McDonnell <omcdonnell@gmail.com>
2025-10-02 10:14:38 -0700

    add missing reference images, simplify cupertino radio, switch, checkbox controls, rename textfield

--- sdk/python/packages/flet/integration_tests/controls/material/test_progress_bar.py
+++ sdk/python/packages/flet/integration_tests/controls/material/test_progress_bar.py
@@ -4,11 +4,21 @@ import flet as ft
 import flet.testing as ftt
 
 
+# @pytest.mark.asyncio(loop_scope="module")
+# async def test_indeterminate(flet_app: ftt.FletTestApp, request):
+#     await flet_app.assert_control_screenshot(
+#         request.node.name,
+#         ft.ProgressBar(
+#             width=400, color=ft.Colors.GREEN_400, bgcolor=ft.Colors.GREY_400, value=None
+#         ),
+#     )
+
+
 @pytest.mark.asyncio(loop_scope="module")
-async def test_basic(flet_app: ftt.FletTestApp, request):
+async def test_determinate(flet_app: ftt.FletTestApp, request):
     await flet_app.assert_control_screenshot(
         request.node.name,
         ft.ProgressBar(
-            width=400, color=ft.Colors.GREEN_400, bgcolor=ft.Colors.GREY_400, value=None
+            width=400, color=ft.Colors.GREEN_400, bgcolor=ft.Colors.GREY_400, value=0.5
         ),
     )
--- sdk/python/packages/flet/integration_tests/controls/material/test_radio.py
+++ sdk/python/packages/flet/integration_tests/controls/material/test_radio.py
@@ -3,52 +3,24 @@ import pytest
 import flet as ft
 import flet.testing as ftt
 
+rg = ft.RadioGroup(
+    content=ft.Column(
+        controls=[
+            ft.Radio(value="one", label="One"),
+            ft.Radio(value="two", label="Two"),
+            ft.Radio(value="three", label="Three"),
+        ],
+    ),
+    value="one",
+)
+
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_radio(flet_app: ftt.FletTestApp, request):
-    group = ft.RadioGroup(
-        content=ft.Column(
-            controls=[
-                ft.Radio(value="one", label="One"),
-                ft.Radio(value="two", label="Two"),
-                ft.Radio(value="three", label="Three"),
-            ]
-        )
-    )
+async def test_basic_one(flet_app: ftt.FletTestApp, request):
+    await flet_app.assert_control_screenshot(request.node.name, rg)
 
-    flet_app.page.enable_screenshots = True
-    flet_app.page.window.width = 400
-    flet_app.page.window.height = 600
-    flet_app.page.controls = [group]
-    flet_app.page.update()
-    await flet_app.tester.pump_and_settle()
 
-    # one chosen
-    await flet_app.tester.tap(await flet_app.tester.find_by_text("One"))
-    await flet_app.tester.pump_and_settle()
-    flet_app.assert_screenshot(
-        "one",
-        await flet_app.page.take_screenshot(
-            pixel_ratio=flet_app.screenshots_pixel_ratio
-        ),
-    )
-
-    # two chosen
-    await flet_app.tester.tap(await flet_app.tester.find_by_text("Two"))
-    await flet_app.tester.pump_and_settle()
-    flet_app.assert_screenshot(
-        "two",
-        await flet_app.page.take_screenshot(
-            pixel_ratio=flet_app.screenshots_pixel_ratio
-        ),
-    )
-
-    # three chosen
-    await flet_app.tester.tap(await flet_app.tester.find_by_text("Three"))
-    await flet_app.tester.pump_and_settle()
-    flet_app.assert_screenshot(
-        "three",
-        await flet_app.page.take_screenshot(
-            pixel_ratio=flet_app.screenshots_pixel_ratio
-        ),
-    )
+@pytest.mark.asyncio(loop_scope="module")
+async def test_basic_three(flet_app: ftt.FletTestApp, request):
+    rg.value = "two"
+    await flet_app.assert_control_screenshot(request.node.name, rg)
