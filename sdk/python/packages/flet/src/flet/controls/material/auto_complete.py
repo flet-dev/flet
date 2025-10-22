7a3412b3884493399628cac2a0a47532980363b1
ndonkoHenri <robotcoder4@protonmail.com>
2025-10-22 15:32:45 +0200

    initial commit

--- sdk/python/packages/flet/src/flet/controls/material/auto_complete.py
+++ sdk/python/packages/flet/src/flet/controls/material/auto_complete.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
+from flet import LayoutControl
 from flet.controls.base_control import control
-from flet.controls.control import Control
-from flet.controls.control_event import Event, EventHandler
+from flet.controls.control_event import ControlEventHandler, Event, EventHandler
 from flet.controls.types import Number
 
 __all__ = ["AutoComplete", "AutoCompleteSelectEvent", "AutoCompleteSuggestion"]
@@ -11,20 +11,43 @@ __all__ = ["AutoComplete", "AutoCompleteSelectEvent", "AutoCompleteSuggestion"]
 
 @dataclass
 class AutoCompleteSuggestion:
+    """
+    Represents a suggestion item for the [`AutoComplete`][flet.] control.
+    """
+
     key: str
+    """A unique identifier or value used for filtering and selection."""
+
     value: str
+    """The display text shown to the user."""
 
 
 @dataclass
 class AutoCompleteSelectEvent(Event["AutoComplete"]):
+    """Event representing the selection of a suggestion in the AutoComplete control."""
+
+    index: int
+    """
+    The index of the selected suggestion from the corresponding
+    [`AutoCompletesuggestions`][flet.] list.
+    """
+
     selection: AutoCompleteSuggestion
+    """The selected suggestion."""
 
 
 @control("AutoComplete")
-class AutoComplete(Control):
+class AutoComplete(LayoutControl):
     """
     Helps the user make a selection by entering some text and choosing from among a
     list of displayed options.
+    """
+
+    value: str = ""
+    """
+    Current text displayed in the input field.
+
+    This value reflects user input even if it does not match any provided suggestion.
     """
 
     suggestions: list[AutoCompleteSuggestion] = field(default_factory=list)
@@ -53,7 +76,19 @@ class AutoComplete(Control):
     Called when a suggestion is selected.
     """
 
+    on_change: Optional[ControlEventHandler["AutoComplete"]] = None
+    """
+    Called when the input text changes.
+    """
+
     @property
-    def selected_index(self):
-        # TODO: check availability of _selected_index + a default if not yet available
-        return self._selected_index
+    def selected_index(self) -> Optional[int]:
+        """
+        The index of the (last) selected suggestion.
+
+        It is `None` until a suggestion has been selected from the UI.
+
+        Note:
+            This property is read-only.
+        """
+        return getattr(self, "_selected_index", None)
