94380a84d5b3879068b28f0f0f401d12273bceff
ndonkoHenri <robotcoder4@protonmail.com>
2025-11-07 01:48:49 +0100

    refactor: enhance documentation for reorderable components

--- sdk/python/packages/flet/src/flet/controls/core/reorderable_draggable.py
+++ sdk/python/packages/flet/src/flet/controls/core/reorderable_draggable.py
@@ -11,6 +11,24 @@ class ReorderableDraggable(LayoutControl, AdaptiveControl):
 
     It creates a listener for a drag immediately following a pointer down
     event over the given [`content`][(c).] control.
+
+    Example:
+        ```python
+        ft.ReorderableListView(
+            expand=True,
+            show_default_drag_handles=False,
+            controls=[
+                ft.ReorderableDraggable(
+                    index=i,
+                    content=ft.ListTile(
+                        title=f"Draggable Item {i}",
+                        bgcolor=ft.Colors.GREY if i % 2 == 0 else ft.Colors.BLUE_ACCENT,
+                    ),
+                )
+                for i in range(10)
+            ],
+        )
+        ```
     """
 
     index: int
