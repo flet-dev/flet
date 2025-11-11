46555d3ca87cc960988299046eec4eadcf3cff01
ndonkoHenri <robotcoder4@protonmail.com>
2025-11-11 12:42:50 +0100

    rename files to use _ instead of -

--- sdk/python/examples/publish-gallery.sh
+++ sdk/python/examples/publish-gallery.sh
@@ -7,7 +7,7 @@ flet publish python/apps/icons-cupertino-browser/main.py --distpath $DIST_PATH/i
 flet publish python/tutorials/calc/calc.py --distpath $DIST_PATH/calculator --base-url calculator --app-name "Calculator" --app-description "A simple calculator app written in Flet."
 flet publish python/tutorials/solitaire/solitaire-final-part1/main.py --distpath $DIST_PATH/solitaire --base-url solitaire --assets assets --app-name "Solitaire" --app-description "Learn how to handle gestures and position controls on a page."
 flet publish python/apps/trolli/src/main.py --distpath $DIST_PATH/trolli --base-url trolli --assets ../assets --route-url-strategy "hash" --app-name "Trolli" --app-description "A clone of Trello."
-flet publish python/apps/routing-navigation/home-store.py --distpath $DIST_PATH/simple-routing --base-url simple-routing --route-url-strategy "hash" --app-name "Flet routing example" --app-description "An example of routing in Flet."
+flet publish python/apps/routing-navigation/home_store.py --distpath $DIST_PATH/simple-routing --base-url simple-routing --route-url-strategy "hash" --app-name "Flet routing example" --app-description "An example of routing in Flet."
 flet publish python/apps/counter/counter.py --distpath $DIST_PATH/counter --base-url counter --app-name "Counter" --app-description "Counter to get an idea of Flet."
 flet publish python/apps/flet-animation/main.py --distpath $DIST_PATH/flet-animation --base-url flet-animation --app-name "Flet animation" --app-description "An example of implicit animations in Flet."
 flet publish python/apps/greeter/greeter.py --distpath $DIST_PATH/greeter --base-url greeter --app-name "Greeter" --app-description "A basic example of interactive forms in Flet."
