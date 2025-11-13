425bc8980f538d4c8162223ba4fd21bc95f64649
TheEthicalBoy <98978078+ndonkoHenri@users.noreply.github.com>
2025-11-13 18:45:50 +0100

    fix: `TextOverflow` not working in `TextStyle` (#5809)
    * initial commit
    
    * integration tests

--- sdk/python/packages/flet/integration_tests/controls/core/test_text.py
+++ sdk/python/packages/flet/integration_tests/controls/core/test_text.py
@@ -5,7 +5,7 @@ import flet.testing as ftt
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_basic_text(flet_app: ftt.FletTestApp, request):
+async def test_basic(flet_app: ftt.FletTestApp, request):
     await flet_app.assert_control_screenshot(
         request.node.name,
         ft.Text("Hello, world!"),
@@ -13,8 +13,30 @@ async def test_basic_text(flet_app: ftt.FletTestApp, request):
 
 
 @pytest.mark.asyncio(loop_scope="module")
-async def test_bold_text(flet_app: ftt.FletTestApp, request):
+async def test_bold(flet_app: ftt.FletTestApp, request):
     await flet_app.assert_control_screenshot(
         request.node.name,
         ft.Text("Hello, world!", weight=ft.FontWeight.BOLD),
+    )
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_overflow(flet_app: ftt.FletTestApp, request):
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.Column(
+            width=100,
+            controls=[
+                ft.Text(
+                    value="Hello World! This is some very long text.",
+                    overflow=ft.TextOverflow.ELLIPSIS,
+                ),
+                ft.Text(
+                    value="Hello World! This is some very long text.",
+                    style=ft.TextStyle(
+                        overflow=ft.TextOverflow.ELLIPSIS,
+                    ),
+                ),
+            ],
+        ),
     )
