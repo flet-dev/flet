461a5ed8942123325ec25c35b29448b45798ae27
ndonkoHenri <robotcoder4@protonmail.com>
2025-11-24 00:34:31 +0100

    fix #5562: `Pagelet` integration tests

--- sdk/python/packages/flet/integration_tests/controls/core/test_pagelet.py
+++ sdk/python/packages/flet/integration_tests/controls/core/test_pagelet.py
@@ -1,28 +1,38 @@
-# @pytest.mark.asyncio(loop_scope="module")
-# async def test_pagelet_basic(flet_app: ftt.FletTestApp, request):
-#     await flet_app.assert_control_screenshot(
-#         request.node.name,
-#         ft.Pagelet(
-#             ft.Container(bgcolor=ft.Colors.AMBER, content=ft.Text("Pagelet Content"))
-#         ),
-#     )
+import pytest
+
+import flet as ft
+import flet.testing as ftt
 
 
-# @pytest.mark.asyncio(loop_scope="module")
-# async def test_pagelet_basic(flet_app: ftt.FletTestApp, request):
+@pytest.mark.asyncio(loop_scope="module")
+async def test_basic(flet_app: ftt.FletTestApp, request):
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.Pagelet(
+            appbar=ft.AppBar(title="Pagelet AppBar"),
+            content=ft.Text("Pagelet Content"),
+            width=350,
+            height=300,
+        ),
+    )
 
-#     flet_app.page.enable_screenshots = True
-#     flet_app.resize_page(400, 300)
-#     flet_app.page.add(
-#         ft.Pagelet(
-#             ft.Container(bgcolor=ft.Colors.AMBER, content=ft.Text("Pagelet Content"))
-#         ),
-#     )
-#     await flet_app.tester.pump_and_settle()
 
-#     flet_app.assert_screenshot(
-#         "pagelet_basic",
-#         await flet_app.page.take_screenshot(
-#             pixel_ratio=flet_app.screenshots_pixel_ratio
-#         ),
-#     )
+@pytest.mark.asyncio(loop_scope="module")
+async def test_cupertino_adaptive(flet_app: ftt.FletTestApp, request):
+    flet_app.page.platform = ft.PagePlatform.MACOS
+    await flet_app.assert_control_screenshot(
+        request.node.name,
+        ft.Pagelet(
+            adaptive=True,
+            width=350,
+            height=300,
+            appbar=ft.AppBar(title="Pagelet AppBar"),
+            content=ft.Text("Pagelet Content"),
+            navigation_bar=ft.NavigationBar(
+                destinations=[
+                    ft.NavigationBarDestination(icon=ft.Icons.ADD, label="New"),
+                    ft.NavigationBarDestination(icon=ft.Icons.INBOX, label="Inbox"),
+                ],
+            ),
+        ),
+    )
