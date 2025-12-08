9fecf6faa820c7d2be9af651c05635a6162b9c77
Owen McDonnell <omcdonnell@gmail.com>
2025-12-08 08:15:44 -0800

    unccoment menu bar test

--- sdk/python/packages/flet/integration_tests/examples/material/test_menu_bar.py
+++ sdk/python/packages/flet/integration_tests/examples/material/test_menu_bar.py
@@ -37,46 +37,46 @@ async def test_image_for_docs(flet_app_function: ftt.FletTestApp, request):
     )
 
 
-# @pytest.mark.parametrize(
-#     "flet_app_function",
-#     [{"flet_app_main": nested_submenus.main}],
-#     indirect=True,
-# )
-# @pytest.mark.asyncio(loop_scope="function")
-# async def test_nested_submenus(flet_app_function: ftt.FletTestApp):
-#     flet_app_function.page.enable_screenshots = True
-#     flet_app_function.resize_page(400, 400)
-#     flet_app_function.page.update()
-#     await flet_app_function.tester.pump_and_settle(
-#         duration=ft.Duration(milliseconds=500)
-#     )
-#     flet_app_function.assert_screenshot(
-#         "nested_submenus1",
-#         await flet_app_function.page.take_screenshot(
-#             pixel_ratio=flet_app_function.screenshots_pixel_ratio
-#         ),
-#     )
-#     smb = await flet_app_function.tester.find_by_text("File")
-#     await flet_app_function.tester.tap(smb)
-#     await flet_app_function.tester.pump_and_settle()
-#     flet_app_function.assert_screenshot(
-#         "nested_submenus2",
-#         await flet_app_function.page.take_screenshot(
-#             pixel_ratio=flet_app_function.screenshots_pixel_ratio
-#         ),
-#     )
-#     mib = await flet_app_function.tester.find_by_text("Save")
-#     await flet_app_function.tester.tap(mib)
-#     await flet_app_function.tester.pump_and_settle()
-#     flet_app_function.assert_screenshot(
-#         "nested_submenus3",
-#         await flet_app_function.page.take_screenshot(
-#             pixel_ratio=flet_app_function.screenshots_pixel_ratio
-#         ),
-#     )
+@pytest.mark.parametrize(
+    "flet_app_function",
+    [{"flet_app_main": nested_submenus.main}],
+    indirect=True,
+)
+@pytest.mark.asyncio(loop_scope="function")
+async def test_nested_submenus(flet_app_function: ftt.FletTestApp):
+    flet_app_function.page.enable_screenshots = True
+    flet_app_function.resize_page(400, 400)
+    flet_app_function.page.update()
+    await flet_app_function.tester.pump_and_settle(
+        duration=ft.Duration(milliseconds=500)
+    )
+    flet_app_function.assert_screenshot(
+        "nested_submenus1",
+        await flet_app_function.page.take_screenshot(
+            pixel_ratio=flet_app_function.screenshots_pixel_ratio
+        ),
+    )
+    smb = await flet_app_function.tester.find_by_text("File")
+    await flet_app_function.tester.tap(smb)
+    await flet_app_function.tester.pump_and_settle()
+    flet_app_function.assert_screenshot(
+        "nested_submenus2",
+        await flet_app_function.page.take_screenshot(
+            pixel_ratio=flet_app_function.screenshots_pixel_ratio
+        ),
+    )
+    mib = await flet_app_function.tester.find_by_text("Save")
+    await flet_app_function.tester.tap(mib)
+    await flet_app_function.tester.pump_and_settle()
+    flet_app_function.assert_screenshot(
+        "nested_submenus3",
+        await flet_app_function.page.take_screenshot(
+            pixel_ratio=flet_app_function.screenshots_pixel_ratio
+        ),
+    )
 
-#     flet_app_function.create_gif(
-#         ["nested_submenus1", "nested_submenus2", "nested_submenus3"],
-#         "nested_submenus",
-#         duration=1600,
-#     )
+    flet_app_function.create_gif(
+        ["nested_submenus1", "nested_submenus2", "nested_submenus3"],
+        "nested_submenus",
+        duration=1600,
+    )
