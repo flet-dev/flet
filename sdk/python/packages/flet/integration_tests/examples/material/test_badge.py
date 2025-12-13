7190ce5fe11b22f579eced08069c968d2884b77b
ndonkoHenri <robotcoder4@protonmail.com>
2025-12-13 18:21:18 +0100

    rename badge basic example test

--- sdk/python/packages/flet/integration_tests/examples/material/test_badge.py
+++ sdk/python/packages/flet/integration_tests/examples/material/test_badge.py
@@ -32,7 +32,7 @@ async def test_basic(flet_app_function: ftt.FletTestApp):
     flet_app_function.page.update()
     await flet_app_function.tester.pump_and_settle()
     flet_app_function.assert_screenshot(
-        "badge-navigation-bar",
+        "basic",
         await flet_app_function.page.take_screenshot(
             pixel_ratio=flet_app_function.screenshots_pixel_ratio
         ),
