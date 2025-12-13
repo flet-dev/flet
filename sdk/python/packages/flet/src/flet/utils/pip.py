096d5dae8ac1fc3d6625e88a5c102c1ade37d848
ndonkoHenri <robotcoder4@protonmail.com>
2025-12-13 11:39:33 +0100

    refactor: clean up imports and improve upgrade error message in pip.py

--- sdk/python/packages/flet/src/flet/utils/pip.py
+++ sdk/python/packages/flet/src/flet/utils/pip.py
@@ -21,7 +21,9 @@ def install_flet_package(name: str):
         print("OK")
     else:
         print(
-            f'Unable to upgrade "{name}" package to version {flet.version.version}. Please use "pip install \'flet[all]=={flet.version.version}\' --upgrade" command to upgrade Flet.'
+            f'Unable to upgrade "{name}" package to version {flet.version.version}. '
+            f"Please use \"pip install 'flet[all]=={flet.version.version}' --upgrade\" "
+            f"command to upgrade Flet."
         )
         exit(1)
 
@@ -30,14 +32,12 @@ def ensure_flet_desktop_package_installed():
     try:
         import flet_desktop.version
 
-        import flet.version
-
         if (
             flet_desktop.version.version
             and flet_desktop.version.version != flet.version.version
         ):
             raise RuntimeError("flet-desktop version mismatch")
-    except:
+    except Exception:
         install_flet_package("flet-desktop")
 
 
@@ -45,20 +45,17 @@ def ensure_flet_web_package_installed():
     try:
         import flet_web.version
 
-        import flet.version
-
         if (
             flet_web.version.version
             and flet_web.version.version != flet.version.version
         ):
             raise RuntimeError("flet-web version mismatch")
-    except:
+    except Exception:
         install_flet_package("flet-web")
 
 
 def ensure_flet_cli_package_installed():
     try:
-        import flet.version
         import flet_cli.version
 
         if (
@@ -66,5 +63,5 @@ def ensure_flet_cli_package_installed():
             and flet_cli.version.version != flet.version.version
         ):
             raise RuntimeError("flet-cli version mismatch")
-    except:
+    except Exception:
         install_flet_package("flet-cli")
