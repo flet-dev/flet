bb7c8365ac92ef0cbc307cab821cfe7b128196dc
Hehs Jesuh <asabaev.islam@icloud.com>
2026-01-14 13:25:58 +0300

    update docs

---
+++ sdk/python/examples/services/secure_storage/basic.py
@@ -0,0 +1,77 @@
+import flet as ft
+import flet_secure_storage as fss
+
+
+def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    storage = fss.SecureStorage()
+
+    key = ft.TextField(label="Key", value="example_key")
+    value = ft.TextField(label="Value", value="secret_value")
+    result = ft.Text(theme_style=ft.TextThemeStyle.TITLE_LARGE)
+
+    async def set_value(e):
+        await storage.set(key.value, value.value)
+        result.value = "Value saved"
+        page.update()
+
+    async def get_value(e):
+        result.value = await storage.get(key.value)
+        page.update()
+
+    async def remove_value(e):
+        await storage.remove(key.value)
+        result.value = "Value removed"
+        page.update()
+
+    async def clear_storage(e):
+        await storage.clear()
+        result.value = "Storage cleared"
+        page.update()
+
+    async def contains_key(e):
+        exists = await storage.contains_key(key.value)
+        result.value = f"Key exists: {exists}"
+        page.update()
+
+    async def get_availability(e):
+        is_availability = await storage.get_availability()
+        page.show_dialog(
+            ft.SnackBar(
+                content=ft.Text(
+                    value=f"Protected data available: {is_availability}"
+                    if is_availability
+                    else "Protected data available: None"
+                )
+            )
+        )
+        page.update()
+
+    page.add(
+        ft.Column(
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=10,
+            controls=[
+                result,
+                key,
+                value,
+                ft.Row(
+                    width=300,
+                    wrap=True,
+                    controls=[
+                        ft.Button("Set", on_click=set_value),
+                        ft.Button("Get", on_click=get_value),
+                        ft.Button("Contains key", on_click=contains_key),
+                        ft.Button("Remove", on_click=remove_value),
+                        ft.Button("Clear", on_click=clear_storage),
+                        ft.Button("Check Data Availability", on_click=get_availability),
+                    ],
+                ),
+            ],
+        ),
+    )
+
+
+ft.run(main)
---
+++ sdk/python/packages/flet/docs/services/securestorage.md
@@ -0,0 +1,14 @@
+---
+class_name: flet.SecureStorage
+examples: ../../examples/services/secure_storage
+---
+
+{{ class_summary(class_name) }}
+
+## Examples
+
+```python
+--8<-- "{{ examples }}/basic.py"
+```
+
+{{ class_members(class_name) }}
