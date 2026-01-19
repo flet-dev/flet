ae4e41d52d550f622d7f39e92596a216f9b05022
Feodor Fitsner <feodor@appveyor.com>
2026-01-19 09:25:18 -0800

    Restrict version patching to lines starting at line start
    Updated sed expressions in patch_python_package_versions to only match version assignment lines at the beginning of the line. This prevents accidental replacements elsewhere in the file.

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -9,8 +9,8 @@ function patch_python_package_versions() (
     for pkg in flet flet-cli flet-desktop flet-desktop-light flet-web; do
       version_py="packages/$pkg/src/${pkg//-/_}/version.py"
       if [[ -f "$version_py" ]]; then
-        sed -i -e "s/flet_version = \"\"/flet_version = \"$PYPI_VER\"/g" "$version_py"
-        sed -i -e "s/version = \"\"/version = \"$PYPI_VER\"/g" "$version_py"
+        sed -i -e "s/^flet_version = \"\"/flet_version = \"$PYPI_VER\"/g" "$version_py"
+        sed -i -e "s/^version = \"\"/version = \"$PYPI_VER\"/g" "$version_py"
       else
         echo "Skipping version patch: $version_py not found"
       fi
@@ -20,7 +20,7 @@ function patch_python_package_versions() (
 
     # Get Flutter version from .fvmrc and set it in version.py
     FLUTTER_VERSION="$( uv run "$SCRIPTS/read_fvmrc.py" "${ROOT}/.fvmrc" )"
-    sed -i -e "s/flutter_version = \"\"/flutter_version = \"$FLUTTER_VERSION\"/g" packages/flet/src/flet/version.py
+    sed -i -e "s/^flutter_version = \"\"/flutter_version = \"$FLUTTER_VERSION\"/g" packages/flet/src/flet/version.py
     echo "Patched Flutter SDK version to $FLUTTER_VERSION"
 )
 
