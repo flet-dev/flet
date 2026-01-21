7e44f6334d05f4fb421695406e83288083005ac8
Hehs Jesuh <asabaev.islam@icloud.com>
2026-01-21 23:41:25 +0300

    fix: apply ButtonStyle in TextButton (#6047)

--- packages/flet/lib/src/controls/button.dart
+++ packages/flet/lib/src/controls/button.dart
@@ -98,7 +98,7 @@ class _ButtonControlState extends State<ButtonControl> with FletStoreMixin {
     var theme = Theme.of(context);
 
     var style = parseButtonStyle(
-      widget.control.internals?["style"],
+      widget.control.properties["style"],
       theme,
       defaultForegroundColor:
           widget.control.getColor("color", context, theme.colorScheme.primary)!,
