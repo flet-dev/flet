aebfd0b4bcc75fc4fa95e6cd8631d8fcd578db4f
Feodor Fitsner <feodor@appveyor.com>
2026-02-12 09:54:10 -0800

    Adjust PyPI cleanup version and package name
    Update the pypi-cleanup VER regex to target 0.80.5/0.80.6 dev builds instead of 0.81.0.dev. Add flet-color-pickers to the cleanup script and rename occurrences of flet-color-picker to flet-color-pickers in the CI workflow so package names are consistent.

--- .github/scripts/clean-pypi.sh
+++ .github/scripts/clean-pypi.sh
@@ -1,6 +1,6 @@
 # set PYPI_CLEANUP_PASSWORD with pypi.org password
 
-VER="0\.81\.0\.dev(?!7200)"
+VER="0\.80\.[56]\.dev(?!7200)"
 uv tool install pypi-cleanup
 uvx pypi-cleanup -u flet -p flet -y -r $VER --do-it
 uvx pypi-cleanup -u flet -p flet-cli -y -r $VER --do-it
@@ -13,6 +13,7 @@ uvx pypi-cleanup -u flet -p flet-ads -y -r $VER --do-it
 uvx pypi-cleanup -u flet -p flet-audio -y -r $VER --do-it
 uvx pypi-cleanup -u flet -p flet-audio-recorder -y -r $VER --do-it
 uvx pypi-cleanup -u flet -p flet-charts -y -r $VER --do-it
+uvx pypi-cleanup -u flet -p flet-color-pickers -y -r $VER --do-it
 uvx pypi-cleanup -u flet -p flet-datatable2 -y -r $VER --do-it
 uvx pypi-cleanup -u flet -p flet-flashlight -y -r $VER --do-it
 uvx pypi-cleanup -u flet -p flet-geolocator -y -r $VER --do-it
