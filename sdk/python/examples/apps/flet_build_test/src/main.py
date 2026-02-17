a00758b1cd240fec4bfff3d6130b5d29358fb2d3
ndonkoHenri <robotcoder4@protonmail.com>
2026-02-17 09:34:02 +0100

    comment out extensions

--- sdk/python/examples/apps/flet_build_test/src/main.py
+++ sdk/python/examples/apps/flet_build_test/src/main.py
@@ -5,19 +5,20 @@ import PIL
 from modules.utils import greet
 
 import flet as ft
-import flet_ads  # noqa: F401
-import flet_audio  # noqa: F401
-import flet_audio_recorder  # noqa: F401
-import flet_charts  # noqa: F401
-import flet_datatable2  # noqa: F401
-import flet_flashlight  # noqa: F401
-import flet_geolocator  # noqa: F401
-import flet_lottie  # noqa: F401
-import flet_map  # noqa: F401
-import flet_permission_handler  # noqa: F401
-import flet_rive  # noqa: F401
-import flet_secure_storage  # noqa: F401
-import flet_webview  # noqa: F401
+
+# import flet_ads  # noqa: F401
+# import flet_audio  # noqa: F401
+# import flet_audio_recorder  # noqa: F401
+# import flet_charts  # noqa: F401
+# import flet_datatable2  # noqa: F401
+# import flet_flashlight  # noqa: F401
+# import flet_geolocator  # noqa: F401
+# import flet_lottie  # noqa: F401
+# import flet_map  # noqa: F401
+# import flet_permission_handler  # noqa: F401
+# import flet_rive  # noqa: F401
+# import flet_secure_storage  # noqa: F401
+# import flet_webview  # noqa: F401
 
 
 def main(page: ft.Page):
