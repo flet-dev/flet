8e7bedb6b43a653701fec60b84ae2e6cec2e5df2
ndonkoHenri <robotcoder4@protonmail.com>
2026-03-01 02:51:13 +0100

    improve ads example

--- sdk/python/examples/controls/ads/example_1.py
+++ sdk/python/examples/controls/ads/example_1.py
@@ -1,46 +1,38 @@
 import flet as ft
 import flet_ads as fta
 
+# Test ad unit IDs
+ids = {
+    ft.PagePlatform.ANDROID: {
+        "banner": "ca-app-pub-3940256099942544/6300978111",
+        "interstitial": "ca-app-pub-3940256099942544/1033173712",
+    },
+    ft.PagePlatform.IOS: {
+        "banner": "ca-app-pub-3940256099942544/2934735716",
+        "interstitial": "ca-app-pub-3940256099942544/4411468910",
+    },
+}
+
 
 def main(page: ft.Page):
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.appbar = ft.AppBar(
         adaptive=True,
         title="Mobile Ads Playground",
         bgcolor=ft.Colors.LIGHT_BLUE_300,
     )
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.scroll = ft.ScrollMode.AUTO
 
-    # Test ad unit IDs
-    ids = {
-        ft.PagePlatform.ANDROID: {
-            "banner": "ca-app-pub-3940256099942544/6300978111",
-            "interstitial": "ca-app-pub-3940256099942544/1033173712",
-        },
-        ft.PagePlatform.IOS: {
-            "banner": "ca-app-pub-3940256099942544/2934735716",
-            "interstitial": "ca-app-pub-3940256099942544/4411468910",
-        },
-    }
+    def show_new_interstitial_ad():
+        async def show_iad(e: ft.Event[fta.InterstitialAd]):
+            await iad.show()
 
-    def handle_interstitial_ad_close(e: ft.Event[fta.InterstitialAd]):
-        nonlocal iad
-        print("Closing InterstitialAd")
-        page.services.remove(e.control)
-        page.services.append(iad := get_new_interstitial_ad())
-        page.update()
-
-    async def handle_interstitial_ad_display(e: ft.Event[ft.OutlinedButton]):
-        nonlocal iad
-        print("Showing InterstitialAd")
-        await iad.show()
-
-    def get_new_interstitial_ad():
-        return fta.InterstitialAd(
-            unit_id=ids.get(page.platform, {}).get("interstitial"),
-            on_load=lambda e: print("InterstitialAd loaded"),
+        iad = fta.InterstitialAd(
+            unit_id=ids[page.platform]["interstitial"],
+            on_load=show_iad,
             on_error=lambda e: print("InterstitialAd error", e.data),
             on_open=lambda e: print("InterstitialAd opened"),
-            on_close=handle_interstitial_ad_close,
+            on_close=lambda e: print("InterstitialAd closed"),
             on_impression=lambda e: print("InterstitialAd impression"),
             on_click=lambda e: print("InterstitialAd clicked"),
         )
@@ -51,7 +43,7 @@ def main(page: ft.Page):
             height=50,
             bgcolor=ft.Colors.TRANSPARENT,
             content=fta.BannerAd(
-                unit_id=ids.get(page.platform, {}).get("banner"),
+                unit_id=ids[page.platform]["banner"],
                 on_click=lambda e: print("BannerAd clicked"),
                 on_load=lambda e: print("BannerAd loaded"),
                 on_error=lambda e: print("BannerAd error", e.data),
@@ -62,16 +54,16 @@ def main(page: ft.Page):
             ),
         )
 
-    page.services.append(iad := get_new_interstitial_ad())
-
     page.add(
         ft.OutlinedButton(
             content="Show InterstitialAd",
-            on_click=handle_interstitial_ad_display,
+            on_click=show_new_interstitial_ad,
+            disabled=page.web or not page.platform.is_mobile(),  # mobile only
         ),
         ft.OutlinedButton(
             content="Show BannerAd",
             on_click=lambda e: page.add(get_new_banner_ad()),
+            disabled=page.web or not page.platform.is_mobile(),  # mobile only
         ),
     )
 
