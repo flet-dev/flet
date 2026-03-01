0bce39eb284abb9fe0255425b9a36022e8161bcd
ndonkoHenri <robotcoder4@protonmail.com>
2026-03-01 23:50:34 +0100

    fix parseLoadRequestMethod

--- sdk/python/packages/flet-webview/src/flutter/flet_webview/lib/src/utils/webview.dart
+++ sdk/python/packages/flet-webview/src/flutter/flet_webview/lib/src/utils/webview.dart
@@ -3,7 +3,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 LoadRequestMethod? parseLoadRequestMethod(String? value,
     [LoadRequestMethod? defaultValue]) {
-  return parseEnum(LoadRequestMethod.values, value);
+  return parseEnum(LoadRequestMethod.values, value, defaultValue);
 }
 
 JavaScriptMode? parseJavaScriptMode(String? value,
