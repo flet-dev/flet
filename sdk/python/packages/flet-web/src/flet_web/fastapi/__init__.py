d6bd29e35b60f58c645c9a017dde032d80c2798d
ndonkoHenri <robotcoder4@protonmail.com>
2026-03-02 09:31:34 +0100

    fix #6193: Add FletOAuth to module exports

--- sdk/python/packages/flet-web/src/flet_web/fastapi/__init__.py
+++ sdk/python/packages/flet-web/src/flet_web/fastapi/__init__.py
@@ -2,7 +2,16 @@ from flet_web.fastapi.app import app
 from flet_web.fastapi.flet_app import FletApp
 from flet_web.fastapi.flet_app_manager import app_manager
 from flet_web.fastapi.flet_fastapi import FastAPI
+from flet_web.fastapi.flet_oauth import FletOAuth
 from flet_web.fastapi.flet_static_files import FletStaticFiles
 from flet_web.fastapi.flet_upload import FletUpload
 
-__all__ = ["app", "FletApp", "app_manager", "FastAPI", "FletStaticFiles", "FletUpload"]
+__all__ = [
+    "FastAPI",
+    "FletApp",
+    "FletOAuth",
+    "FletStaticFiles",
+    "FletUpload",
+    "app",
+    "app_manager",
+]
