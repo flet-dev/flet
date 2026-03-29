6675f1dbb6ce8a2e1d229690e9d20d97bd637a3b
InesaFitsner <inesa@appveyor.com>
2026-03-29 10:24:34 -0700

    Migrate counter app examples

--- sdk/python/examples/publish-gallery.sh
+++ sdk/python/examples/publish-gallery.sh
@@ -6,7 +6,7 @@ flet publish tutorials/calculator/calc.py --distpath $DIST_PATH/calculator --bas
 flet publish tutorials/solitaire_declarative/solitaire-final/main.py --distpath $DIST_PATH/solitaire --base-url solitaire --assets assets --app-name "Solitaire" --app-description "Learn how to handle gestures and position controls on a page."
 flet publish apps/trolli-declarative/src/main.py --distpath $DIST_PATH/trolli --base-url trolli --assets assets --route-url-strategy "hash" --app-name "Trolli" --app-description "A clone of Trello."
 flet publish apps/routing_navigation/home_store.py --distpath $DIST_PATH/simple_routing --base-url simple_routing --route-url-strategy "hash" --app-name "Flet routing example" --app-description "An example of routing in Flet."
-flet publish apps/counter/counter.py --distpath $DIST_PATH/counter --base-url counter --app-name "Counter" --app-description "Counter to get an idea of Flet."
+flet publish apps/counter/basic/main.py --distpath $DIST_PATH/counter --base-url counter --app-name "Counter" --app-description "Counter to get an idea of Flet."
 flet publish apps/flet_animation/main.py --distpath $DIST_PATH/flet_animation --base-url flet_animation --app-name "Flet animation" --app-description "An example of implicit animations in Flet."
 flet publish apps/greeter/greeter.py --distpath $DIST_PATH/greeter --base-url greeter --app-name "Greeter" --app-description "A basic example of interactive forms in Flet."
 flet publish apps/hello_world/hello.py --distpath $DIST_PATH/hello_world --base-url hello_world --app-name "Hello, world!" --app-description "A very minimal example of Flet app."
