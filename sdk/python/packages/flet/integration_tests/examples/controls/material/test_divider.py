b29382b065d5b10e57bfb8520e3f196b8fd1ff2a
InesaFitsner <inesa@appveyor.com>
2026-04-10 18:18:58 -0700

    Add Divider example screenshot

--- sdk/python/packages/flet/integration_tests/examples/controls/material/test_divider.py
+++ sdk/python/packages/flet/integration_tests/examples/controls/material/test_divider.py
@@ -1,5 +1,6 @@
 import pytest
 
+import examples.controls.material.divider.basic.main as basic
 import flet as ft
 import flet.testing as ftt
 
@@ -17,5 +18,25 @@ async def test_image_for_docs(flet_app_function: ftt.FletTestApp, request):
                 ft.Divider(),
                 ft.Text("Section B"),
             ],
+        ),
+    )
+
+
+@pytest.mark.parametrize(
+    "flet_app_function",
+    [{"flet_app_main": basic.main}],
+    indirect=True,
+)
+@pytest.mark.asyncio(loop_scope="function")
+async def test_basic(flet_app_function: ftt.FletTestApp):
+    flet_app_function.page.enable_screenshots = True
+    flet_app_function.resize_page(320, 480)
+    flet_app_function.page.update()
+
+    await flet_app_function.tester.pump_and_settle()
+    flet_app_function.assert_screenshot(
+        "basic",
+        await flet_app_function.page.take_screenshot(
+            pixel_ratio=flet_app_function.screenshots_pixel_ratio
         ),
     )
